43b045db4460f80b4311b40d2c8294c1414aa2ae
Bryan Honof <bryanhonof@gmail.com>
2021-08-11 15:00:01 +0200

    Add arpa2cm back to quick-mem

--- pkgs/quick-mem/default.nix
+++ pkgs/quick-mem/default.nix
@@ -1,10 +1,10 @@
-{ src, stdenv, cmake, arpa2common }:
+{ src, stdenv, cmake, arpa2cm, arpa2common }:
 stdenv.mkDerivation {
   inherit src;
 
   name = "quick-mem";
 
-  nativeBuildInputs = [ cmake arpa2common ];
+  nativeBuildInputs = [ cmake arpa2common arpa2cm ];
 
   configurePhase = ''
     export PREFIX=$out
