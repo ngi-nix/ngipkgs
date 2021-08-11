888c652725929ef7e3ae210d9929988c48dda667
Bryan Honof <bryanhonof@gmail.com>
2021-08-11 13:14:47 +0200

    Add arpa2common

---
+++ pkgs/arpa2common/default.nix
@@ -0,0 +1,39 @@
+{ src, stdenv, cmake, arpa2cm, ragel, lmdb, libressl, libsodium, pkgconfig
+, libkrb5, e2fsprogs, doxygen, graphviz }:
+stdenv.mkDerivation {
+  inherit src;
+
+  name = "arpa2common";
+
+  nativeBuildInputs = [
+    cmake
+    arpa2cm
+    ragel
+    lmdb
+    libressl
+    libsodium
+    pkgconfig
+    libkrb5
+    e2fsprogs
+    doxygen
+    graphviz
+  ];
+
+  propagatedBuildInputs = [ lmdb libkrb5 libressl ];
+
+  configurePhase = ''
+    export PREFIX=$out
+  '';
+
+  buildPhase = ''
+    make all
+  '';
+
+  installPhase = ''
+    make install
+  '';
+
+  # The project uses single argument `printf` throughout the program
+  # Disabeling this seems like the easiest fix
+  hardeningDisable = [ "format" ];
+}
