61b5905d1f5d8628eedf02de376094d1c462fe42
Bryan Honof <bryanhonof@gmail.com>
2021-08-16 12:12:06 +0200

    Add freeDiameter derivation

---
+++ pkgs/freeDiameter/default.nix
@@ -0,0 +1,24 @@
+{ src, stdenv, cmake, pkgconfig, flex, bison, lksctp-tools, libidn, libgcrypt
+, gnutls }:
+stdenv.mkDerivation {
+  inherit src;
+
+  name = "freeDiameter";
+
+  nativeBuildInputs =
+    [ cmake pkgconfig flex bison lksctp-tools libidn libgcrypt gnutls ];
+
+  configurePhase = ''
+    mkdir -p build
+    cd build
+    cmake ..
+  '';
+
+  buildPhase = ''
+    cmake --build .
+  '';
+
+  installPhase = ''
+    cmake --install . --prefix $out
+  '';
+}
