e69e1345c7e0f227d6249ac2216aea80aafb87ad
Kerstin Humm <kerstin@erictapen.name>
2021-08-16 13:08:34 +0200

    init flake

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "Weblate package and module";
+
+  inputs.nixpkgs.url = "github:NixOS/Nixpkgs/nixos-unstable";
+  inputs.poetry2nix.url = "github:nix-community/poetry2nix/master";
+  inputs.weblate.url = "git+file:///home/kerstin/git/weblate?ref=weblate-4.7.2-poetry";
+  inputs.weblate.flake = false;
+
+  outputs = { self, nixpkgs, poetry2nix, weblate }:
+    let
+      pkgs = import nixpkgs {
+        system = "x86_64-linux";
+        overlays = [ poetry2nix.overlay ];
+      };
+    in
+    {
+
+      packages.x86_64-linux.weblate = pkgs.poetry2nix.mkPoetryApplication {
+        projectDir = weblate;
+      };
+
+      defaultPackage.x86_64-linux = self.packages.x86_64-linux.weblate;
+
+    };
+}
