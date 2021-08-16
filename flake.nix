7701823054c9c28b7d73289cd8054a2e44796688
Kerstin Humm <kerstin@erictapen.name>
2021-08-16 16:42:22 +0200

    add poetry files to this directory

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Weblate package and module";
 
   inputs.nixpkgs.url = "github:NixOS/Nixpkgs/nixos-unstable";
-  inputs.poetry2nix.url = "github:nix-community/poetry2nix/master";
+  inputs.poetry2nix.url = "github:nix-community/poetry2nix/1.18.0";
   inputs.weblate.url = "git+file:///home/kerstin/git/weblate?ref=weblate-4.7.2-poetry";
   inputs.weblate.flake = false;
 
@@ -16,7 +16,7 @@
     {
 
       packages.x86_64-linux.weblate = pkgs.poetry2nix.mkPoetryApplication {
-        projectDir = weblate;
+        projectDir = ./.;
       };
 
       defaultPackage.x86_64-linux = self.packages.x86_64-linux.weblate;
