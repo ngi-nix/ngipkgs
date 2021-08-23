39a5a50d927d84f1a79af32f62bdb405c8e0a2f2
Kerstin Humm <kerstin@erictapen.name>
2021-08-23 13:20:02 +0200

    first finished build of weblate

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Weblate package and module";
 
   inputs.nixpkgs.url = "github:NixOS/Nixpkgs/nixos-unstable";
-  inputs.poetry2nix.url = "github:nix-community/poetry2nix/1.18.0";
+  inputs.poetry2nix.url = "github:nix-community/poetry2nix/master";
   inputs.weblate.url = "git+file:///home/kerstin/git/weblate?ref=weblate-4.7.2-poetry";
   inputs.weblate.flake = false;
 
@@ -17,6 +17,30 @@
 
       packages.x86_64-linux.weblate = pkgs.poetry2nix.mkPoetryApplication {
         projectDir = ./.;
+        src = weblate;
+        overrides = pkgs.poetry2nix.overrides.withDefaults (
+          self: super: {
+            ruamel-yaml = super.ruamel-yaml.overridePythonAttrs (old: {
+              propagatedBuildInputs = (old.propagatedBuildInputs or [ ]) ++ [
+                self.ruamel-yaml-clib
+              ];
+            });
+	    weblate-language-data = super.weblate-language-data.overridePythonAttrs (old: {
+	      buildInputs = (old.buildInputs or [ ]) ++ [
+	        self.translate-toolkit
+	      ];
+	    });
+	    borgbackup = super.borgbackup.overridePythonAttrs (old: {
+	      BORG_OPENSSL_PREFIX = pkgs.openssl.dev;
+	      nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [ pkgs.pkg-config ];
+	      buildInputs = (old.buildInputs or [ ]) ++ (with pkgs; [ openssl acl ]);
+	      NIX_LDFLAGS = "-L${pkgs.openssl}/lib";
+	      NIX_CFLAGS_COMPILE = "-isystem ${pkgs.openssl.dev}/include";
+	    });
+	    # use borgbackup from nixpkgs for now.
+	    # borgbackup = pkgs.borgbackup;
+          }
+        );
       };
 
       defaultPackage.x86_64-linux = self.packages.x86_64-linux.weblate;
