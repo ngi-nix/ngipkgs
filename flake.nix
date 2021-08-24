838f775110c7a5c43de3776b50e17a0a2aa6aa9a
Bryan Honof <bryanhonof@gmail.com>
2021-08-24 10:20:10 +0200

    Move the outputs attribute set to the top
    The inputs attribute set was getting a bit long, moving it to the bottom
    makes for a better reading experience, since all the important parts are
    at the top now.

--- flake.nix
+++ flake.nix
@@ -1,6 +1,47 @@
-{
+rec {
   description =
     "Development projects for InternetWide.org, which builds a modern (domain) hosting stack intended to make users once more first-class citizens of the Internet.";
+
+  outputs = { self, nixpkgs, ... }@args:
+    let
+      supportedSystems = [ "x86_64-linux" ];
+
+      # BEGIN Helper functions
+      forAllSystems = f:
+        nixpkgs.lib.genAttrs supportedSystems (system: f system);
+
+      nixpkgsFor = forAllSystems (system:
+        import nixpkgs {
+          inherit system;
+          overlays = [ self.overlay ];
+        });
+      # END Helper functions
+    in {
+      overlay = final: prev:
+        nixpkgs.lib.composeManyExtensions [
+          args.poetry2nix.overlay
+          self.overlays.arpa2-packages
+        ] final prev;
+
+      overlays = {
+        arpa2-packages = import ./overlays/arpa2-packages inputs args;
+        arpa2-python-packages =
+          import ./overlays/arpa2-python-packages inputs args;
+      };
+
+      packages = forAllSystems (system: {
+        inherit (nixpkgsFor.${system})
+          arpa2cm arpa2common steamworks quick-mem quick-der lillydap leaf
+          quick-sasl tlspool tlspool-gui kip freeDiameter steamworks-pulleyback;
+      });
+
+      checks = forAllSystems (system: {
+        inherit (nixpkgsFor.${system})
+          arpa2cm arpa2common steamworks steamworks-pulleyback quick-mem
+          quick-der lillydap leaf quick-sasl tlspool tlspool-gui freeDiameter
+          kip;
+      });
+    };
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
@@ -15,7 +56,7 @@
       type = "gitlab";
       owner = "arpa2";
       repo = "arpa2cm";
-      rev = "c3aa5b6ce5c3c14a66bbf27044c517186176cb66";
+      ref = "v0.9.0";
       flake = false;
     };
 
@@ -23,7 +64,7 @@
       type = "gitlab";
       owner = "arpa2";
       repo = "arpa2common";
-      rev = "c65fbe034a288ba504b4b68b62b1546b8c3ddf5a";
+      ref = "v2.2.14";
       flake = false;
     };
 
@@ -31,7 +72,7 @@
       type = "gitlab";
       owner = "arpa2";
       repo = "steamworks";
-      rev = "93935c899cd14b7659af0dbca05c776f7dc85ff4";
+      ref = "v0.97.2";
       flake = false;
     };
 
@@ -39,7 +80,7 @@
       type = "gitlab";
       owner = "arpa2";
       repo = "steamworks-pulleyback";
-      rev = "f63d89ba54f3919fa9c67aab9d59efad3f2dc9c7";
+      ref = "v0.3.0";
       flake = false;
     };
 
@@ -47,7 +88,7 @@
       type = "gitlab";
       owner = "arpa2";
       repo = "quick-mem";
-      rev = "4f8df4475ba5a48eaf633885c15f38f13b4f7392";
+      ref = "v0.2.2";
       flake = false;
     };
 
@@ -55,7 +96,7 @@
       type = "gitlab";
       owner = "arpa2";
       repo = "quick-der";
-      rev = "bda3d1e8f6e021778531ab61a434960931dd88a1";
+      ref = "v1.6.2";
       flake = false;
     };
 
@@ -63,7 +104,7 @@
       type = "gitlab";
       owner = "arpa2";
       repo = "lillydap";
-      rev = "7dd28599dad7068d0f373f01f4bb106fd14deca6";
+      ref = "v0.9.2";
       flake = false;
     };
 
@@ -71,7 +112,7 @@
       type = "gitlab";
       owner = "arpa2";
       repo = "leaf";
-      rev = "b3861efce0ba143f6eb5451aac5be24f18e6d8ab";
+      ref = "master";
       flake = false;
     };
 
@@ -79,7 +120,7 @@
       type = "gitlab";
       owner = "arpa2";
       repo = "quick-sasl";
-      rev = "9b7981254e14e53c41a118193ca550d503a46cba";
+      ref = "v0.11.0";
       flake = false;
     };
 
@@ -87,7 +128,7 @@
       type = "gitlab";
       owner = "arpa2";
       repo = "tlspool";
-      rev = "dcfea4f6e504b9109d7f2300261a38d85db05025";
+      ref = "v0.9.6";
       flake = false;
     };
 
@@ -95,7 +136,7 @@
       type = "gitlab";
       owner = "arpa2";
       repo = "tlspool-gui";
-      rev = "371858d5b19d0d32ef12c13dd1284a9560f47f9d";
+      ref = "v0.0.6";
       flake = false;
     };
 
@@ -103,7 +144,7 @@
       type = "gitlab";
       owner = "arpa2";
       repo = "kip";
-      rev = "ffdfba53ef1476df3d45662dec9aae1e7136b32a";
+      ref = "master";
       flake = false;
     };
 
@@ -111,57 +152,8 @@
       type = "github";
       owner = "freeDiameter";
       repo = "freeDiameter";
-      rev = "8ac823b77653a03da82232470b376c2b281b0973";
+      ref = "master";
       flake = false;
     };
   };
-
-  outputs = { self, nixpkgs, ... }@inputs:
-    let
-      supportedSystems = [ "x86_64-linux" ];
-
-      # BEGIN Helper functions
-      forAllSystems = f:
-        nixpkgs.lib.genAttrs supportedSystems (system: f system);
-
-      nixpkgsFor = forAllSystems (
-        system:
-          import nixpkgs {
-            inherit system;
-            overlays = [ self.overlay ];
-          }
-      );
-      # END Helper functions
-    in
-      {
-        overlay = final: prev:
-          nixpkgs.lib.composeManyExtensions [
-            inputs.poetry2nix.overlay
-            self.overlays.arpa2-packages
-          ] final prev;
-
-        overlays = {
-          arpa2-packages = import ./overlays/arpa2-packages inputs;
-          arpa2-python-packages = import ./overlays/arpa2-python-packages inputs;
-        };
-
-        packages = forAllSystems (
-          system: {
-            inherit (nixpkgsFor.${system})
-              arpa2cm arpa2common steamworks quick-mem quick-der lillydap leaf
-              quick-sasl tlspool tlspool-gui kip freeDiameter steamworks-pulleyback
-              ;
-          }
-        );
-
-        checks = forAllSystems (
-          system: {
-            inherit (nixpkgsFor.${system})
-              arpa2cm arpa2common steamworks steamworks-pulleyback quick-mem
-              quick-der lillydap leaf quick-sasl tlspool tlspool-gui freeDiameter
-              kip
-              ;
-          }
-        );
-      };
 }
