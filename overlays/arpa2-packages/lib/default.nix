db892598eb5654c1db20fb6d84756a2fee838cd5
Bryan Honof <bryanhonof@gmail.com>
2021-08-25 13:14:18 +0200

    Add documentation

--- overlays/arpa2-packages/lib/default.nix
+++ overlays/arpa2-packages/lib/default.nix
@@ -4,6 +4,9 @@
   mkArpa2Derivation = { ... }@args:
     let
       defaultSet = {
+        # cmake, apra2cm, and arpa2common, are almost always a dependency of a
+        # project inside the bigger apra2 project. So just include them by
+        # default.
         nativeBuildInputs = with pkgs; [ cmake arpa2cm arpa2common ];
 
         # Remove `./Makefile` since it causes the default builder to not use
