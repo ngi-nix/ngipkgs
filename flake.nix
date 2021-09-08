9ba57dd1f0ea3904dd8ef9732b0b53177433acac
Joseph Lucas <lucas.jojo@gmail.com>
2021-09-09 01:09:24 +0200

    remove darwin plateform

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
   inputs.nixpkgs.url = "github:JosephLucas/nixpkgs/anastasis";
   outputs = { self, nixpkgs }:
     let
-      supportedSystems = [ "x86_64-linux" "x86_64-darwin" ];
+      supportedSystems = [ "x86_64-linux" ];
       forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
       nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; overlays = [ self.overlay ]; });
     in
