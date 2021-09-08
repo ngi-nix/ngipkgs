4066f55ec0d287fbb78cd134306529159005b74c
Joseph Lucas <lucas.jojo@gmail.com>
2021-09-08 12:23:09 +0200

    add Hydra check and devShell

--- flake.nix
+++ flake.nix
@@ -11,5 +11,8 @@
       overlay = final: prev: { anastasis = (final.callPackage ./default.nix {}); };
       packages = forAllSystems (system: { inherit (nixpkgsFor.${system}) anastasis; });
       defaultPackage = forAllSystems (system: self.packages.${system}.anastasis);
+      # FIXME: check also for x86_64-darwin as soon as Hydra will check darwin derivations
+      checks.x86_64-linux.anastasis = self.packages.x86_64-linux.anastasis;
+      devShell = self.defaultPackage;
   };
 }
