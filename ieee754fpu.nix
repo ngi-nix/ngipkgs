9b5a15a1f6a94270c7505083c4f24ef9e3080666
Las Safin <me@las.rs>
2021-09-11 17:07:34 +0000

    wip

---
+++ ieee754fpu.nix
@@ -0,0 +1,21 @@
+{ lib, buildPythonPackage, bigfloat, fetchgit }:
+
+buildPythonPackage {
+  pname = "libresoc-ieee754fpu";
+  version = "unstable-2021-06-05";
+
+  src = fetchgit {
+    url = "https://git.libre-soc.org/git/ieee754fpu.git";
+    rev = "c62fa3a7ee95832587d7725729dcdb9a002ae015";
+    sha256 = "wbr1vGFzUlUtBT6IcRsykADYeksiVoq/LacU/dbRQ0o=";
+  };
+
+  propagatedBuildInputs = [ bigfloat ];
+
+  doCheck = false;
+
+  meta = with lib; {
+    homepage = "https://pypi.org/project/libresoc-ieee754fpu/";
+    license = licenses.lgpl3Plus;
+  };
+}
---
+++ verilog.nix
@@ -0,0 +1,33 @@
+{ version }:
+
+{ stdenv, python3Packages }:
+
+stdenv.mkDerivation {
+  pname = "libresoc.v";
+  inherit version;
+
+  src = ../.;
+
+  strictDeps = true;
+
+  nativeBuildInputs = with python3Packages; [ python libresoc-ieee754fpu libresoc-openpower-isa ];
+
+  configurePhase = "true";
+
+  buildPhase = ''
+    runHook preBuild
+    python3 src/soc/simple/issuer_verilog.py \
+      --debug=jtag --enable-core --enable-pll \
+      --enable-xics --enable-sram4x4kblock --disable-svp64 \
+      libresoc.v
+    runHook postBuild
+  '';
+
+  installPhase = ''
+    runHook preInstall
+    mv libresoc.v $out
+    runHook postInstall
+  '';
+
+  fixupPhase = "true";
+}
