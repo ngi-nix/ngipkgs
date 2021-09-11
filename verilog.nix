a541c89b76935105a275237e550da0d26a9a9e9e
Las Safin <me@las.rs>
2021-09-11 20:53:05 +0000

    Get build starting

--- verilog.nix
+++ verilog.nix
@@ -10,13 +10,15 @@ stdenv.mkDerivation {
 
   strictDeps = true;
 
-  nativeBuildInputs = with python3Packages; [ python libresoc-ieee754fpu libresoc-openpower-isa ];
+  nativeBuildInputs = with python3Packages; [ nmigen-soc python libresoc-ieee754fpu libresoc-openpower-isa ];
 
   configurePhase = "true";
 
   buildPhase = ''
     runHook preBuild
-    python3 src/soc/simple/issuer_verilog.py \
+    cd src
+    export PYTHONPATH="$PWD:$PYTHONPATH"
+    python3 soc/simple/issuer_verilog.py \
       --debug=jtag --enable-core --enable-pll \
       --enable-xics --enable-sram4x4kblock --disable-svp64 \
       libresoc.v
