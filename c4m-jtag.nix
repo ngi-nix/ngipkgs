3f37a7880fe4fb77ede472bf4ede7cfd0578b299
Las Safin <me@las.rs>
2021-09-12 19:35:23 +0000

    Fix c4m-jtag

--- c4m-jtag.nix
+++ c4m-jtag.nix
@@ -1,16 +1,22 @@
 { version, src }:
 
-{ lib, python, buildPythonPackage, nmigen-soc, nmigen, modgrammar }:
+{ lib, python, buildPythonPackage, nmigen-soc, nmigen, modgrammar, setuptools-scm }:
 
 buildPythonPackage {
-  pname = "libresoc-openpower-isa";
-  inherit version src;
+  pname = "c4m-jtag";
+  inherit src;
+  version = "2.17";
 
+  nativeBuildInputs = [ setuptools-scm ];
   propagatedBuildInputs = [ nmigen-soc nmigen modgrammar ];
 
   doCheck = false;
 
   pythonImportsCheck = [ "c4m.nmigen.jtag.tap" ];
+
+  prePatch = ''
+    sed -i -e 's/use_scm_version=scm_version..,//g' setup.py
+  '';
 
   meta = with lib; {
     homepage = "https://pypi.org/project/libresoc-openpower-isa/";
