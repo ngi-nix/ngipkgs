56b8e335ed92f1c454a2e6b3993029d5fe894651
xmzlyw <100826331+xmzlyw@users.noreply.github.com>
2022-08-31 21:44:49 +0800

    Init hyperbeam flake
    Co-authored-by: Bill Ewanick <bill@ewanick.com>
    Co-authored-by: a-kenji <aks.kenji@protonmail.com>
    Co-authored-by: Matthias Meschede <MMesch@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # hyperspace
+
+This flake was generated with dream2nix which added all dependencies in the lock files folder.
+To run, you need to have a recent version of Nix installed which has good flake support.
+
+You can then run:
+
+```
+echo 'hello world' | nix run github:ngi-nix/hyperspace
+```
--- flake.nix
+++ flake.nix
@@ -1,11 +1,11 @@
 {
   inputs.dream2nix.url = "github:nix-community/dream2nix";
-  inputs.hypercore.url = "github:hypercore-protocol/hypercore";
-  inputs.hypercore.flake = false;
+  inputs.hyperbeam.url = "github:mafintosh/hyperbeam";
+  inputs.hyperbeam.flake = false;
   outputs = inp:
     inp.dream2nix.lib.makeFlakeOutputs {
       systemsFromFile = ./nix_systems;
       config.projectRoot = ./.;
-      source = inp.hypercore;
+      source = inp.hyperbeam;
     };
 }
