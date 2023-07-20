7649658f5c28e58fddb149c3b6cd25c9a91bda42
Anish Lakhwara <anish+git@lakhwara.com>
2023-07-20 14:27:08 -0700

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:pkgs/flarum/default.nix

--- pkgs/flarum/default.nix
+++ pkgs/flarum/default.nix
@@ -1,3 +1,12 @@
-{ ... }: {
+{ stdenv, fetchFromGitHub }:
 
+stdenv.mkDerivation rec {
+  pname = "flarum";
+  version = "v1.8.1";
+  src = fetchFromGitHub {
+    owner = "flarum";
+    repo = "framework";
+    rev = version;
+  };
 }
+
