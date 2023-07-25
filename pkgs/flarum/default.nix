dc7176e60f690851ede200b88833bba1006abf38
Albert Chae <albertchae@users.noreply.github.com>
2023-07-24 17:39:09 -0700

    Use albertchae/flarum fork which has composer.lock committed
    copy installPhase build script from closed PR https://github.com/NixOS/nixpkgs/pull/96869
    
    fix typo

--- pkgs/flarum/default.nix
+++ pkgs/flarum/default.nix
@@ -5,37 +5,35 @@
 , pkgs
 , nixosTests
 , dataDir ? "/var/lib/flarum"
-, runtimeDir ? "/run/falrum"
+, runtimeDir ? "/run/flarum"
 }:
 
 let
-  package = (import ./composition.nix {
-    inherit pkgs;
-    inherit (stdenv.hostPlatform) system;
-    noDev = true; # Disable development dependencies
-  }).overrideAttrs (attrs : {
-    installPhase = attrs.installPhase + ''
-      rm -R $out/bootstrap/cache
-      # Move static contents for the NixOS module to pick it up, if needed.
-      mv $out/bootstrap $out/bootstrap-static
-      mv $out/storage $out/storage-static
-      ln -s ${dataDir}/.env $out/.env
-      ln -s ${dataDir}/storage $out/
-      ln -s ${dataDir}/storage/app/public $out/public/storage
-      ln -s ${runtimeDir} $out/bootstrap
-      chmod +x $out/artisan
-    '';
-  });
-in package.override rec {
   pname = "flarum";
   version = "1.8.0";
 
   src = fetchFromGitHub {
     owner = "albertchae";
     repo = pname;
-    rev = "v${version}";
-    hash = "sha256-xadZIdyH20mxfxCyiDRtSRSrPj8DWXpuup61WSsjgWw=";
+    rev = "833ccafb14cbf196c92d23524d4026bc8c740441";
+    hash = "sha256-3Ig20CgjAaJq4dkSrtkjV6yqDpiaBv7Tcc81wUwCfVQ=";
   };
+  package = (import ./composition.nix {
+    inherit pkgs;
+    inherit (stdenv.hostPlatform) system;
+    noDev = true; # Disable development dependencies
+  }).overrideAttrs (attrs : {
+    installPhase = attrs.installPhase + ''
+      cp -ar ${src}/{site.php,flarum,.nginx.conf} $out/
+      # ln -s $\{dependencies}/composer.{json,lock} $out/
+      # ln -s $\{dependencies}/vendor $out/
+      cp ${src}/public/index.php $out/public/
+      # ln -s $\{cfg.stateDir}/assets $out/public/assets
+      # ln -s $\{cfg.stateDir}/{storage,extensions} $out/
+    '';
+  });
+in package.override rec {
+  inherit src version pname;
 
   passthru = {
     tests = { inherit (nixosTests) pixelfed; };
