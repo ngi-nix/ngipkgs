ac50c3ba5b9508a109714d8d66bb1bf7c4c5eef7
Cleeyv <cleeyv@riseup.net>
2023-07-27 14:07:31 -0400

    Add more comments to the refactored flake

--- flake.nix
+++ flake.nix
@@ -2,8 +2,9 @@
   description = "NgiPkgs";
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-  inputs.systems.url = "github:nix-systems/x86_64-linux";
   inputs.flake-utils.url = "github:numtide/flake-utils";
+  # Set the defaultSystem list for flake-utils to only x86_64-linux
+  inputs.systems.url = "github:nix-systems/x86_64-linux";
   inputs.flake-utils.inputs.systems.follows = "systems";
 
   outputs = { self, nixpkgs, flake-utils, ... }:
@@ -17,14 +18,17 @@
           nixosModules = {
             modules = import ./modules/all-modules.nix;
             ngipkgs = { ... }: {
-              # inject an additional argument into the module system evaluation.
-              # this way our package set can be accessed separately and we don't have
+              # Inject an additional argument into the module system evaluation.
+              # This way our package set can be accessed separately and we don't have
               # to muck around with overlays (which don't work with flakes as you'd expect)
               _module.args.ngipkgs = self.packages.${system};
             };
           };
         });
     checkOutputs = (system: {
+      # Configurations have to go in checkOutputs (ie, avoid `eachDefaultSystem`) to generate
+      # a single attribute name for nixos-container deployments (`<config-name>`), because
+      # nixos-container can't parse dot-separated sequence attribute paths (`x86_64-linux.<config-name>`).
       nixosConfigurations =
         let
           pkgs = nixpkgs.legacyPackages.${system};
@@ -33,10 +37,14 @@
         in
         builtins.mapAttrs inject-ngipkgs all-configurations;
 
+      # To generate a Hydra jobset for CI builds of all packages
+      # https://hydra.ngi0.nixos.org/jobset/ngipkgs/main
       hydraJobs = {
         packages.${system} = self.packages.${system};
       };
 
+      # For .github/workflows/ci.yaml to *build* all packages, because
+      # `nix flake check` only evaluates packages, but it builds checks.
       checks.${system} = self.packages.${system};
     } );
   in (flake-utils.lib.eachDefaultSystem buildOutputs) // (checkOutputs "x86_64-linux");
