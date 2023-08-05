9a6f2553872fb5a588aebb4bef0a3cf5b4d9ee82
Jason Odoom <jasonodoom@gmail.com>
2023-08-05 15:07:14 -0400

    librecast: package lcrq
    Towards #3.
    
    Package https://codeberg.org/librecast/lcrq, direct dependency of https://codeberg.org/librecast/librecast.
    
    Co-authored-by: Jason Odoom <jasonodoom@gmail.com>
    Co-authored-by: Anish Lakhwara <anish+git@lakhwara.com>
    Co-authored-by: Dominic Mills <dominic.millz27@gmail.com>
    Co-authored-by: Albert Chae <albertchae@users.noreply.github.com>
    Co-authored-by: Jack Leightcap <jack@leightcap.com>
    Signed-off-by: Jack Leightcap <jack@leightcap.com>

--- all-packages.nix
+++ all-packages.nix
@@ -3,6 +3,7 @@
     flarum = callPackage ./pkgs/flarum {};
     gnunet-messenger-cli = callPackage ./pkgs/gnunet-messenger-cli {};
     kikit = callPackage ./pkgs/kikit {};
+    lcrq = callPackage ./pkgs/lcrq {};
     liberaforms = callPackage ./pkgs/liberaforms {};
     liberaforms-env = callPackage ./pkgs/liberaforms/env.nix {};
     libgnunetchat = callPackage ./pkgs/libgnunetchat {};
