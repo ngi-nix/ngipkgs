5c884ac64c3876e7c8ba79bb04e476727ce8bcca
Lorenz Leutgeb <lorenz@leutgeb.xyz>
2023-09-15 14:34:40 +0200

    repo: Introduce `pkgs/by-name`

---
+++ pkgs/by-name/default.nix
@@ -0,0 +1,32 @@
+{
+  lib,
+  callPackage,
+}: let
+  baseDirectory = ./.;
+
+  inherit
+    (builtins)
+    readDir
+    ;
+
+  inherit
+    (lib.attrsets)
+    mapAttrs
+    concatMapAttrs
+    ;
+
+  names = name: type:
+    if type != "directory"
+    then assert name == "README.md" || name == "default.nix"; {}
+    else {${name} = baseDirectory + "/${name}/package.nix";};
+
+  packageFiles = concatMapAttrs names (readDir baseDirectory);
+
+  self =
+    mapAttrs (
+      _: file:
+        callPackage file {}
+    )
+    packageFiles;
+in
+  self
---
+++ pkgs/default.nix
@@ -0,0 +1,33 @@
+{
+  lib,
+  callPackage,
+}: let
+  self = rec {
+    # LiberaForms is intentionally disabled.
+    # Refer to <https://github.com/ngi-nix/ngipkgs/issues/40>.
+    #liberaforms = callPackage ./pkgs/liberaforms {};
+    #liberaforms-env = callPackage ./pkgs/liberaforms/env.nix {};
+
+    pretalx = callPackage ./pretalx {};
+    pretalx-frontend = callPackage ./pretalx/frontend.nix {};
+    pretalx-full = callPackage ./pretalx {
+      withPlugins = [
+        pretalx-downstream
+        pretalx-media-ccc-de
+        pretalx-pages
+        pretalx-venueless
+        pretalx-public-voting
+      ];
+    };
+
+    inherit
+      (lib.recurseIntoAttrs (callPackage ./pretalx/plugins.nix {}))
+      pretalx-downstream
+      pretalx-media-ccc-de
+      pretalx-pages
+      pretalx-venueless
+      pretalx-public-voting
+      ;
+  };
+in
+  self
