f5494e22b7be16eb3a845e53eac311128a618026
Jack Leightcap <jack@leightcap.com>
2023-09-30 16:42:19 -0400

    atomic-web: pnpm builder patches failing
    Signed-off-by: Jack Leightcap <jack@leightcap.com>

--- pkgs/atomic-web/default.nix
+++ pkgs/atomic-web/default.nix
@@ -4,13 +4,17 @@ with (import ./pnpm2nix-lockfile-6.0 { inherit pkgs; });
 mkPnpmPackage rec {
   version = "v0.34.5";
 
+  prePatch = "exit 1";
+
+  patches = [
+    ./package-json-version.patch
+  ];
+
   src = fetchFromGitHub {
-    # https://github.com/atomicdata-dev/atomic-server.git
     owner = "atomicdata-dev";
-    repo = "atomic-server";
-    rev = version;
-    hash = "sha256-rqucTVvlXe9CXPsZ2cNzyDK9onXw/H96PzWpTR7Fdl4=";
+    repo = "atomic-data-browser";
+    rev = "9b913058508c9da6a4062fd0fe39fcefc1205c4d";
+    hash = "sha256-lQDfFXnXCW+fhs1ayjbsm3VojxvMEh9eiUyHoTm3qtg=";
   };
 
-  sourceRoot = "${src}/browser";
 }
