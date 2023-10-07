9ce30ebcc16dab9a2f74cd23f28e02dea26989a4
Jack Leightcap <jack@leightcap.com>
2023-10-06 22:33:43 -0400

    atomic-web: default builder, patch
    - use generic `mkDerivation` scaffolding
    - define/apply patch from forked changes
    
    Signed-off-by: Jack Leightcap <jack@leightcap.com>

--- pkgs/atomic-web/default.nix
+++ pkgs/atomic-web/default.nix
@@ -1,21 +1,33 @@
-{ pkgs, fetchFromGitHub, mkYarnPackage, fetchYarnDeps, ... }:
+{ pkgs, fetchFromGitHub, stdenv, fetchpatch, ... }:
 
-mkYarnPackage rec {
+stdenv.mkDerivation rec {
+  name = "atomic-web";
   version = "v0.34.5";
 
   src = fetchFromGitHub {
     owner = "atomicdata-dev";
     repo = "atomic-server";
+    # FIXME(jl): note where/why for this commit hash
     rev = "873caf39d99f703427aa50fe32ec1aca19e4b3b0";
     hash = "sha256-mcnPv51rhfqrWGU/Gas6CJ0h0PdpRvJ2onnCw8Mccew=";
   };
+
+  patches = [
+    (fetchpatch {
+      url =
+        "https://github.com/atomicdata-dev/atomic-server/compare/develop...ngi-nix:atomic-server:workspace-versions.patch";
+      hash = "sha256-r0UdQg8SAfYLilpgyqswJ9wWt/Owe6S8l7SwOlnp67cgj";
+    })
+  ];
+
   sourceRoot = "${src}/browser";
 
-  packageJSON = "${sourceRoot}/package.json";
-  # Upstream does not contain a yarn.lock
-  yarnLock = ./yarn.lock;
-  offlineCache = fetchYarnDeps {
-    yarnLock = ./yarn.lock;
-    hash = "sha256-GK5Ehk82VQ5ajuBTQlPwTB0aaxhjAoD2Uis8wiam7Z0=";
-  };
+  /* packageJSON = "${sourceRoot}/package.json";
+     # Upstream does not contain a yarn.lock
+     yarnLock = ./yarn.lock;
+     offlineCache = fetchYarnDeps {
+       yarnLock = ./yarn.lock;
+       hash = "sha256-GK5Ehk82VQ5ajuBTQlPwTB0aaxhjAoD2Uis8wiam7Z0=";
+     };
+  */
 }
