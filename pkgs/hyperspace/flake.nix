50295cc514829d9475416584b4fbe4285e5fa29e
Alejandro Sanchez Medina <alejandrosanchzmedina@gmail.com>
2023-10-07 17:35:39 +0100

    hyperspace: fix build after syncing nixpkgs with parent flake lock

--- pkgs/hyperspace/flake.nix
+++ pkgs/hyperspace/flake.nix
@@ -2,7 +2,7 @@
   description = "Monorepo holding various Hypercore related packages";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/8b5ab8341e33322e5b66fb46ce23d724050f6606";
     dream2nix = { url = "github:nix-community/dream2nix"; inputs.nixpkgs.follows = "nixpkgs"; };
     hyperbeam = { url = "github:mafintosh/hyperbeam"; flake = false; };
     hypercore = { url = "github:hypercore-protocol/hypercore"; flake = false; };
@@ -14,10 +14,16 @@
 
   outputs = { self, nixpkgs, dream2nix, ... }@inputs:
     let
+      pkgs = import nixpkgs {
+        system = "x86_64-linux";
+        config.permittedInsecurePackages = [
+          "nodejs-16.20.2"
+        ];
+      };
+
       mkOuts = source: dream2nix.lib.makeFlakeOutputs {
-        systems = [ "x86_64-linux" ];
         config.projectRoot = ./.;
-        inherit source;
+        inherit pkgs source;
         settings = [{ subsystemInfo.nodejs = 16; }];
       };
     in
