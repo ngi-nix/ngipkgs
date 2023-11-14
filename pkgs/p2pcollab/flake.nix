696e6dd33916da7eb81536cd9b8ec432ba21f359
Matúš Ferech <matus.ferech@gmail.com>
2023-11-14 17:59:40 +0100

    p2pcollab: fmt

--- pkgs/p2pcollab/flake.nix
+++ pkgs/p2pcollab/flake.nix
@@ -4,52 +4,51 @@
   # we're using this commit since the required dependencies aren't in master yet
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/9e14dd7220446ed5889138e024f33dfe367556d0";
 
-  outputs = { self, nixpkgs }:
-    let
-      supportedSystems = [
-        "x86_64-linux"
-        "aarch64-linux"
-        "armv7l-linux"
-        "x86_64-darwin"
-        "aarch64-darwin"
-      ];
-      supportedOcamlPackages = [
-        "ocamlPackages_4_10"
-        "ocamlPackages_4_11"
-        "ocamlPackages_4_12"
-      ];
-      defaultOcamlPackages = "ocamlPackages_4_12";
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    supportedSystems = [
+      "x86_64-linux"
+      "aarch64-linux"
+      "armv7l-linux"
+      "x86_64-darwin"
+      "aarch64-darwin"
+    ];
+    supportedOcamlPackages = [
+      "ocamlPackages_4_10"
+      "ocamlPackages_4_11"
+      "ocamlPackages_4_12"
+    ];
+    defaultOcamlPackages = "ocamlPackages_4_12";
 
-      forAllOcamlPackages = nixpkgs.lib.genAttrs supportedOcamlPackages;
-      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-      nixpkgsFor =
-        forAllSystems (system:
-          import nixpkgs {
-            inherit system;
-            overlays = [ self.overlay ];
-          });
-    in
-    {
-      overlay = final: prev:
-        with final;
-        let mkOcamlPackages = prevOcamlPackages:
-          with prevOcamlPackages;
-          let ocamlPackages = {
-            inherit buildDunePackage angstrom lwt noise ounit stdint;
-            inherit ocaml;
-            inherit findlib;
-            inherit ocamlbuild;
-            inherit opam-file-format;
+    forAllOcamlPackages = nixpkgs.lib.genAttrs supportedOcamlPackages;
+    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    nixpkgsFor = forAllSystems (system:
+      import nixpkgs {
+        inherit system;
+        overlays = [self.overlay];
+      });
+  in {
+    overlay = final: prev:
+      with final; let
+        mkOcamlPackages = prevOcamlPackages:
+          with prevOcamlPackages; let
+            ocamlPackages = {
+              inherit buildDunePackage angstrom lwt noise ounit stdint;
+              inherit ocaml;
+              inherit findlib;
+              inherit ocamlbuild;
+              inherit opam-file-format;
 
-            noise-socket =
-              buildDunePackage rec {
+              noise-socket = buildDunePackage rec {
                 pname = "noise-socket";
                 version = "0.0.1";
                 src = self;
 
                 useDune2 = true;
 
-                nativeBuildInputs = with ocamlPackages; [ odoc ];
+                nativeBuildInputs = with ocamlPackages; [odoc];
 
                 propagatedBuildInputs = with ocamlPackages; [
                   angstrom
@@ -63,24 +62,25 @@
                   ounit
                 ];
               };
-          };
-          in ocamlPackages;
-        in
-        let allOcamlPackages =
+            };
+          in
+            ocamlPackages;
+      in let
+        allOcamlPackages =
           forAllOcamlPackages (ocamlPackages:
             mkOcamlPackages ocaml-ng.${ocamlPackages});
-        in
-        allOcamlPackages // {
+      in
+        allOcamlPackages
+        // {
           ocamlPackages = allOcamlPackages.${defaultOcamlPackages};
         };
 
-      packages =
-        forAllSystems (system:
-          forAllOcamlPackages (ocamlPackages:
-            nixpkgsFor.${system}.${ocamlPackages}));
+    packages = forAllSystems (system:
+      forAllOcamlPackages (ocamlPackages:
+        nixpkgsFor.${system}.${ocamlPackages}));
 
-      defaultPackage =
-        forAllSystems (system:
-          nixpkgsFor.${system}.ocamlPackages.noise-socket);
-    };
+    defaultPackage =
+      forAllSystems (system:
+        nixpkgsFor.${system}.ocamlPackages.noise-socket);
+  };
 }
