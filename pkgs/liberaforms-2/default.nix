8b5e37ffa27ad40918ddad8927c07d46b3e133ad
Alejandro Sanchez Medina <alejandrosanchzmedina@gmail.com>
2023-11-15 10:51:50 -0500

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:pkgs/liberaforms-2/default.nix

--- pkgs/liberaforms-2/default.nix
+++ pkgs/liberaforms-2/default.nix
@@ -4,4 +4,13 @@
 
   src = {
   };
+
+  {fetchFromGitLab, ...}:
+fetchFromGitLab {
+  owner = "liberaforms";
+  repo = "liberaforms";
+  rev = "v2.1.2";
+  sha256 = "sha256-JNs7SU9imLzWeVFGx2gxqqt8Bbea7SsvoHXJBxxona4=";
+}
+
 }
