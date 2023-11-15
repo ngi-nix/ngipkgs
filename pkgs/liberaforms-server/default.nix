51d348627020bab5394b494ac12b8be25e837c2c
Matúš Ferech <matus.ferech@gmail.com>
2023-11-15 17:25:21 +0100

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:pkgs/liberaforms-server/default.nix

--- pkgs/liberaforms-server/default.nix
+++ pkgs/liberaforms-server/default.nix
@@ -2,7 +2,7 @@
   fetchFromGitLab,
   python3,
 }: let
-    pname = "liberaforms-server";
+  pname = "liberaforms-server";
   version = "2.1.2";
 in
   python3.pkgs.buildPythonPackage {
@@ -15,6 +15,63 @@ in
       rev = "v${version}";
       sha256 = "sha256-JNs7SU9imLzWeVFGx2gxqqt8Bbea7SsvoHXJBxxona4=";
     };
+
+    propagatedBuildInputs = with python3.pkgs; [
+      greenlet
+      gunicorn
+      idna
+      importlib-metadata
+      importlib-resources
+      iniconfig
+      itsdangerous
+      jinja2
+      ldap3
+      lxml
+      mako
+      markdown
+      markupsafe
+      marshmallow
+      marshmallow-sqlalchemy
+      minio
+      packaging
+      passlib
+      # password-strength
+      pillow
+      platformdirs
+      pluggy
+      portpicker
+      prometheus-client
+      psutil
+      psycopg2
+      py
+      pyasn1
+      pycodestyle
+      pycparser
+      pyjwt
+      pyparsing
+      pypng
+      pyqrcode
+      python-dateutil
+      python-dotenv
+      python-magic
+      pytz
+      requests
+      six
+      smtpdfix
+      snowballstemmer
+      soupsieve
+      sqlalchemy
+      sqlalchemy-json
+      toml
+      typed-ast
+      unicodecsv
+      unidecode
+      urllib3
+      webencodings
+      werkzeug
+      wtforms
+      zipp
+    ];
 
     preBuild = ''
       cat > setup.py << EOF
