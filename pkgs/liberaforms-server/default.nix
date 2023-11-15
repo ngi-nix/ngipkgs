456d669871537c00d3a8ad6b260ee9824aece655
Auguste Baum <auguste.apple@gmail.com>
2023-11-15 17:07:22 +0100

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:pkgs/liberaforms-server/default.nix

--- pkgs/liberaforms-server/default.nix
+++ pkgs/liberaforms-server/default.nix
@@ -1,17 +1,33 @@
 {
   fetchFromGitLab,
-  buildPythonPackage,
+  python3,
 }: let
+    pname = "liberaforms-server";
   version = "2.1.2";
 in
-  buildPythonPackage {
-    pname = "liberaforms-server";
-    inherit version;
+  python3.pkgs.buildPythonPackage {
+    inherit pname version;
 
     src = fetchFromGitLab {
       owner = "liberaforms";
-      repo = "server";
+      repo = "liberaforms";
       rev = "v${version}";
       sha256 = "sha256-JNs7SU9imLzWeVFGx2gxqqt8Bbea7SsvoHXJBxxona4=";
     };
+
+    preBuild = ''
+      cat > setup.py << EOF
+      from setuptools import setup
+
+      with open('requirements.txt') as f:
+          install_requires = f.read().splitlines()
+
+      setup(
+        name='${pname}',
+        #packages=['someprogram'],
+        version='${version}',
+        install_requires=install_requires,
+      )
+      EOF
+    '';
   }
