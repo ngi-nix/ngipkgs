aea8ec1a4c2db86fd626bbb5b5ff9591c243f434
Alejandro Sanchez Medina <alejandrosanchzmedina@gmail.com>
2023-12-20 23:32:05 -0500

    taler libeufin: init at 0.9.3-dev.33

---
+++ pkgs/by-name/libeufin/deps/artifacts.nix
@@ -0,0 +1,130 @@
+[
+  {
+    url = "https://repo.maven.apache.org/maven2/io/ktor/ktor-server-call-logging-jvm/2.3.6/ktor-server-call-logging-jvm-2.3.6.jar";
+    sha256 = "sha256-2+0JE/yoKag8d8N2sfqRatXffpGn2pfvK/NINMgp4CQ=";
+  }
+  {
+    url = "https://repo.maven.apache.org/maven2/io/ktor/ktor-server-cors-jvm/2.3.6/ktor-server-cors-jvm-2.3.6.jar";
+    sha256 = "sha256-XLMYHGDhdKZp4D66H4F+IXpgvEtPvzfiTmK9fRVdsQw=";
+  }
+  {
+    url = "https://repo.maven.apache.org/maven2/io/ktor/ktor-server-content-negotiation-jvm/2.3.6/ktor-server-content-negotiation-jvm-2.3.6.jar";
+    sha256 = "sha256-8MKiz4jyiUBSJ0tN3ADbZk2pvDmRjuVxm8jVyepIcVk=";
+  }
+  {
+    url = "https://repo.maven.apache.org/maven2/io/ktor/ktor-server-status-pages-jvm/2.3.6/ktor-server-status-pages-jvm-2.3.6.jar";
+    sha256 = "sha256-t+5eJAjoPi5+SiErANGoUiYV4JPZaUSINI/mk3rez5g=";
+  }
+  {
+    url = "https://repo.maven.apache.org/maven2/io/ktor/ktor-server-test-host-jvm/2.3.6/ktor-server-test-host-jvm-2.3.6.jar";
+    sha256 = "sha256-hrtQZChQiW2Io+VGGv3CcmTMKn5DR9i/CCSum8brQrQ=";
+  }
+  {
+    url = "https://repo.maven.apache.org/maven2/io/ktor/ktor-server-host-common-jvm/2.3.6/ktor-server-host-common-jvm-2.3.6.jar";
+    sha256 = "sha256-q8FRK1MwK4/J3I2fDKGYZrXjjYduo9gVCF7uqvfal4s=";
+  }
+  {
+    url = "https://repo.maven.apache.org/maven2/io/ktor/ktor-server-core-jvm/2.3.6/ktor-server-core-jvm-2.3.6.jar";
+    sha256 = "sha256-UNG4jOCN5nOn4gvJ+LCU+MSi4lPTh8vjdiqIgwf8Uz0=";
+  }
+  {
+    url = "https://repo.maven.apache.org/maven2/io/ktor/ktor-server-netty-jvm/2.3.6/ktor-server-netty-jvm-2.3.6.jar";
+    sha256 = "sha256-mpZbiQdcyn15humQi8qUBvK/nySUIQRJTj8i1YahFMI=";
+  }
+  {
+    url = "https://repo.maven.apache.org/maven2/io/ktor/ktor-serialization-kotlinx-json-jvm/2.3.6/ktor-serialization-kotlinx-json-jvm-2.3.6.jar";
+    sha256 = "sha256-E9rhtl841ROH5xaro1/EQ/MDipJkpB5KmJI8L8/ssAQ=";
+  }
+  {
+    url = "https://repo.maven.apache.org/maven2/io/ktor/ktor-serialization-kotlinx-json/2.3.6/ktor-serialization-kotlinx-json-2.3.6.jar";
+    sha256 = "sha256-yV9zY97ZcooTqT7WWOl1IRtBhPC4+cF8Kbz3WqM+sBA=";
+  }
+  {
+    url = "https://repo.maven.apache.org/maven2/io/ktor/ktor-client-cio-jvm/2.3.6/ktor-client-cio-jvm-2.3.6.jar";
+    sha256 = "sha256-HRkrjyUR+7vBmmALl2AAW/JnF6I2FpB4ARqUREXL8Zk=";
+  }
+  {
+    url = "https://repo.maven.apache.org/maven2/io/ktor/ktor-client-core-jvm/2.3.6/ktor-client-core-jvm-2.3.6.jar";
+    sha256 = "sha256-F8RqVqEoxU3JiRYL1th7UQHoxkuAGpG05DMuwpmw68s=";
+  }
+  {
+    url = "https://repo.maven.apache.org/maven2/io/ktor/ktor-websocket-serialization-jvm/2.3.6/ktor-websocket-serialization-jvm-2.3.6.jar";
+    sha256 = "sha256-UHi9+14OjX/t0PDp0gAfWQkJfyGf6IbmHUoX+d57VG4=";
+  }
+  {
+    url = "https://repo.maven.apache.org/maven2/io/ktor/ktor-events-jvm/2.3.6/ktor-events-jvm-2.3.6.jar";
+    sha256 = "sha256-0RummxrxV7hz4nNHnrKRz4zY/R0Efi0z4w4+rO1RlFI=";
+  }
+  {
+    url = "https://repo.maven.apache.org/maven2/io/ktor/ktor-websockets-jvm/2.3.6/ktor-websockets-jvm-2.3.6.jar";
+    sha256 = "sha256-vTxhH8EYZc5QqoPKx3hApDVNqCevbT+GJHKrLseTYRU=";
+  }
+  {
+    url = "https://repo.maven.apache.org/maven2/io/ktor/ktor-http-cio-jvm/2.3.6/ktor-http-cio-jvm-2.3.6.jar";
+    sha256 = "sha256-rbVL7tt2UPJV+7wzxklpnxBBZAsKek3B2TJtGL+n4WE=";
+  }
+  {
+    url = "https://repo.maven.apache.org/maven2/io/ktor/ktor-network-tls-jvm/2.3.6/ktor-network-tls-jvm-2.3.6.jar";
+    sha256 = "sha256-elp6iWFcH9voEKN3BywJyRxa5x7+PfE4QaNTAFh04mU=";
+  }
+  {
+    url = "https://repo.maven.apache.org/maven2/io/ktor/ktor-http-jvm/2.3.6/ktor-http-jvm-2.3.6.jar";
+    sha256 = "sha256-N0avjwFDE7/7BwENwZ/xqRXW7Sb2ww+kpIiH8AGAe0Q=";
+  }
+  {
+    url = "https://repo.maven.apache.org/maven2/io/ktor/ktor-test-dispatcher-jvm/2.3.6/ktor-test-dispatcher-jvm-2.3.6.jar";
+    sha256 = "sha256-C8nubEVHNSgcVzgHL9bU5tq7nHmHqqokDwltUuZ4SDo=";
+  }
+  {
+    url = "https://repo.maven.apache.org/maven2/io/ktor/ktor-network-jvm/2.3.6/ktor-network-jvm-2.3.6.jar";
+    sha256 = "sha256-GzefLtjx6jj2U4yf0EaMY1Sv/Z70uLU38bAv+Ma9/zE=";
+  }
+  {
+    url = "https://repo.maven.apache.org/maven2/io/ktor/ktor-utils-jvm/2.3.6/ktor-utils-jvm-2.3.6.jar";
+    sha256 = "sha256-AI2ez6gSg9XvoGyO8zlrGSvLrBcETG+NbzGeMP4fyyM=";
+  }
+  {
+    url = "https://repo.maven.apache.org/maven2/io/ktor/ktor-io-jvm/2.3.6/ktor-io-jvm-2.3.6.jar";
+    sha256 = "sha256-782fRFGsgA6eQALr11j2Yn/0/oTLnoCoxiIgkE0kQCE=";
+  }
+  {
+    url = "https://repo.maven.apache.org/maven2/io/ktor/ktor-client-apache-jvm/2.3.6/ktor-client-apache-jvm-2.3.6.jar";
+    sha256 = "sha256-Ww2uKnJDUqSslSjsKYaoHdPMoJcCgJlZ20YsN8qm8vg=";
+  }
+  {
+    url = "https://repo.maven.apache.org/maven2/org/jetbrains/kotlin/kotlin-stdlib-jdk7/1.8.20/kotlin-stdlib-jdk7-1.8.20.jar";
+    sha256 = "sha256-rx7EDDuVGv3MDCoBc8e4F2PFKBwtW6+/CoVEokxdzAw=";
+  }
+  {
+    url = "https://repo.maven.apache.org/maven2/org/jetbrains/kotlin/kotlin-stdlib-jdk8/1.8.20/kotlin-stdlib-jdk8-1.8.20.jar";
+    sha256 = "sha256-45i2eXdiJxi/GP+ZtznH2doGDzP7RYouJSAyIcFq8BA=";
+  }
+  {
+    url = "https://repo.maven.apache.org/maven2/org/jetbrains/kotlinx/kotlinx-coroutines-slf4j/1.7.1/kotlinx-coroutines-slf4j-1.7.1.jar";
+    sha256 = "sha256-+7QMp+VaeAF/6siXGAvmT1YqLreBxqP2ybsv+I2SIj8=";
+  }
+  {
+    url = "https://repo.maven.apache.org/maven2/com/github/ajalt/clikt/clikt-jvm/4.2.1/clikt-jvm-4.2.1.jar";
+    sha256 = "sha256-799fw436KVOi4Zlc7LoNwoGqs1OF1ZX+BCFVFYxhZH8=";
+  }
+  {
+    url = "https://repo.maven.apache.org/maven2/com/github/ajalt/mordant/mordant-jvm/2.2.0/mordant-jvm-2.2.0.jar";
+    sha256 = "sha256-L6WeW4GvzHG2q2EosnZ2/8YR0KPBguvA5DezZmq1YgE=";
+  }
+  {
+    url = "https://repo.maven.apache.org/maven2/com/github/ajalt/colormath/colormath-jvm/3.3.1/colormath-jvm-3.3.1.jar";
+    sha256 = "sha256-JhMoNBXi4SZhaX3HKVra2g9g7Bfs/K8+8cTuD9t4iRM=";
+  }
+  {
+    url = "https://repo.maven.apache.org/maven2/org/jetbrains/markdown/0.5.2/markdown-0.5.2.jar";
+    sha256 = "sha256-HGsba7nOgwSLhkiZCsHTQnv6z+vI5Y4y7pBD3dDcOio=";
+  }
+  {
+    url = "https://repo.maven.apache.org/maven2/org/jetbrains/kotlin/kotlin-stdlib-common/1.9.10/kotlin-stdlib-common-1.9.10.jar";
+    sha256 = "sha256-zeM0G6GKK6JisLfPbFWyDJDo1DTkLJoT5qP3cNuWWog=";
+  }
+  {
+    url = "https://repo.maven.apache.org/maven2/org/jetbrains/kotlinx/atomicfu/0.19.0/atomicfu-0.19.0.jar";
+    sha256 = "sha256-BAJqx3zvZEde/enCOFVT8wRc2YoMmTS6u8wX293t9Xc=";
+  }
+]
