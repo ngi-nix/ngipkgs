e108f868123318053dc8973c57d4ddad4e0d2750
John Ericson <John.Ericson@Obsidian.Systems>
2024-01-25 02:12:32 +0100

    makemake/flake.lock: Update Hydra to CA PR
    https://github.com/NixOS/hydra/pull/875 is the next thing to test.
    
    Flake lock file updates:
    
    • Updated input 'hydra':
        'github:NixOS/hydra/d45e14fd4381d1476a606c20ef3573215067472c' (2024-01-24)
      → 'github:thufschmitt/hydra/002a77a954259ddb49fd262607ba44fb2c3b34a9' (2024-01-24)

--- ngi0/makemake/flake.nix
+++ ngi0/makemake/flake.nix
@@ -1,7 +1,7 @@
 {
   inputs.nixpkgs.follows = "hydra/nixpkgs";
   inputs.nix.follows = "hydra/nix";
-  inputs.hydra.url = "github:NixOS/hydra";
+  inputs.hydra.url = "github:thufschmitt/hydra/nix-ca";
 
   outputs = { self, nixpkgs, nix, hydra }: {
 
