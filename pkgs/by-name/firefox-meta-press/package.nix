bfabbd99efd29352d217136cd44ccbcc641a242e
Weijia Wang <9713184+wegank@users.noreply.github.com>
2024-04-11 22:48:09 +0200

    firefox-meta-press: init

---
+++ pkgs/by-name/firefox-meta-press/package.nix
@@ -0,0 +1,27 @@
+{
+  wrapFirefox,
+  firefox-devedition-unwrapped,
+  fetchFirefoxAddon,
+  meta-press,
+}: let
+  # workaround for https://github.com/NixOS/nixpkgs/issues/273509
+  # TODO: remove when nixpkgs rev is updated
+  firefox-unwrapped = (
+    firefox-devedition-unwrapped.overrideAttrs (previousAttrs: {
+      passthru =
+        previousAttrs.passthru
+        // {
+          requireSigning = false;
+          allowAddonSideload = true;
+        };
+    })
+  );
+in
+  wrapFirefox firefox-unwrapped {
+    nixExtensions = [
+      (fetchFirefoxAddon {
+        name = "meta-press-es"; # Has to be unique!
+        src = "${meta-press}/firefox_addon.xpi";
+      })
+    ];
+  }
