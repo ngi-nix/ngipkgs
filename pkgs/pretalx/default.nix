50fe2ab65c9bdcd60dc0422d559b041f8026e428
Lorenz Leutgeb <lorenz@leutgeb.xyz>
2024-04-12 16:21:15 +0200

    Revert "pretalx: restore main program to pretalx"
    This reverts commit 4476a4da73e548011076e91457f288f71319f3c3.

--- pkgs/pretalx/default.nix
+++ pkgs/pretalx/default.nix
@@ -5,20 +5,10 @@
 pretalx.overrideAttrs (
   finalAttrs: previousAttrs: {
     propagatedBuildInputs = previousAttrs.propagatedBuildInputs ++ withPlugins;
-    postInstall =
-      previousAttrs.postInstall
-      + ''
-        cp $out/bin/${previousAttrs.meta.mainProgram} $out/bin/${finalAttrs.meta.mainProgram}
-      '';
     passthru =
       previousAttrs.passthru
       // {
         PYTHONPATH = "${pretalx.python.pkgs.makePythonPath finalAttrs.propagatedBuildInputs}:${pretalx.outPath}/${pretalx.python.sitePackages}";
-      };
-    meta =
-      previousAttrs.meta
-      // {
-        mainProgram = "pretalx";
       };
   }
 )
