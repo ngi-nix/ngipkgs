9048d580f8adaaae4ccef4e267debf96818db8cd
Lorenz Leutgeb <lorenz.leutgeb@gmail.com>
2024-05-02 09:47:06 +0200

    vula: init at 0.2.2023112801 (#97)
    * vula: init at 0.1.16
    
    * highctidh: init at 1.0.2023121800
    
    * vula: 0.1.16 → 0.2.2023112801
    
    * vula: Patch and copy systemd unit files
    
    * vula: Add NixOS module
    
    * vula: Add NixOS test
    
    * vula: Also patch and copy DBus configuration
    
    * highctidh, vula: fix license
    
    ---------
    
    Co-authored-by: Weijia Wang <9713184+wegank@users.noreply.github.com>

---
+++ projects/Vula/test.nix
@@ -0,0 +1,42 @@
+{
+  sources,
+  pkgs,
+  ...
+}: {
+  name = "vula";
+
+  nodes = {
+    server = {config, ...}: {
+      imports = [
+        sources.modules.default
+        sources.modules."services.vula"
+      ];
+      services.vula.enable = true;
+    };
+  };
+
+  testScript = {nodes, ...}: ''
+    start_all()
+  '';
+
+  # NOTE: Below configuration is for "interactive" (=developing/debugging) only.
+  interactive.nodes = let
+    # Use kmscon <https://www.freedesktop.org/wiki/Software/kmscon/>
+    # to provide a slightly nicer console, and while we're at it,
+    # also use a nice font.
+    # With kmscon, we can for example zoom in/out using [Ctrl] + [+]
+    # and [Ctrl] + [-]
+    niceConsoleAndAutologin.services.kmscon = {
+      enable = true;
+      autologinUser = "root";
+      fonts = [
+        {
+          name = "Fira Code";
+          package = pkgs.fira-code;
+        }
+      ];
+    };
+  in {
+    server = niceConsoleAndAutologin;
+  };
+}
