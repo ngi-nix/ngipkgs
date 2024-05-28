05341fb155e62f638eece54f96afd548b96bb6d5
OPNA2608 <opna2608@protonmail.com>
2024-05-28 17:24:22 +0200

    peertube-plugin-hello-world: init at 0.0.22

---
+++ pkgs/by-name/peertube-plugin-hello-world/package.nix
@@ -0,0 +1,34 @@
+{
+  lib,
+  buildNpmPackage,
+  fetchFromGitLab,
+}:
+
+buildNpmPackage rec {
+  pname = "peertube-plugin-hello-world";
+  version = "0.0.22";
+
+  src = fetchFromGitLab {
+    domain = "framagit.org";
+    owner = "framasoft";
+    repo = "peertube/official-plugins";
+    rev = "fa9005ab1bab93e41e10bb1be3dc4837bd6bbc47";
+    hash = "sha256-d1DGrmRCavc1a6r3UvT7AzcLkFMJktwDKpEjgx7RJAI=";
+  };
+
+  sourceRoot = "${src.name}/peertube-plugin-hello-world";
+
+  npmDepsHash = "sha256-Y6bq2w5ykqLMY9eDTNKL3DMkoOx+imV7OCw2Hy961Tk=";
+
+  dontNpmBuild = true;
+
+  # TODO: passthru.updateScript? there are no tags, versions come as file changes in subprojects
+
+  meta = {
+    description = "Hello world PeerTube plugin example";
+    homepage = "https://framagit.org/framasoft/peertube/official-plugins/tree/master/peertube-plugin-hello-world";
+    license = lib.licenses.agpl3Only;
+    maintainers = with lib.maintainers; [ OPNA2608 ];
+    platforms = lib.platforms.unix;
+  };
+}
