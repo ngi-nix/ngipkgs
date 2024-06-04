5d70ecabbe5140c85a58298c7a38f49f335a5793
eljamm <fedi.jamoussi@protonmail.ch>
2024-06-04 09:33:59 +0100

    depolymerization: init at 0-unstable-2024-03-18

---
+++ pkgs/by-name/depolymerization/package.nix
@@ -0,0 +1,34 @@
+{
+  lib,
+  rustPlatform,
+  fetchgit,
+  stdenv,
+  darwin,
+}:
+rustPlatform.buildRustPackage {
+  pname = "depolymerization";
+  version = "0-unstable-2024-03-18";
+
+  src = fetchgit {
+    url = "https://git.taler.net/depolymerization.git/";
+    rev = "80ded0febe5acbfa64cef3d84846b4a1300ea97f";
+    hash = "sha256-wb1IxaiM+uGEa9l8Iz0fDluIWZgVMnT8BUZ/R5M3dcU=";
+  };
+
+  cargoHash = "sha256-ePDr47U50yXMC2C9KXTqWmmtDzONXm0yTPuxTNEidXI=";
+
+  patches = [./0001-Fix-status-test-docs.patch];
+
+  buildInputs = lib.optionals stdenv.isDarwin [
+    darwin.apple_sdk.frameworks.CoreFoundation
+    darwin.apple_sdk.frameworks.Security
+    darwin.apple_sdk.frameworks.SystemConfiguration
+  ];
+
+  meta = with lib; {
+    description = "Wire gateway for Bitcoin/Ethereum";
+    homepage = "https://git.taler.net/depolymerization.git/";
+    license = licenses.agpl3Only;
+    maintainers = with maintainers; [];
+  };
+}
