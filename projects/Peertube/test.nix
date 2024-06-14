a22b7ee48f500f1713afbdcb7183c5a336d12a27
OPNA2608 <opna2608@protonmail.com>
2024-06-14 19:10:37 +0200

    peertube-plugin-auto-mute: init at 0.0.6

--- projects/Peertube/test.nix
+++ projects/Peertube/test.nix
@@ -16,6 +16,7 @@ let
     peertube-plugin-auth-openid-connect
     peertube-plugin-auth-saml2
     peertube-plugin-auto-block-videos
+    peertube-plugin-auto-mute
     peertube-plugin-hello-world
     peertube-theme-dark
     peertube-plugin-livechat
