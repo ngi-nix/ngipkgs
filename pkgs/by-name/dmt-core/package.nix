bec5c90ee0686a6ee3cbf3de69f4ef432513ebc5
Jason Odoom <jasonodoom@gmail.com>
2024-06-27 20:05:38 -0700

    dmt-core: init at 2.1.0

--- pkgs/by-name/dmt-core/package.nix
+++ pkgs/by-name/dmt-core/package.nix
@@ -1,71 +1,73 @@
-{ lib, buildPythonApplication, fetchurl, fetchPypi, pythonPackages }:
+{ lib, fetchPypi, fetchurl, python3Packages }:
 
 let
-  reprint = buildPythonApplication rec {
+  inherit (python3Packages) buildPythonPackage;
+
+  reprint = buildPythonPackage rec {
     pname = "reprint";
     version = "0.6.0";
     src = fetchurl {
       url =
         "https://files.pythonhosted.org/packages/ab/30/a742e69e37d4148dfa89acb6aa2a82ea298d1dfea02c6e9b2a43d4ed1065/reprint-0.6.0-py2.py3-none-any.whl";
-      sha256 = "DrxNwhrvyBAgXz/MIXg+qRu5YpKoUAZjSC6GSYm/9U0=";
+      hash = "sha256-DrxNwhrvyBAgXz/MIXg+qRu5YpKoUAZjSC6GSYm/9U0=";
     };
     format = "wheel";
   };
 
-  verilogae = buildPythonApplication rec {
+  verilogae = buildPythonPackage rec {
     pname = "verilogae";
     version = "1.0.0";
     src = fetchurl {
       url =
         "https://files.pythonhosted.org/packages/aa/a6/9daea00745844faba44c2917c66838adfc315269f38518ecca49e6eb5fb5/verilogae-1.0.0-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl";
-      sha256 = "QsEVq/4c44xCkovOhXOj8Zh6rp4Ipq+NGjbTW25Fd6E=";
+      hash = "sha256-QsEVq/4c44xCkovOhXOj8Zh6rp4Ipq+NGjbTW25Fd6E=";
     };
     format = "wheel";
   };
 
-in buildPythonApplication rec {
+in buildPythonPackage rec {
   pname = "DMT_core";
   version = "2.1.0";
-  pyproject = true;
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "489E+uNn4NgyCwxsUMEPH/1ZuM+5uNq4zx8F88rkHMU=";
+    hash = "sha256-489E+uNn4NgyCwxsUMEPH/1ZuM+5uNq4zx8F88rkHMU=";
   };
+  doCheck = false;
 
   propagatedBuildInputs = [
-    pythonPackages.joblib
-    pythonPackages.pint
-    pythonPackages.pyyaml
-    pythonPackages.more-itertools
-    pythonPackages.semver
-    pythonPackages.h5py
-    pythonPackages.cycler
-    pythonPackages.colormath
-    pythonPackages.colorama
-    pythonPackages.scipy
-    pythonPackages.scikit-rf
-    pythonPackages.packaging
-    pythonPackages.pandas
-    pythonPackages.pyqtgraph
-    pythonPackages.matplotlib
-    pythonPackages.pyarrow
-    pythonPackages.requests
+    python3Packages.colorama
+    python3Packages.colormath
+    python3Packages.cycler
+    python3Packages.h5py
+    python3Packages.joblib
+    python3Packages.matplotlib
+    python3Packages.more-itertools
+    python3Packages.packaging
+    python3Packages.pandas
+    python3Packages.pint
+    python3Packages.pyarrow
+    python3Packages.pyqtgraph
+    python3Packages.pyyaml
+    python3Packages.requests
+    python3Packages.scikit-rf
+    python3Packages.scipy
+    python3Packages.semver
+    python3Packages.setuptools
     reprint
     verilogae
-    pythonPackages.setuptools
   ];
 
-  nativeCheckInputs = [ pythonPackages.pytest ];
+  nativeCheckInputs = [ python3Packages.pytest ];
 
-  meta = with lib; {
+  meta = {
     changelog =
       "https://gitlab.com/dmt-development/dmt-core/-/blob/Version_${version}/CHANGELOG?ref_type=tags";
     description =
-      "DeviceModelingToolkit (DMT) is a Python tool targeted at helping modeling engineers extract model parameters, run circuit and TCAD simulations and automate their infrastructure.";
+      "Python tool to help modeling engineers extract model parameters, run circuit and TCAD simulations and automate infrastructure";
     homepage = "https://gitlab.com/dmt-development/dmt-core";
-    license = licenses.gpl3Plus;
-    maintainers = with maintainers; [ jasonodoom ];
-    platforms = platforms.linux;
+    license = lib.licenses.gpl3Plus;
+    maintainers = with lib.maintainers; [ jasonodoom ];
+    platforms = lib.platforms.linux;
   };
 }
