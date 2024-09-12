be1be6999dc4517bc40779f9882167b1ea079364
Weijia Wang <9713184+wegank@users.noreply.github.com>
2024-09-12 15:31:28 +0200

    lillydap: 0.9.2 -> 1.0.1

--- pkgs/by-name/lillydap/package.nix
+++ pkgs/by-name/lillydap/package.nix
@@ -9,13 +9,13 @@
 }:
 stdenv.mkDerivation rec {
   pname = "lillydap";
-  version = "0.9.2";
+  version = "1.0.1";
 
   src = fetchFromGitLab {
     owner = "arpa2";
     repo = "lillydap";
     rev = "v${version}";
-    hash = "sha256-L2zmitXezGzDZXLDxohU3DTuHE18KUZEMg98ui2AF+c=";
+    hash = "sha256-SQuvu1A9Iq/fKthfYyVQGWFuyHYnhIry/wvnwgdMKHY=";
   };
 
   nativeBuildInputs = [cmake arpa2cm arpa2common quickder gperf];
