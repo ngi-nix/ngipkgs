92579c6562c54dc4689ae0aee9500d4c0a96f4a6
Cleeyv <cleeyv@riseup.net>
2024-09-17 12:01:48 -0400

    Copy deployment files from the nix store

--- pkgs/by-name/deploy-ngipkgs/package.nix
+++ pkgs/by-name/deploy-ngipkgs/package.nix
@@ -2,15 +2,29 @@
   writeShellApplication,
   git,
 }:
+let
+  repo = fetchGit {
+    url = "https://github.com/ngi-nix/ngipkgs/howto-docs";
+  };
+  storePath = builtins.storePath repo;
+in
 writeShellApplication {
   name = "deploy-ngipkgs";
   runtimeInputs = [ git ];
   text = ''
-    git clone -b howto-docs --no-checkout --depth=1 https://github.com/ngi-nix/ngipkgs.git deploy-ngipkgs && # clone an empty version of ngipkgs to a dir called deploy-ngipkgs
-    cd deploy-ngipkgs &&
-    git checkout howto-docs -- projects && # checkout only the projects directory of ngipkgs
-    cp -a projects/. . && # copy contents of the projects directory into the root of deploy-ngipkgs
-    rm -rf .git projects default.nix && # cleanup
-    git init && git add . # create a new git repo for tracking config changes during deployment
+    # Create a new directory for the deployment repo
+    mkdir ./deploy-ngipkgs
+
+    # Copy projects directory from repo already downloaded to the local store
+    cp -r ${storePath}/projects/* ./deploy-ngipkgs
+
+    # Cleanup project files that are not used for deployment
+    cd ./deploy-ngipkgs
+    find . -type f \( -name 'default.nix' -o -name 'service.nix' -o -name 'container.nix' -o -name 'module.nix' -o -name 'nss*' -o -name 'dbus*' \) -exec rm {} \;
+    find . -name 'test*' -exec rm -r {} \;
+    find . -type d -empty -delete
+
+    # Create a new git repo for tracking config changes during deployment
+    git init && git add .
   '';
 }
