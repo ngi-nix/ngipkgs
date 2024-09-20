421cf19e0c4f79ae134ab1c7105da4c590a1bb22
Weijia Wang <9713184+wegank@users.noreply.github.com>
2024-09-20 02:18:20 +0200

    projects/PeerTube: normalize service name

--- projects/PeerTube/default.nix
+++ projects/PeerTube/default.nix
@@ -7,7 +7,7 @@
     inherit (pkgs) peertube-plugin-akismet peertube-plugin-auth-ldap peertube-plugin-auth-openid-connect peertube-plugin-auth-saml2 peertube-plugin-auto-block-videos peertube-plugin-auto-mute peertube-plugin-hello-world peertube-plugin-logo-framasoft peertube-plugin-matomo peertube-plugin-privacy-remover peertube-plugin-transcoding-custom-quality peertube-plugin-transcoding-profile-debug peertube-plugin-video-annotation peertube-theme-background-red peertube-theme-dark peertube-theme-framasoft peertube-plugin-livechat;
   };
   nixos = {
-    modules.services.peertube.plugins = ./module.nix;
+    modules.services.peertube = ./module.nix;
     tests.peertube-plugins = import ./test.nix args;
     examples = {
       base = {
