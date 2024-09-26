a4f6c79711609ad73e6f184707ce7d7372800709
Weijia Wang <9713184+wegank@users.noreply.github.com>
2024-09-26 02:52:00 +0200

    quicksasl: 0.11.0 -> 0.13.2

--- pkgs/by-name/quicksasl/package.nix
+++ pkgs/by-name/quicksasl/package.nix
@@ -14,13 +14,13 @@
 }:
 stdenv.mkDerivation rec {
   pname = "quicksasl";
-  version = "0.11.0";
+  version = "0.13.2";
 
   src = fetchFromGitLab {
     owner = "arpa2";
     repo = "quick-sasl";
     rev = "v${version}";
-    hash = "sha256-z9kgKssuXq8qae9dTLP5REzkp1C4/jnJr7ydOavPWKM=";
+    hash = "sha256-kMKZRromm/hb9PZwvWAzmJorSqTB8xMIbWASfSjajiQ=";
   };
 
   nativeBuildInputs = [cmake arpa2cm arpa2common libkrb5 quickmem cyrus_sasl quickder libev e2fsprogs pkg-config];
