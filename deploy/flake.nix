43150a5580e88278882339ea9f5f53a2f6420f6a
Cleeyv <71959829+cleeyv@users.noreply.github.com>
2024-10-02 06:28:43 -0400

    Document how to deploy NGIpkgs services (#369)
    * Init deploy dir with README docs
    
    Co-authored-by: Valentin Gagarin <valentin@gagarin.work>

---
+++ deploy/flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "An example deployment of NGIpkgs software to a local VM";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    ngipkgs.url = "github:ngi-nix/ngipkgs";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    ngipkgs,
+  }: {
+    nixosConfigurations.myMachine = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./configuration.nix
+        ngipkgs.nixosModules.default
+
+        ### VULA
+        # ngipkgs.nixosModules."services.vula"
+        # ../projects/Vula/example-simple.nix
+        ###
+        ### KBIN
+        # ngipkgs.nixosModules."services.kbin"
+        # ../projects/Kbin/example.nix
+        ###
+        ### PEERTUBE
+        # ngipkgs.nixosModules."services.peertube"
+        # ../projects/PeerTube/example.nix
+        ###
+        ### ATOMICDATA
+        # ngipkgs.nixosModules."services.atomic-server"
+        # ../projects/AtomicData/example.nix
+        ###
+      ];
+    };
+  };
+}
