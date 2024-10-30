5003952b5624de72dc05808d6236100ad45a68f8
Valentin Gagarin <valentin@gagarin.work>
2024-10-30 16:21:10 +0100

    add flarum service

--- projects/Flarum/default.nix
+++ projects/Flarum/default.nix
@@ -1,3 +1,8 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  sources,
+  ...
+}: {
   packages = {inherit (pkgs) flarum;};
+  nixos.modules.services.flarum = "${sources.inputs.nixpkgs}/nixos/modules/services/web-apps/flarum.nix";
 }
