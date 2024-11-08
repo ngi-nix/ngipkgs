30c7a7c5a18cdace8c85504b533543fef5474634
OPNA2608 <opna2608@protonmail.com>
2024-11-08 18:06:26 +0100

    libervia-templates: 0.8.0-unstable-2024-06-06 -> 0.8.0-unstable-2024-10-26

--- pkgs/by-name/libervia-templates/package.nix
+++ pkgs/by-name/libervia-templates/package.nix
@@ -5,13 +5,13 @@
 }:
 python3Packages.buildPythonPackage rec {
   pname = "libervia-templates";
-  version = "0.8.0-unstable-2024-06-06";
+  version = "0.8.0-unstable-2024-10-26";
   pyproject = true;
 
   src = fetchhg {
     url = "https://repos.goffi.org/libervia-templates";
-    rev = "e7152fc8a81f7da8f4a17f0a684e6c27051399b9";
-    hash = "sha256-FFFpPfEfbzPmoOHafurv18ID0PsGVUaG2bHjAKYNXfQ=";
+    rev = "2bbcb7da56bcaa213c709dd0cb9d5d5456e699d4";
+    hash = "sha256-DbP8VzCF2hOBf6F1saXoeWYHBT1vUFo4crx9s29S/u8=";
   };
 
   strictDeps = true;
