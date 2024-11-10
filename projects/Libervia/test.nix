22987980404f250e241bb2aa55b39b5e7f7ff3bf
OPNA2608 <opna2608@protonmail.com>
2024-11-10 02:03:10 +0100

    projects/Libervia/test: Test Kivy desktop too

--- projects/Libervia/test.nix
+++ projects/Libervia/test.nix
@@ -144,6 +144,8 @@ in {
         # Small script to register our test user in prosody
         systemPackages = [
           setup-initial-libervia-user
+          pkgs.firefox
+          pkgs.xdotool
         ];
       };
     };
@@ -167,6 +169,11 @@ in {
         # Now check if we can see it
         machine.wait_for_text("alice")
 
+    # Next workspace
+    def next_workspace():
+        machine.send_key("ctrl-alt-right")
+        machine.sleep(2) # Make sure we're actually on the next workspace
+
     start_all()
 
     machine.wait_for_x()
@@ -183,32 +190,57 @@ in {
     machine.succeed("sudo -su alice tail -f /home/alice/backend.log >&2 &")
     machine.succeed("sudo -su alice touch /home/alice/frontend.log")
     machine.succeed("sudo -su alice tail -f /home/alice/frontend.log >&2 &")
+    machine.succeed("sudo -su alice touch /home/alice/desktop.log")
+    machine.succeed("sudo -su alice tail -f /home/alice/desktop.log >&2 &")
 
-    # Start libervia backend in foreground, so we can read logs
-    spawn_terminal()
-    machine.send_chars("libervia-backend fg | tee -a ~/backend.log\n")
-    machine.wait_for_console_text("Backend is ready")
+    with subtest("libervia-backend works"):
+        # Start libervia backend in foreground, so we can read logs
+        spawn_terminal()
+        machine.send_chars("libervia-backend fg | tee -a ~/backend.log\n")
+        machine.wait_for_console_text("Backend is ready")
 
-    # Next workspace
-    machine.send_key("ctrl-alt-right")
-    machine.sleep(2) # Make sure we're actually on the second workspace
+    next_workspace()
 
-    # Register profile with setup XMPP account in Libervia
-    spawn_terminal()
-    machine.send_chars("libervia-cli profile create -j ${xmppId} -x ${xmppPassword} alice | tee -a ~/frontend.log\n")
-    machine.wait_for_console_text(r"\[${xmppUser}\] Profile session started")
+    with subtest("libervia CLI works"):
+        # Register profile with setup XMPP account in Libervia
+        spawn_terminal()
+        machine.send_chars("libervia-cli profile create -j ${xmppId} -x ${xmppPassword} alice | tee -a ~/frontend.log\n")
+        machine.wait_for_console_text(r"\[${xmppUser}\] Profile session started")
 
-    # Log in
-    machine.send_chars("libervia-cli profile connect -p alice -c | tee -a ~/frontend.log\n")
-    machine.wait_for_console_text("Data consistency ensured/restored.")
+        # Log in
+        machine.send_chars("libervia-cli profile connect -p alice -c | tee -a ~/frontend.log\n")
+        machine.wait_for_console_text("Data consistency ensured/restored.")
 
-    # Send something
-    machine.send_chars("libervia-cli message send ${xmppId} </etc/xmppMessage | tee -a ~/frontend.log\n")
-    machine.wait_for_console_text("${xmppUser} has 1 items")
+        # Send something
+        machine.send_chars("libervia-cli message send ${xmppId} </etc/xmppMessage | tee -a ~/frontend.log\n")
+        machine.wait_for_console_text("${xmppUser} has 1 items")
 
-    # Check if we can query the message
-    machine.send_chars("libervia-cli message mam | tee -a ~/frontend.log\n")
-    machine.wait_for_console_text("${xmppUser}> ${xmppMessage}") # first log, us sending the message to ourself
-    machine.wait_for_console_text("${xmppUser}> ${xmppMessage}") # second log, us receicing the message from ourself
+        # Check if we can query the message
+        machine.send_chars("libervia-cli message mam | tee -a ~/frontend.log\n")
+        machine.wait_for_console_text("${xmppUser}> ${xmppMessage}") # first log, us sending the message to ourself
+        machine.wait_for_console_text("${xmppUser}> ${xmppMessage}") # second log, us receicing the message from ourself
+
+    next_workspace()
+
+    with subtest("libervia-desktop-kivy works"):
+        # Start it
+        spawn_terminal()
+        machine.send_chars("libervia-desktop-kivy 2>&1 | tee -a ~/desktop.log\n")
+        machine.wait_for_text("Select a profile")
+        machine.send_key("alt-f10")
+
+        # Log in as alice
+        machine.succeed("sudo -su alice xdotool mousemove 518 163 click 1")
+        machine.sleep(2)
+        machine.succeed("sudo -su alice xdotool mousemove 509 721 click 1")
+        machine.wait_for_text("chat")
+
+        # Enter chat
+        machine.succeed("sudo -su alice xdotool mousemove 493 199 click 1")
+        machine.wait_for_text("Your contacts")
+
+        # Open conversation with ourselves
+        machine.succeed("sudo -su alice xdotool mousemove 80 148 click 1")
+        machine.wait_for_text("${xmppMessage}")
   '';
 }
