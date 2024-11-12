f2180a3301e7f102a5dcb907602b4d79102f9c38
wamirez <wamirez@protonmail.com>
2024-11-12 14:55:22 -0400

    Add weblate's nixpkgs and nixos refs

--- projects/Weblate/default.nix
+++ projects/Weblate/default.nix
@@ -3,7 +3,11 @@
   lib,
   sources,
 } @ args: {
+  packages = {
+    inherit (pkgs) weblate;
+  };
   nixos = {
+    modules.services.weblate = "${sources.inputs.nixpkgs}/nixos/modules/services/web-apps/weblate.nix";
     examples.base = {
       description = ''
         Basic example for Weblate, with manual secrets deployment and automatic Nginx/ACME setup.
