21a3e7b84faf880b99b1cadf67c37d6806a68c73
wamirez <wamirez@protonmail.com>
2024-11-14 14:50:46 -0400

    Add scion's nixpkgs and nixos refs
    the test fails with some inscrutable PKI error

---
+++ projects/Scion/default.nix
@@ -0,0 +1,23 @@
+{
+  pkgs,
+  lib,
+  sources,
+} @ args: {
+  packages = {
+    inherit
+      (pkgs)
+      scion
+      scion-apps
+      scion-bootstrapper
+      ioq3-scion
+      pan-bindings
+      ;
+  };
+  nixos = {
+    modules.services.scion = "${sources.inputs.nixpkgs}/nixos/modules/services/networking/scion/scion.nix";
+    # TODO: unbreak
+    # tests.scion = "${sources.inputs.nixpkgs}/nixos/tests/scion/freestanding-deployment/default.nix";
+    tests = null;
+    examples = null;
+  };
+}
