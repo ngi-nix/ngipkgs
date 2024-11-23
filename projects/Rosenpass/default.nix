444460a1a9dfbe5295ac2e769aa5500e5b5fa2d5
wamirez <wamirez@protonmail.com>
2024-11-23 16:50:45 -0400

    Add Rosenpass NixOS services and missing example

--- projects/Rosenpass/default.nix
+++ projects/Rosenpass/default.nix
@@ -3,8 +3,12 @@
   lib,
   sources,
 } @ args: {
-  packages = {inherit (pkgs) rosenpass rosenpass-tools;};
+  packages = {
+    inherit (pkgs) rosenpass rosenpass-tools;
+  };
   nixos = {
+    modules.services.rosenpass = "${sources.inputs.nixpkgs}/nixos/modules/services/networking/rosenpass.nix";
     tests.rosenpass = import ./tests args;
+    examples = null;
   };
 }
