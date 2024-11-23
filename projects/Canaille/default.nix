6d154a83dc991f735100eaf2792a8bc16a3753ce
wamirez <wamirez@protonmail.com>
2024-11-23 15:49:08 -0400

    Add Canaille Nixpkgs and NixOS refs

---
+++ projects/Canaille/default.nix
@@ -0,0 +1,14 @@
+{
+  pkgs,
+  lib,
+  sources,
+} @ args: {
+  packages = {
+    inherit (pkgs) canaille;
+  };
+  nixos = {
+    modules.services.canaille = "${sources.inputs.nixpkgs}/nixos/modules/services/security/canaille.nix";
+    tests.canaille = "${sources.inputs.nixpkgs}/nixos/tests/canaille.nix";
+    examples = null;
+  };
+}
