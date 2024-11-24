881c90072b77b92024ae5e874daa7483c33923ba
Daniel Ramírez <wamirez@protonmail.com>
2024-11-24 14:53:21 -0400

    Add Nitrokey-3 Nixpkgs refs (#433)

---
+++ projects/Nitrokey-3/default.nix
@@ -0,0 +1,13 @@
+{pkgs, ...} @ args: {
+  packages = {
+    inherit
+      (pkgs)
+      # TODO: nitrokey-3-firmware
+      nitrokey-fido2-firmware
+      nitrokey-pro-firmware
+      nitrokey-start-firmware
+      nitrokey-storage-firmware
+      nitrokey-trng-rs232-firmware
+      ;
+  };
+}
