3bda88eb10935dfb99450153917ef17a6851ae64
Daniel Ramírez <wamirez@protonmail.com>
2024-11-25 06:07:13 -0400

    Add Pixelfed nixpkgs and NixOS refs (#414)

---
+++ projects/Pixelfed/default.nix
@@ -0,0 +1,12 @@
+{
+  pkgs,
+  lib,
+  sources,
+} @ args: {
+  packages = {inherit (pkgs) pixelfed;};
+  nixos = {
+    modules.services.pixelfed = "${sources.inputs.nixpkgs}/nixos/modules/services/web-apps/pixelfed.nix";
+    tests.pixelfed = "${sources.inputs.nixpkgs}/nixos/tests/web-apps/pixelfed/standard.nix";
+    examples = null;
+  };
+}
