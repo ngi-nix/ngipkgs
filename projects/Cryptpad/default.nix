1999ec3051ce9e1cf1fdefa6a191a8ae74bf73cf
Valentin Gagarin <valentin@gagarin.work>
2024-11-25 23:58:30 +0100

    fix cryptpad tests

--- projects/Cryptpad/default.nix
+++ projects/Cryptpad/default.nix
@@ -6,7 +6,7 @@
   packages = {inherit (pkgs) cryptpad;};
   nixos = {
     modules.services.cryptpad = "${sources.inputs.nixpkgs}/nixos/modules/services/web-apps/cryptpad.nix";
-    tests.cryptpad = "${sources.inputs.nixpkgs}/nixos/tests/cryptpad.nix";
+    tests.cryptpad = import "${sources.inputs.nixpkgs}/nixos/tests/cryptpad.nix" args;
     examples = null;
   };
 }
