ff7482c1b9af34ab525fbbe6ab3e1560b9391253
Valentin Gagarin <valentin@gagarin.work>
2024-11-25 11:27:45 +0100

    move everything to gnunet

--- projects/GNUnet-Messenger/default.nix
+++
@@ -1,5 +0,0 @@
-{pkgs, ...} @ args: {
-  packages = {
-    inherit (pkgs) gnunet-messenger-cli;
-  };
-}
---
+++ projects/gnunet/default.nix
@@ -0,0 +1,20 @@
+{
+  pkgs,
+  sources,
+  ...
+} @ args: {
+  packages = {
+    inherit
+      (pkgs)
+      gnunet
+      gnunet-gtk
+      gnunet-messenger-cli
+      libgnurl
+      ;
+  };
+  nixos = {
+    modules.services.gnunet = "${sources.inputs.nixpkgs}/nixos/modules/services/networking/gnunet.nix";
+    tests = null;
+    examples = null;
+  };
+}
