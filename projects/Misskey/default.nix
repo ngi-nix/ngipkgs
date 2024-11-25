712d6a25a925231076cbbbb6116c7643226c7a6e
Daniel Ramírez <wamirez@protonmail.com>
2024-11-25 17:44:12 -0400

    Add Misskey Nixpkgs and NixOS refs (#434)

---
+++ projects/Misskey/default.nix
@@ -0,0 +1,14 @@
+{
+  pkgs,
+  lib,
+  sources,
+} @ args: {
+  packages = {
+    inherit (pkgs) misskey;
+  };
+  nixos = {
+    modules.services.misskey = "${sources.inputs.nixpkgs}/nixos/modules/services/web-apps/misskey.nix";
+    tests.misskey = "${sources.inputs.nixpkgs}/nixos/tests/misskey.nix";
+    examples = null;
+  };
+}
