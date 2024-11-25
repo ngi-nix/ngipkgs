72776e69dd5767f9da0d0c03cf81d5e641e85abb
Daniel Ramírez <wamirez@protonmail.com>
2024-11-25 19:06:24 -0400

    Add wireguard-rs ngipkgs ref (#431)

---
+++ projects/wireguard/default.nix
@@ -0,0 +1,23 @@
+{
+  pkgs,
+  lib,
+  sources,
+  ...
+} @ args: {
+  packages = {
+    inherit (pkgs) wireguard-rs wireguard-go wireguard-tools;
+  };
+  nixos = {
+    modules.services.wireguard = "${sources.inputs.nixpkgs}/nixos/modules/services/networking/wireguard.nix";
+    tests =
+      lib.foldl'
+      (acc: test: acc // {${test} = "${sources.inputs.nixpkgs}/nixos/tests/wireguard/${test}.nix";})
+      {} [
+        "basic"
+        "namespaces"
+        "wg-quick"
+        "generated"
+      ];
+    examples = null;
+  };
+}
