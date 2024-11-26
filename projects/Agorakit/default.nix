e63105fdaef4840fd6231097cf0bbeb952c1e2cc
Valentin Gagarin <valentin@gagarin.work>
2024-11-26 01:35:02 +0100

    fix agorakit tests

--- projects/Agorakit/default.nix
+++ projects/Agorakit/default.nix
@@ -8,7 +8,7 @@
   };
   nixos = {
     modules.services.agorakit = "${sources.inputs.nixpkgs}/nixos/modules/services/web-apps/agorakit.nix";
-    tests.basic = "${sources.inputs.nixpkgs}/nixos/tests/web-apps/agorakit.nix";
+    tests.basic = import "${sources.inputs.nixpkgs}/nixos/tests/web-apps/agorakit.nix" args;
     examples = null;
   };
 }
