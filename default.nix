1423678b788e75450486b8f36a34a0ad6b413e2c
Valentin Gagarin <valentin@gagarin.work>
2024-11-28 00:05:07 +0100

    replace some flake outputs with things computed in default.nix (#451)
    this is much easier to interact with using `nix repl -f .`

--- default.nix
+++ default.nix
@@ -2,12 +2,46 @@
   sources ? (import ./flake-compat.nix {root = ./.;}).inputs,
   system ? builtins.currentSystem,
   pkgs ?
-    import sources.nixpkgs {
+    import sources.nixpkgs
+    {
       config = {};
       overlays = [];
       inherit system;
     },
-}: {
+  lib ? import "${sources.nixpkgs}/lib",
+}: let
+  dream2nix = import sources.dream2nix;
+  sops-nix = import "${sources.sops-nix}/modules/sops";
+in rec {
+  overlays.default = final: prev:
+    import ./pkgs/by-name {
+      pkgs = prev;
+      inherit lib dream2nix;
+    };
+
+  examples = with lib; mapAttrs (_: project: mapAttrs (_: example: example.path) project.nixos.examples) projects;
+
+  nixos-modules = with lib;
+    foldl recursiveUpdate {} (attrValues (mapAttrs (_: project: project.nixos.modules) projects))
+    // {
+      # Allow using packages from `ngipkgs` to be used alongside regular `pkgs`
+      ngipkgs = {...}: {
+        nixpkgs.overlays = [overlays.default];
+      };
+    };
+
+  ngipkgs = import ./pkgs/by-name {inherit pkgs lib dream2nix;};
+
+  projects = import ./projects {
+    inherit lib;
+    pkgs = pkgs // ngipkgs;
+    sources = {
+      inputs = sources;
+      modules = nixos-modules // {inherit sops-nix;};
+      inherit examples;
+    };
+  };
+
   shell = pkgs.mkShellNoCC {
     packages = [];
   };
