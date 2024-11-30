8396c3a282b5b44375c7bf32c2bf6aba24f7aece
Valentin Gagarin <valentin@gagarin.work>
2024-11-30 02:17:45 +0100

    set a temporary null value for Agorakit tests

--- projects/Agorakit/default.nix
+++ projects/Agorakit/default.nix
@@ -10,6 +10,7 @@
     modules.services.agorakit = "${sources.inputs.nixpkgs}/nixos/modules/services/web-apps/agorakit.nix";
     # TODO: wait for https://github.com/NixOS/nixpkgs/pull/359164 to land in `nixpkgs-unstable`
     # tests.basic = import "${sources.inputs.nixpkgs}/nixos/tests/web-apps/agorakit.nix" args;
+    tests = null;
     examples = null;
   };
 }
