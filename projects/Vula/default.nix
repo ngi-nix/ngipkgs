74d42f70c1fdcda3f7ea805115ee72ec5982c70e
kye <kyesarri@gmail.com>
2025-02-25 08:53:29 +1100

    25.02.25 - adminGroup to operatorsGroup

--- projects/Vula/default.nix
+++ projects/Vula/default.nix
@@ -1,6 +1,5 @@
-{ pkgs, ... }@args:
-{
-  packages = { inherit (pkgs) vula; };
+{pkgs, ...} @ args: {
+  packages = {inherit (pkgs) vula;};
   nixos.modules.services.vula = ./service.nix;
   nixos.tests.test = import ./test.nix args;
   nixos.examples.simple = {
@@ -8,7 +7,7 @@
     description = ''
       Simple configuration for Vula. Vula nodes will automatically discover each other on networks that support [multicast DNS](https://en.wikipedia.org/wiki/Multicast_DNS) (mDNS).
 
-      Add users to the group defined in `config.services.vula.adminGroup` to grant them permissions to manage Vula through the `vula` command.
+      Add users to the group defined in `config.services.vula.operatorsGroup` to grant them permissions to manage Vula through the `vula` command.
     '';
   };
 }
