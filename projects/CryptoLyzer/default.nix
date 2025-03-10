dbbb5abafc71842ea314e7ae6bf095a10705b523
Valentin Gagarin <valentin@gagarin.work>
2025-03-10 12:34:53 +0100

    add CryptoLyzer (#555)

---
+++ projects/CryptoLyzer/default.nix
@@ -0,0 +1,59 @@
+{
+  lib,
+  pkgs,
+  sources,
+}@args:
+{
+  metadata = {
+    summary = ''
+      CryptoLyzer is a cybersecurity tool that can analyse cryptographic settings of clients and servers for different protocols, and test endpoints against a set of known vulnerabilities.
+    '';
+    subgrants = [
+      "CryptoLyzer"
+    ];
+  };
+  # TODO: add a type for pure Nixpkgs stuff
+  # nixpkgs.python.extensions.packages = {
+  #   cryptolyzer.meta = {
+  #     TODO broken = true;
+  #     links = {
+  #       python-api = {
+  #         text = "Python API documentation";
+  #         url = "https://cryptolyzer.readthedocs.io/en/latest/api/";
+  #       };
+  #     };
+  #   };
+  # };
+  nixos = {
+    modules.programs.cryptolyzer = {
+      module =
+        { lib, pkgs, ... }:
+        {
+          options.programs.cryptolyzer = {
+            enable = lib.mkEnableOption "CryptoLyzer";
+            package = lib.mkPackageOption pkgs.python313Packages "cryptolyzer" { };
+          };
+        };
+      links = {
+        development = {
+          text = "Development environment with `pipenv`";
+          url = "https://cryptolyzer.readthedocs.io/en/latest/development/";
+        };
+        cli = {
+          text = "Command-line interface documentation";
+          url = "https://cryptolyzer.readthedocs.io/en/latest/cli/";
+        };
+        installation = {
+          text = "General installation instructions";
+          url = "https://cryptolyzer.readthedocs.io/en/latest/installation/";
+        };
+      };
+    };
+    # TODO: this absolute basic example, which may show up just about anywhere, can probably extracted into a pattern with two parameters: the program module and the command to run for the smoke test
+    examples.basic = {
+      module = ./example.nix;
+      description = "";
+      tests.basic = ./test.nix;
+    };
+  };
+}
