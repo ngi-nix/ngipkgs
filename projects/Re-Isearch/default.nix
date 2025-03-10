a9d1d92fa1a70973a682b43b1bd3315a4fe5cded
eljamm <fedi.jamoussi@protonmail.ch>
2025-03-10 15:44:21 +0100

    Add Re-Isearch

---
+++ projects/Re-Isearch/default.nix
@@ -0,0 +1,41 @@
+{
+  lib,
+  pkgs,
+  sources,
+}@args:
+{
+  metadata = {
+    summary = ''
+      Novel multimodal search and retrieval engine
+    '';
+    subgrants = [
+      "Re-iSearch"
+      "Re-Isearch-Vector"
+    ];
+  };
+  nixos.modules.programs = {
+    re-isearch = {
+      module = ./module.nix;
+      examples.re-isearch = {
+        module = ./example.nix;
+        description = "";
+        # TODO: index and search a document (see `links.tests`)
+        tests.search-document = null;
+      };
+      links = {
+        handbook = {
+          text = "re-Isearch-Handbook (PDF)";
+          url = "https://github.com/re-Isearch/re-Isearch/blob/master/docs/re-Isearch-Handbook.pdf";
+        };
+        build = {
+          text = "Build from source";
+          url = "https://github.com/re-Isearch/re-Isearch/blob/master/INSTALLATION";
+        };
+        tests = {
+          text = "Testing (Step 5)";
+          url = "https://github.com/re-Isearch/re-Isearch/blob/master/INSTALLATION";
+        };
+      };
+    };
+  };
+}
