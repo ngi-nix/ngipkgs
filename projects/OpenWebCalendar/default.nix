6801e819c1033ebbc39e2a404cb7f293d1e5c377
Kerstin Humm <kerstin@erictapen.name>
2025-03-12 15:23:38 +0100

    OpenWebCalendar: move from projects-old into projects
    Co-authored-by: Fedi Jamoussi <Fedi.Jamoussi@protonmail.ch>

---
+++ projects/OpenWebCalendar/default.nix
@@ -0,0 +1,41 @@
+{
+  pkgs,
+  lib,
+  sources,
+  ...
+}@args:
+{
+  metadata.subgrants = [ "OpenWebCalendar" ];
+  nixos.modules.services.open-web-calendar = {
+    name = "open-web-calendar";
+    module = "${sources.inputs.nixpkgs}/nixos/modules/services/web-apps/open-web-calendar.nix";
+    examples.basic = null;
+    links = {
+      development = {
+        text = "Development";
+        url = "https://open-web-calendar.quelltext.eu/dev/";
+      };
+      maintain = {
+        text = "Maintain the project";
+        url = "https://open-web-calendar.quelltext.eu/dev/maintain/";
+      };
+      configure = {
+        text = "Configure the service";
+        url = "https://open-web-calendar.quelltext.eu/host/configure/";
+      };
+      executable-pypi = {
+        text = "Install and run the exexutable from PyPI";
+        url = "https://open-web-calendar.quelltext.eu/host/pypi/";
+      };
+      executable-docker = {
+        text = "Install and run the exexutable with Docker";
+        url = "https://open-web-calendar.quelltext.eu/host/docker/";
+      };
+      pypi = {
+        text = "PyPI project page";
+        url = "https://pypi.org/project/open-web-calendar/";
+      };
+    };
+  };
+  nixos.tests.open-web-calendar = "${sources.inputs.nixpkgs}/nixos/tests/web-apps/open-web-calendar.nix";
+}
