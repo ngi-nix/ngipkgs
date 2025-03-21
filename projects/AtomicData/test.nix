b06f4f04448060eb93bd9b26ec8d41f0c0538496
eljamm <fedi.jamoussi@protonmail.ch>
2025-03-21 14:39:02 +0100

    AtomicData: fix test

--- projects/AtomicData/test.nix
+++ projects/AtomicData/test.nix
@@ -32,6 +32,8 @@ in
     ''
       start_all()
 
+      server.wait_for_open_port(9883) # curl fails otherwise
+
       with subtest("atomic"):
           server.wait_for_unit("atomic-server.service")
           server.succeed("curl --fail --connect-timeout 10 http://localhost:9883/setup")
