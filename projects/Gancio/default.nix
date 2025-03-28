a80e2f16e2788982ef1ae440e07fda92f5fcd4d4
goodylove <goodyc474@gmail.com>
2025-03-28 11:17:44 +0100

    Gancio: add example.nix file

--- projects/Gancio/default.nix
+++ projects/Gancio/default.nix
@@ -1,10 +1,20 @@
-{
+
+  {
   pkgs,
   lib,
   sources,
   ...
 }@args:
 {
+
+   metadata = {
+    summary = "Gancio Shared agenda for local communities that supports Activity";
+    subgrants = [
+      "gancio"
+      "Hex designs"
+    ];
+  };
+
   packages = { inherit (pkgs) gancio; };
   nixos = {
     modules.services.gancio = "${sources.inputs.nixpkgs}/nixos/modules/services/web-apps/gancio.nix";
