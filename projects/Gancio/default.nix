66f84a125d590d2fb4e696e74a2e6e68e1117e4d
goodylove <goodyc474@gmail.com>
2025-03-31 22:04:38 +0100

    Gancio: a dd services.gancio.settings

--- projects/Gancio/default.nix
+++ projects/Gancio/default.nix
@@ -5,13 +5,13 @@
 }@args:
 {
   metadata = {
-    summary = "Gancio Shared agenda for local communities that supports Activity";
+    summary = "Shared agenda for local communities that supports Activity Pub";
       subgrants = [
-        "gancio"
-        "hex designs"
+        "Gancio"
+        "Hex designs"
       ];
   };
-  nixos.modules.programs = {
+  nixos.modules.services = {
     gancio = {
       module = "${sources.inputs.nixpkgs}/nixos/modules/services/web-apps/gancio.nix" ;
       examples.gancio ={
@@ -22,3 +22,4 @@
     };
   };
 } 
+
--- projects/Gancio/example.nix
+++ projects/Gancio/example.nix
@@ -1,6 +1,10 @@
 { ... }:
 {
-  programs.gancio = {
+ services.gancio = { 
     enable = true ;
-  };
+    settings = {
+       hostname = " ";
+       host = "127.0.0.1";     
+    };
+  }; 
 }
