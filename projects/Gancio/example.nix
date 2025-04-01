08c61061b5c868c23456411777b797f1b9e7f8e1
goodylove <goodyc474@gmail.com>
2025-04-01 03:34:15 +0100

    Gancio: add security.acme.acceptTerms

--- projects/Gancio/example.nix
+++ projects/Gancio/example.nix
@@ -1,10 +1,17 @@
-{ ... }:
+{ 
+pkgs ,...
+}:
 {
- services.gancio = { 
+ services.gancio = {  
     enable = true ;
+    package = pkgs.gancio;
+    plugins = [ pkgs.gancioPlugins.telegram-bridge ];
     settings = {
-       hostname = " ";
-       host = "127.0.0.1";     
+      hostname = "agenda.example.org";
+      db.dialect = "postgres";
     };
   }; 
+  security.acme.acceptTerms = true ;
+  networking.firewall.allowedTCPPorts = [ 80 443 ] ;
+  security.acme.defaults.email = "info@cisti.org" ;
 }
