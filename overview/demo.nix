bf53f7fc2e0fd6585212547d88ec1a99e1b3fce3
eljamm <fedi.jamoussi@protonmail.ch>
2025-04-02 13:37:14 +0200

    feat: use full cryptpad demo

--- overview/demo.nix
+++ overview/demo.nix
@@ -1,6 +1,20 @@
 {
   ngipkgs,
 }:
+let
+  servicePort = 9000;
+  domainName = "localhost:${toString servicePort}";
+in
 ngipkgs.demo {
-  services.cryptpad.enable = true;
+  services.cryptpad = {
+    enable = true;
+    settings = {
+      httpPort = servicePort;
+      httpUnsafeOrigin = "http://${domainName}";
+      httpSafeOrigin = "http://${domainName}";
+    };
+  };
+
+  networking.firewall.allowedTCPPorts = [ servicePort ];
+  networking.firewall.allowedUDPPorts = [ servicePort ];
 }
