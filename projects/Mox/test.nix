79f79a2203e458e7e5757cd08fa13aa63ffbc49b
MarkTanui <marktanui75@gmail.com>
2025-04-04 12:54:24 +0300

    Use unbound for dnssec

--- projects/Mox/test.nix
+++ projects/Mox/test.nix
@@ -19,6 +19,26 @@
         # Allow necessary ports through the firewall
         networking.firewall.allowedTCPPorts = [ 25 80 143 443 587 993 ];
         networking.firewall.allowedUDPPorts = [ 53 ];
+
+        services.unbound = {
+          enable = true;
+          settings = {
+            server = {
+              interface = [ "127.0.0.1" ];
+            };
+            forward-zone = [
+              {
+                name = ".";
+                forward-addr = [
+                  "9.9.9.9#dns.quad9.net"
+                  "149.112.112.112#dns.quad9.net"
+                ];
+                forward-tls-upstream = true;
+              }
+            ];
+          };
+          enableRootTrustAnchor = true;
+        };
       };
   };
 
