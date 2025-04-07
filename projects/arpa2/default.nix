e5f5278ef13889f525f4cdbed365e1c1be2280c3
eljamm <fedi.jamoussi@protonmail.ch>
2025-04-07 18:01:59 +0200

    arpa2: projects-old -> projects

---
+++ projects/arpa2/default.nix
@@ -0,0 +1,33 @@
+{
+  lib,
+  pkgs,
+  sources,
+}@args:
+
+{
+  metadata = {
+    summary = "An umbrella for open source projects that implement the open source and open networking ideals of a secure, private and decentral Internet";
+    subgrants = [
+      "DANCE4All"
+      "LDAPmiddleware"
+      "SASL-XMSS"
+      "SASLworks"
+      "Steamworks"
+      "TLS-KDH-mbed"
+      "arpa2"
+      "arpa2-nginx"
+      "steamworks"
+    ];
+  };
+
+  nixos.modules.programs = {
+    arpa2 = {
+      module = ./module.nix;
+      examples.basic = {
+        module = ./example.nix;
+        description = "";
+        tests.basic = null;
+      };
+    };
+  };
+}
