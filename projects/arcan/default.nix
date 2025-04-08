9e7efd9c5ae0d6a202e9b52d72da97c2813ebb0a
maryjane <ndukauba.oluchi@gmail.com>
2025-04-08 11:30:47 +0100

    expose arcan

---
+++ projects/arcan/default.nix
@@ -0,0 +1,37 @@
+{
+  lib,
+  pkgs,
+  sources,
+}@args:
+{
+  metadata = {
+    summary = "Explorative p2p protocol for fast and secure remote desktops";
+    subgrants = [
+      "Arcan-A12"
+      "Arcan-A12-directory"
+      "Arcan-A12-tools"
+    ];
+  };
+
+  nixos.modules.programs = {
+    arcan = {
+      name = "arcan";
+      module = ./module.nix;
+      examples.base = {
+        module = ./example.nix;
+        description = "testing documentation";
+        tests.basic = null;
+      };
+      links = {
+        build = {
+          text = "arcan Documentation";
+          url = "https://github.com/letoram/arcan#compiling";
+        };
+        test = {
+          text = "arcan Documentation";
+          url = "https://github.com/letoram/arcan/tree/master/tests";
+        };
+      };
+    };
+  };
+}
