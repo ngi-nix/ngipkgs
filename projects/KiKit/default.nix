ff38ca1c4f48890bc1d60d338ad231e2d68e57b6
eljamm <fedi.jamoussi@protonmail.ch>
2025-04-09 16:24:43 +0200

    KiKit: projects-old -> projects

---
+++ projects/KiKit/default.nix
@@ -0,0 +1,26 @@
+{
+  lib,
+  pkgs,
+  sources,
+}@args:
+
+{
+  # NOTE: this should probably be part of `KiCad`
+  metadata = {
+    summary = "Tooling for automation of production of PCB designed in KiCAD";
+    subgrants = [
+      "KiKit"
+    ];
+  };
+
+  nixos.modules.programs = {
+    kikit = {
+      module = ./module.nix;
+      examples.basic = {
+        module = ./example.nix;
+        description = "";
+        tests.basic = null;
+      };
+    };
+  };
+}
