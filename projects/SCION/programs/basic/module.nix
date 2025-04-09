36132e2b9887925534fd08e0a14531de3a2cb62a
Muhammed Ali <muhamzyali@gmail.com>
2025-04-09 18:22:32 +0100

    added packages to scion

--- projects/SCION/programs/basic/module.nix
+++ projects/SCION/programs/basic/module.nix
@@ -11,4 +11,14 @@ in
   options.programs.scion = {
     enable = lib.mkEnableOption "scion";
   };
+
+  config = lib.mkIf cfg.enable {
+    environment.systemPackages = with pkgs; [
+      scion
+      scion-apps
+      scion-bootstrapper
+      ioq3-scion
+      pan-bindings
+    ];
+  };
 }
