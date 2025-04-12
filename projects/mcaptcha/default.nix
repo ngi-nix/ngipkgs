30b2c4bf05c29738aa5b3b2851d7765af69ed765
Axler <69816272+Axler1@users.noreply.github.com>
2025-04-12 22:33:58 +0000

    Remove comments

--- projects/mcaptcha/default.nix
+++ projects/mcaptcha/default.nix
@@ -12,22 +12,8 @@
   metadata = {
     summary = "mCaptcha is a backend component for a CAPTCHA system designed to provide a seamless user experience without unnecessary complexity.";
     subgrants = [
-      # 1. Navigate to the [NLnet project list](https://nlnet.nl/project/)
-      # 2. Enter the project name in the search bar
-      # 3. Review all the entries returned by the search
-      # 4. Collect the links to entries that relate to the project
-      #
-      # For example, for a project called `Foobar`, this can be something like:
-      #
-      #   - https://nlnet.nl/project/Foobar
-      #   - https://nlnet.nl/project/Foobar-mobile
-      #
-      # The subgrants will then be:
-      #
       "mCaptcha"
     ];
-    # Top-level links for things that are in common across the whole project
-    # Remove the `links` attribute below if no such links exist
     links = {
       website = {
         text = "Official website";
@@ -40,27 +26,9 @@
     };
   };
 
-  # NOTE: Replace `_serviceName_` with the actual service name
   nixos.modules.services = {
     mcaptcha = {
       name = "mcaptcha";
-      # Check if the service exists in https://search.nixos.org/options?
-      # If it does, click on one of its options and copy the text in the `Declared in` field
-      # into the `module` attribute:
-      #
-      # ```nix
-      # module = "${sources.inputs.nixpkgs}/<DECLARED_IN_TEXT>";
-      # ```
-      #
-      # Example (Cryptpad):
-      #
-      # ```nix
-      # module = "${sources.inputs.nixpkgs}/nixos/modules/services/web-apps/cryptpad.nix";
-      # ```
-      #
-      # Note: we can either use the module in nixpkgs or make one ourselves
-      # inside a `module.nix` file, but we can't do both at the same time.
-      #
       module = ./services/mcaptcha/module.nix;
       examples.basic = {
         module = ./services/mcaptcha/examples/basic.nix;
