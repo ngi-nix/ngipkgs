d5d5ee9e75daeac1b08bae6041de91f76fca193b
themadbit <marktanui75@gmail.com>
2025-04-16 13:47:30 +0300

    Add conventional port and firewall usage

--- projects/Mox/services/mox/examples/basic.nix
+++ projects/Mox/services/mox/examples/basic.nix
@@ -4,4 +4,10 @@
   services.mox.enable = true;
   services.mox.hostname = "mail";
   services.mox.user = "admin@example.com";
+  services.mox.openFirewall = true;
+  services.mox.openPorts = [
+    25
+    80
+    443
+  ];
 }
