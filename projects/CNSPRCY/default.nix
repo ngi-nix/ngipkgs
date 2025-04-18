fee8b24e12ba8e24c15bc3a01349af7a850f3c7b
tk <tahia.khan@gmail.com>
2025-04-18 10:45:38 -0400

    cnsprcy working with programs template

--- projects/CNSPRCY/default.nix
+++ projects/CNSPRCY/default.nix
@@ -3,20 +3,28 @@
   pkgs,
   sources,
 }@args:
+
 {
   metadata = {
+    summary = "E2EE connections between trusted devices for establishing private group chats.";
     subgrants = [
       "CNSPRCY"
     ];
+    links = {
+      exampleLink = {
+        text = "Title";
+        url = "<URL>";
+      };
+    };
   };
 
   # https://git.sr.ht/~xaos/cnsprcy/tree/master/item/src/config.rs
   nixos.modules.programs.cnsprcy = {
-    module = ./module.nix;
-    examples.cnsprcy = {
-      module = ./example.nix;
+    module = ./programs/cnsprcy/module.nix;
+    examples.basic = {
+      module = ./programs/cnsprcy/examples/basic.nix;
       description = "";
-      tests.basic = import ./test.nix args;
+      tests.basic = import ./programs/cnsprcy/tests/basic.nix args;
     };
   };
 }
