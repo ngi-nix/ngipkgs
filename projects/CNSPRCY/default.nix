cd0331f5391fdd676bb07a3daa8b6492188fe8dd
tk <tahia.khan@gmail.com>
2025-04-18 10:45:38 -0400

    CNSPRCY: adds service module and basic test to cnsprcy project

--- projects/CNSPRCY/default.nix
+++ projects/CNSPRCY/default.nix
@@ -3,20 +3,38 @@
   pkgs,
   sources,
 }@args:
+
 {
   metadata = {
+    summary = "E2EE connections between trusted devices for establishing private group chats.";
     subgrants = [
       "CNSPRCY"
     ];
+    links = {
+      source = {
+        text = "CNSPRCY source code";
+        url = "https://git.sr.ht/~xaos/cnsprcy";
+      };
+    };
   };
 
-  # https://git.sr.ht/~xaos/cnsprcy/tree/master/item/src/config.rs
   nixos.modules.programs.cnsprcy = {
-    module = ./module.nix;
-    examples.cnsprcy = {
-      module = ./example.nix;
-      description = "";
-      tests.basic = import ./test.nix args;
+    name = "cnsprcy";
+    module = ./programs/cnsprcy/module.nix;
+    examples.basic = {
+      module = ./programs/cnsprcy/examples/basic.nix;
+      description = "Checks for cnspr executable";
+      tests.basic = import ./programs/cnsprcy/tests/basic.nix args;
+    };
+  };
+
+  nixos.modules.services.cnsprcy = {
+    name = "cnsprcy";
+    module = ./services/cnsprcy/module.nix;
+    examples.basic = {
+      module = ./services/cnsprcy/examples/basic.nix;
+      description = "Checks that cnsprcy systemd service is running";
+      tests.basic = import ./services/cnsprcy/tests/basic.nix args;
     };
   };
 }
