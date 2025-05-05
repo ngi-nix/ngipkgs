772833be87572b4acbd169cd1538a5f97f2b00be
Garvit Khandelwal <70192868+Garvit-77@users.noreply.github.com>
2025-05-05 12:13:35 +0000

    updated pr
    Signed-off-by: Garvit Khandelwal <70192868+Garvit-77@users.noreply.github.com>

--- projects/Galene/example.nix
+++ projects/Galene/example.nix
@@ -1,5 +1,11 @@
 { ... }:
 {
-  services.galene.enable = true;
-  services.galene.insecure = true;
+  services = {
+    galene = {
+      enable = true;
+      httpport = 8443;
+      certfile = "etc/galene/cert.pem";
+      keyfile = "etc/galene/keyfile.pem";
+    };
+  }
 }
--- projects/Galene/test.nix
+++ projects/Galene/test.nix
@@ -1,19 +1,36 @@
 {
   sources,
+  pkgs,
   ...
 }:
+
+let 
+  certs = pkgs.runCommand "galene-certs" {} ''
+    mkdir -p $out
+    ${pkgs.openssl}/bin/openssl req -x509 -newkey rsa:4096 -keyout $out/key.pem -out $out/cert.pem -days 365 -nodes -subj '/CN=localhost'
+  '';
+in
 {
   name = "galene";
 
   nodes = {
     machine =
-      { ... }:
+      { config, pkgs, ... }:
       {
         imports = [
           sources.modules.ngipkgs
           sources.modules.services.galene
           sources.examples.Galene.galene
         ];
+
+        environment.etc."galene/cert.pem".source = "${certs}/cert.pem";
+        environment.etc."galene/key.pem".source = "${certs}/key.pem";
+        services.galene = {
+          enable = true;
+          httpPort = 8443;
+          certFile = "/etc/galene/cert.pem";
+          keyFile = "/etc/galene/key.pem";
+        };
       };
   };
 
@@ -21,5 +38,8 @@
     { nodes, ... }:
     ''
       start_all()
+      machine.waitForUnit("galene.service")
+      machine.wait_for_open_port(8443)
+      machine.succeed("curl --insecure https://localhost:8443 -s -o /dev/null -w '%{http_code}' | grep 200")
     '';
 }
