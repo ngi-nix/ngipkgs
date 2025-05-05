eb5a3ee63ff26e98750e3c8feb504517a2b161c6
Ivan Mincik <ivan.mincik@gmail.com>
2025-05-05 14:42:01 +0200

    cryptpad: fix demo port forwarding
    Closes: #970

--- projects/Cryptpad/demo.nix
+++ projects/Cryptpad/demo.nix
@@ -7,8 +7,8 @@
     settings = {
       httpPort = 9000;
       httpAddress = "0.0.0.0";
-      httpUnsafeOrigin = "http://localhost:${toString config.services.cryptpad.settings.httpPort}";
-      httpSafeOrigin = "http://localhost:${toString config.services.cryptpad.settings.httpPort}";
+      httpUnsafeOrigin = "http://localhost:19000";
+      httpSafeOrigin = "http://localhost:19000";
     };
   };
 }
