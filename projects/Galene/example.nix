064d06a3326ecc07e5f748e95a0e24892421b7c4
Garvit-77 <garvitname@gmail.com>
2025-05-10 00:04:03 +0530

    syntax changes
    Signed-off-by: Garvit-77 <garvitname@gmail.com>

--- projects/Galene/example.nix
+++ projects/Galene/example.nix
@@ -3,9 +3,9 @@
   services = {
     galene = {
       enable = true;
-      httpport = 8443;
-      certfile = "etc/galene/cert.pem";
-      keyfile = "etc/galene/keyfile.pem";
+      httpPort = 8443;
+      certFile = "/etc/galene/cert.pem";
+      keyFile = "/etc/galene/keyfile.pem";
     };
-  }
+  };
 }
--- projects/Galene/test.nix
+++ projects/Galene/test.nix
@@ -4,8 +4,8 @@
   ...
 }:
 
-let 
-  certs = pkgs.runCommand "galene-certs" {} ''
+let
+  certs = pkgs.runCommand "galene-certs" { } ''
     mkdir -p $out
     ${pkgs.openssl}/bin/openssl req -x509 -newkey rsa:4096 -keyout $out/key.pem -out $out/cert.pem -days 365 -nodes -subj '/CN=localhost'
   '';
@@ -22,15 +22,8 @@ in
           sources.modules.services.galene
           sources.examples.Galene.galene
         ];
-
         environment.etc."galene/cert.pem".source = "${certs}/cert.pem";
         environment.etc."galene/key.pem".source = "${certs}/key.pem";
-        services.galene = {
-          enable = true;
-          httpPort = 8443;
-          certFile = "/etc/galene/cert.pem";
-          keyFile = "/etc/galene/key.pem";
-        };
       };
   };
 
@@ -38,7 +31,7 @@ in
     { nodes, ... }:
     ''
       start_all()
-      machine.waitForUnit("galene.service")
+      machine.wait_for_unit("galene.service")
       machine.wait_for_open_port(8443)
       machine.succeed("curl --insecure https://localhost:8443 -s -o /dev/null -w '%{http_code}' | grep 200")
     '';
