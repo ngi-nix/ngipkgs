8f922d64e3afc951bb41faeee8f59dc42fb6b1f6
tk <tahia.khan@gmail.com>
2025-05-08 20:18:18 -0400

    applies PR comments

--- projects/CNSPRCY/services/cnsprcy/module.nix
+++ projects/CNSPRCY/services/cnsprcy/module.nix
@@ -6,7 +6,6 @@
 }:
 let
   cfg = config.services.cnsprcy;
-  stateDir = "/var/lib/cnsprcy";
 in
 {
   options.services.cnsprcy = {
@@ -17,6 +16,12 @@ in
       type = lib.types.str;
       description = "Hostname of CNSPRCY server";
       default = builtins.getEnv "HOSTNAME";
+    };
+
+    stateDir = lib.mkOption {
+      type = lib.types.str;
+      description = "State directory for CNSPRCY server";
+      default = "/var/lib/cnsprcy";
     };
 
     user = lib.mkOption {
@@ -40,32 +45,40 @@ in
       group = cfg.group;
 
       # CNSPRCY looks for the user home to initialize its data directory
-      # at $HOME/.local/share/cnsprcy, but systemd.tmpfiles needs to create
+      # at $stateDir/.local/share/cnsprcy, but systemd.tmpfiles needs to create
       # the dir otherwise permissions won't be set correctly.
-      home = "${stateDir}";
+      home = cfg.stateDir;
       useDefaultShell = true;
     };
     users.groups."${cfg.group}" = { };
 
     systemd.tmpfiles.rules = [
-      "d ${stateDir}/.local/share/cnsprcy 0700 ${cfg.user} ${cfg.group} -"
-      "d ${stateDir}/.local/share/cnsprcy/handlers 0700 ${cfg.user} ${cfg.group} -"
+      "d ${cfg.stateDir}/.local/share/cnsprcy 0700 ${cfg.user} ${cfg.group} -"
+      "d ${cfg.stateDir}/.local/share/cnsprcy/handlers 0700 ${cfg.user} ${cfg.group} -"
     ];
 
     systemd.services.cnsprcy = {
       description = "CNSPRCY service";
       wantedBy = [ "multi-user.target" ];
-      after = [ "nss-user-lookup.target" ];
+      after = [
+        "nss-user-lookup.target"
+        "systemd-tmpfiles-setup.service"
+      ];
 
       # Runs interactive config initialization to set machine name, generate
-      # conspirator keys and initial CNSPRCY db
-      preStart = "printf 'n\n${cfg.hostname}\ny\n' | ${pkgs.cnsprcy}/bin/cnspr config init";
+      # conspirator keys and initial CNSPRCY db. TODO use the config init flags
+      # instead, but will need to investigate how to feed args to `cnspr serve`.
+      #
+      # That said we'll ultimately want to generate the config using options rather
+      # than dynamically like this.
+      # See https://github.com/ngi-nix/ngipkgs/pull/870/files#r2077898766
+      preStart = "printf 'n\n${cfg.hostname}\ny\n' | ${lib.getExe cfg.package} config init";
 
       serviceConfig = {
-        ExecStart = "${pkgs.cnsprcy}/bin/cnspr serve";
+        ExecStart = "${lib.getExe cfg.package} serve";
         Restart = "on-failure";
-        User = "${cfg.user}";
-        Group = "${cfg.group}";
+        User = cfg.user;
+        Group = cfg.group;
       };
     };
 
