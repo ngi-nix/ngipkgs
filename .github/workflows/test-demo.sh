1b51235c13adecf7933e14fbb0c82dac9f7393e4
Valentin Gagarin <valentin@gagarin.work>
2025-05-15 23:32:00 +0200

    CI: move NIX_CONFIG into workflow YAML
    that way we can access it easily from Nix code for use in documentation

--- .github/workflows/test-demo.sh
+++ .github/workflows/test-demo.sh
@@ -1,18 +1,6 @@
 #!/usr/bin/env bash
 
-# nix build .#overview
-# podman run --privileged \
-#   --volume ./result/project/Cryptpad/default.nix:/default.nix \
-#   --volume .github/workflows/test-demo.sh:/test-demo.sh <DISTRO> /bin/bash \
-#   -c "bash /test-demo.sh <DISTRO>"
-
-set -euo pipefail
-
-DISTRO="$1"
-# shellcheck disable=SC2089,2026
-NIX_CONFIG='substituters = https://cache.nixos.org/ https://ngi.cachix.org/'$'\n''trusted-public-keys = cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY= ngi.cachix.org-1:n+CAL72ROC3qQuLxIHpV+Tw5t42WhXmMhprAGkRSrOw='
-export NIX_CONFIG
-
+set -eo pipefail
 
 echo -e "\n-> Installing Nix ..."
 # Debian/Ubuntu
