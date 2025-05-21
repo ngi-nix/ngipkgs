c1f7a2711a25470d7ecf3e7247c50bec644eb09f
eljamm <fedi.jamoussi@protonmail.ch>
2025-05-21 10:03:05 +0200

    demo: refactor vm config

---
+++ overview/demo/vm/services.nix
@@ -0,0 +1,15 @@
+{
+  services.openssh = {
+    enable = true;
+    ports = [
+      10022
+    ];
+    settings = {
+      PasswordAuthentication = true;
+      PermitEmptyPasswords = "yes";
+      PermitRootLogin = "yes";
+    };
+  };
+
+  networking.firewall.enable = false;
+}
