6f20e96b98e6b54792b02700895f23bf22dd3835
kel <63237763+Kyesarri@users.noreply.github.com>
2025-05-30 13:59:46 +0000

    Vula: fix option name in example description

--- projects/Vula/default.nix
+++ projects/Vula/default.nix
@@ -20,7 +20,7 @@
       description = ''
         Simple configuration for Vula. Vula nodes will automatically discover each other on networks that support [multicast DNS](https://en.wikipedia.org/wiki/Multicast_DNS) (mDNS).
 
-        Add users to the group defined in `config.services.vula.adminGroup` to grant them permissions to manage Vula through the `vula` command.
+        Add users to the group defined in `config.services.vula.operatorsGroup` to grant them permissions to manage Vula through the `vula` command.
       '';
       tests.test = import ./test.nix args;
     };
