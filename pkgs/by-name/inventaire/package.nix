3891e97972f2abae1b78257f3922d696567f8018
OPNA2608 <opna2608@protonmail.com>
2025-06-05 16:47:03 +0200

    inventaire: init
    This is a derivation that puts together inventaire-unwrapped and inventaire-client in a way that allows
    the former to find the latter at runtime.

---
+++ pkgs/by-name/inventaire/package.nix
@@ -0,0 +1,32 @@
+{
+  lib,
+  runCommandNoCC,
+  nodejs,
+  inventaire-unwrapped,
+  inventaire-client,
+}:
+
+# Inventaire has a build process that involves cloning different projects into various locations.
+# Some of these projects are written with the assumption that they'll only ever be used from within the inventaire tree.
+# We're building everything separately, so now we need to assemble the things that care about their locations back into one tree.
+runCommandNoCC "inventaire"
+  {
+    meta = {
+      description = "A libre collaborative resources mapper powered by open-knowledge (wrapper)";
+      mainProgram = "inventaire";
+    };
+  }
+  ''
+    cp -r --no-preserve=mode ${inventaire-unwrapped} $out
+    ln -s ${inventaire-client}/lib/node_modules/inventaire-client $out/lib/node_modules/inventaire/dist/client
+
+    # Launcher
+    mkdir -p $out/bin
+    cat <<EOF >$out/bin/inventaire
+    #!/bin/sh
+
+    exec ${lib.getExe nodejs} $out/lib/node_modules/inventaire/dist/server/server.js
+    EOF
+
+    chmod +x $out/bin/inventaire
+  ''
