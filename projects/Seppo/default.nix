96263d6f12aae5c84cbac1ce4075ddef8807924d
themadbit <marktanui75@gmail.com>
2025-06-11 17:36:46 +0300

    seppo: implement project metadata

---
+++ projects/Seppo/default.nix
@@ -0,0 +1,27 @@
+{
+  lib,
+  pkgs,
+  sources,
+}@args:
+
+{
+  metadata = {
+    summary = "Portable ActivityPub implementation";
+    subgrants = [
+      "Seppo"
+    ];
+    links = {
+      install = {
+        text = "Installation Instructions";
+        url = "https://seppo.mro.name/en/support/#installation";
+      };
+      source = {
+        text = "Codeberg repository";
+        url = "https://codeberg.org/seppo/seppo#seppo";
+      };
+    };
+  };
+
+  nixos.modules.programs.seppo = null;
+  nixos.modules.services.seppo = null;
+}
