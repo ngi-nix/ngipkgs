e62cb5070e72c34e7d49f069aab0a36c3a204d5e
Ethan Carter Edwards <ethan@ethancedwards.com>
2025-06-15 14:36:44 -0400

    projects/nyxt: init
    Links:
    https://nlnet.nl/project/Nyxt-Webextensions/
    https://nlnet.nl/project/NyxtBrowser/
    https://nlnet.nl/project/NyxtUserhosted/
    
    Signed-off-by: Ethan Carter Edwards <ethan@ethancedwards.com>

---
+++ projects/nyxt/default.nix
@@ -0,0 +1,28 @@
+{
+  lib,
+  pkgs,
+  sources,
+}@args:
+
+{
+  metadata = {
+    summary = "Nyxt is a new type of web browser designed to empower users to find and filter information on the Internet";
+    subgrants = [
+      "Nyxt-Webextensions"
+      "NyxtBrowser"
+      "NyxtUserhosted"
+    ];
+  };
+
+  nixos.modules.programs = {
+    nyxt = {
+      name = "nyxt";
+      module = ./programs/nyxt/module.nix;
+      examples.basic = {
+        module = ./programs/nyxt/examples/basic.nix;
+        description = "Enable the nyxt program";
+        tests.basic = import ./programs/nyxt/tests/basic.nix args;
+      };
+    };
+  };
+}
