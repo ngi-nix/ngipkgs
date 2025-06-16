06fd1e5c698d3a0a7fb25a342b4aaf1cec0769de
Ethan Carter Edwards <ethan@ethancedwards.com>
2025-06-16 02:44:13 -0400

    projects/jaq: init (#1157)

---
+++ projects/jaq/default.nix
@@ -0,0 +1,29 @@
+{
+  lib,
+  pkgs,
+  sources,
+}@args:
+
+{
+  metadata = {
+    summary = "Jaq is a data wrangling tool focusing on correctness, speed, and simplicity";
+    subgrants = [
+      "jaq"
+      "Polyglot-jaq"
+    ];
+  };
+
+  nixos.modules.programs = {
+    jaq = {
+      name = "jaq";
+      module = ./programs/jaq/module.nix;
+      examples.basic = {
+        module = ./programs/jaq/examples/basic.nix;
+        description = "Enable the jaq program";
+        tests.basic = import ./programs/jaq/tests/basic.nix args;
+      };
+    };
+  };
+
+  # no service module as this is strictly a program
+}
