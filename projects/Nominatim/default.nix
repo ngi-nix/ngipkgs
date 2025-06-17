4cedbab92230cf5efa86f0d12585a6ff4bb54af8
Ivan Mincik <ivan.mincik@gmail.com>
2025-06-17 14:23:55 +0200

    nominatim: implement project metadata
    Closes: #1181

---
+++ projects/Nominatim/default.nix
@@ -0,0 +1,44 @@
+{
+  lib,
+  pkgs,
+  sources,
+}@args:
+
+{
+  metadata = {
+    summary = "Nominatim is an open-source geographic search engine (geocoder).";
+    subgrants = [
+      "Nominatim"
+      "Nominatim-lib"
+    ];
+    links = {
+      docs = {
+        text = "Documentation";
+        url = "https://nominatim.org/release-docs/latest/";
+      };
+      src = {
+        text = "Source repository";
+        url = "https://github.com/osm-search/Nominatim";
+      };
+      src-ui = {
+        text = "Source repository (UI)";
+        url = "https://github.com/osm-search/nominatim-ui";
+      };
+    };
+  };
+
+  nixos.modules.programs = {
+    nominatim = {
+      name = "nominatim";
+      module = null;
+    };
+  };
+
+  nixos.modules.services = {
+    nominatim = {
+      name = "nominatim";
+      module = null;
+      examples.demo = null;
+    };
+  };
+}
