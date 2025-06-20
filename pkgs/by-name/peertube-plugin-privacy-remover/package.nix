fd272daf7f3bf5b306a0ed3d49aacb65e603ca0b
June Stepp <git@junestepp.me>
2025-06-20 17:37:01 -0500

    peertube-plugin-privacy-remover: add upadate script

--- pkgs/by-name/peertube-plugin-privacy-remover/package.nix
+++ pkgs/by-name/peertube-plugin-privacy-remover/package.nix
@@ -2,6 +2,7 @@
   lib,
   stdenvNoCC,
   fetchFromGitLab,
+  peertube-plugin-akismet,
 }:
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "peertube-plugin-privacy-remover";
@@ -12,7 +13,8 @@ stdenvNoCC.mkDerivation (finalAttrs: {
     owner = "framasoft";
     repo = "peertube/official-plugins";
     rev = "2df3a5909536f17f143b2c391bb483e339f36c3e";
-    hash = "sha256-2kcugu4uEPd/WgOe3vV6xaA1d2OcSBJy2ZkZ8gNv760=";
+    sparseCheckout = [ "peertube-plugin-privacy-remover" ];
+    hash = "sha256-CptC4fBeJ5q9i/EmgShdl1tdtz6ymnGR/T5kj60EBPI=";
   };
 
   sourceRoot = "${finalAttrs.src.name}/peertube-plugin-privacy-remover";
@@ -28,7 +30,7 @@ stdenvNoCC.mkDerivation (finalAttrs: {
     runHook postInstall
   '';
 
-  # TODO: passthru.updateScript? there are no tags, versions come as commits with changes to subdir's package.json
+  passthru.updateScript = peertube-plugin-akismet.peertubeOfficialPluginsUpdateScript;
 
   meta = {
     description = "Remove video privacy settings of your choice";
