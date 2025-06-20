962082b94c10035199ba7408b126e6c190c2e7eb
June Stepp <git@junestepp.me>
2025-06-20 17:28:04 -0500

    peertube-plugin-hello-world: add update script

--- pkgs/by-name/peertube-plugin-hello-world/package.nix
+++ pkgs/by-name/peertube-plugin-hello-world/package.nix
@@ -2,8 +2,9 @@
   lib,
   buildNpmPackage,
   fetchFromGitLab,
+  peertube-plugin-akismet,
 }:
-buildNpmPackage rec {
+buildNpmPackage (finalAttrs: {
   pname = "peertube-plugin-hello-world";
   version = "0.0.22";
 
@@ -12,16 +13,17 @@ buildNpmPackage rec {
     owner = "framasoft";
     repo = "peertube/official-plugins";
     rev = "fa9005ab1bab93e41e10bb1be3dc4837bd6bbc47";
-    hash = "sha256-d1DGrmRCavc1a6r3UvT7AzcLkFMJktwDKpEjgx7RJAI=";
+    sparseCheckout = [ "peertube-plugin-hello-world" ];
+    hash = "sha256-gNeOJpKYqIRVLMbgfSVXnzCD9QGOJfGcJB3IkZweTEI=";
   };
 
-  sourceRoot = "${src.name}/peertube-plugin-hello-world";
+  sourceRoot = "${finalAttrs.src.name}/peertube-plugin-hello-world";
 
   npmDepsHash = "sha256-Y6bq2w5ykqLMY9eDTNKL3DMkoOx+imV7OCw2Hy961Tk=";
 
   dontNpmBuild = true;
 
-  # TODO: passthru.updateScript? there are no tags, versions come as commits with changes to subdir's package.json
+  passthru.updateScript = peertube-plugin-akismet.peertubeOfficialPluginsUpdateScript;
 
   meta = {
     description = "Hello world PeerTube plugin example";
@@ -30,4 +32,4 @@ buildNpmPackage rec {
     maintainers = with lib.maintainers; [ ];
     platforms = lib.platforms.unix;
   };
-}
+})
