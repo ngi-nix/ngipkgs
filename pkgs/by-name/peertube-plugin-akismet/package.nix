450adbd9234b391dc9e482295d36f1d192a45a66
June Stepp <git@junestepp.me>
2025-06-20 09:54:25 -0500

    peertube-plugin-akismet: 0.1.1 > 0-unstable-2025-05-30

--- pkgs/by-name/peertube-plugin-akismet/package.nix
+++ pkgs/by-name/peertube-plugin-akismet/package.nix
@@ -6,20 +6,20 @@
 }:
 buildNpmPackage (finalAttrs: {
   pname = "peertube-plugin-akismet";
-  version = "0.1.1";
+  version = "0-unstable-2025-05-30";
 
   src = fetchFromGitLab {
     domain = "framagit.org";
     owner = "framasoft";
     repo = "peertube/official-plugins";
-    rev = "6d07c51ac627d76de63b093ef0cd9e20da1019cf";
+    rev = "1c6f794d7a5d9c69374cb6fa1daf184258acb63a";
     sparseCheckout = [ "peertube-plugin-akismet" ];
-    hash = "sha256-W9ojtIsKo70gF9Wrpd2y1gt9T2/TDdmJ5yqWJ13+wic=";
+    hash = "sha256-vFWvBxSfKg2YjKzUM0Qds7MEi9O9PFfnweOZu1Sd6KA=";
   };
 
   sourceRoot = "${finalAttrs.src.name}/peertube-plugin-akismet";
 
-  npmDepsHash = "sha256-U+AQeOJI31QPeZrej6STDRtnwYJgSp86ycf4vsqSats=";
+  npmDepsHash = "sha256-cd/vCw2oP8lOEeg9LFj1Zh2Mmj+KKArFhtjd5G7hhTo=";
 
   passthru = {
     updateScript = [
