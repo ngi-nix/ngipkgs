499422dfecc85a8b76d3f2b05abe94f1a6d53da0
eljamm <fedi.jamoussi@protonmail.ch>
2025-06-20 20:27:47 +0200

    projects: re-enable binary artefacts

--- projects/Nitrokey/default.nix
+++ projects/Nitrokey/default.nix
@@ -2,6 +2,7 @@
   lib,
   pkgs,
   sources,
+  system,
 }@args:
 
 {
@@ -24,18 +25,12 @@
     };
   };
 
-  binary =
-    # Depends on the system
-    # see https://github.com/ngi-nix/ngipkgs/pull/773
-    if builtins ? currentSystem then
-      {
-        # TODO: nitrokey-3-firmware
-        "nitrokey-fido2-firmware".data = pkgs.nitrokey-fido2-firmware;
-        "nitrokey-pro-firmware".data = pkgs.nitrokey-pro-firmware;
-        "nitrokey-start-firmware".data = pkgs.nitrokey-start-firmware;
-        "nitrokey-storage-firmware".data = pkgs.nitrokey-storage-firmware;
-        "nitrokey-trng-rs232-firmware".data = pkgs.nitrokey-trng-rs232-firmware;
-      }
-    else
-      { };
+  binary = lib.mkIf (system != "aarch64-linux") {
+    # TODO: nitrokey-3-firmware
+    "nitrokey-fido2-firmware".data = pkgs.nitrokey-fido2-firmware;
+    "nitrokey-pro-firmware".data = pkgs.nitrokey-pro-firmware;
+    "nitrokey-start-firmware".data = pkgs.nitrokey-start-firmware;
+    "nitrokey-storage-firmware".data = pkgs.nitrokey-storage-firmware;
+    "nitrokey-trng-rs232-firmware".data = pkgs.nitrokey-trng-rs232-firmware;
+  };
 }
