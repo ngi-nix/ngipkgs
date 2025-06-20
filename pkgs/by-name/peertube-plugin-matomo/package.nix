de6c19ee5f3deb467919aac71fd837ccc2aa5586
June Stepp <git@junestepp.me>
2025-06-20 17:34:05 -0500

    peertube-plugin-matomo: add update script

--- pkgs/by-name/peertube-plugin-matomo/package.nix
+++ pkgs/by-name/peertube-plugin-matomo/package.nix
@@ -2,8 +2,9 @@
   lib,
   buildNpmPackage,
   fetchFromGitLab,
+  peertube-plugin-akismet,
 }:
-buildNpmPackage rec {
+buildNpmPackage (finalAttrs: {
   pname = "peertube-plugin-matomo";
   version = "1.0.2";
 
@@ -12,14 +13,15 @@ buildNpmPackage rec {
     owner = "framasoft";
     repo = "peertube/official-plugins";
     rev = "9bd2db9033c11c650cdf1035f7eed6f4ba61cf54";
-    hash = "sha256-EUocSJLi2s9VXddRIffG1Aj8/1AlegwP+76IbowhoOo=";
+    sparseCheckout = [ "peertube-plugin-matomo" ];
+    hash = "sha256-c7Wnli7L382XbfEOApPlqALkjK8Le9A9xvUOdMRq4Io=";
   };
 
-  sourceRoot = "${src.name}/peertube-plugin-matomo";
+  sourceRoot = "${finalAttrs.src.name}/peertube-plugin-matomo";
 
   npmDepsHash = "sha256-s2vrUKMRF+VhBPAbv/RQ66UBNOBYEvi/axxJB132R9s=";
 
-  # TODO: passthru.updateScript? there are no tags, versions come as commits with changes to subdir's package.json
+  passthru.updateScript = peertube-plugin-akismet.peertubeOfficialPluginsUpdateScript;
 
   meta = {
     description = "Matomo plugin that tracks page views on a PeerTube instance";
@@ -28,4 +30,4 @@ buildNpmPackage rec {
     maintainers = with lib.maintainers; [ ];
     platforms = lib.platforms.unix;
   };
-}
+})
