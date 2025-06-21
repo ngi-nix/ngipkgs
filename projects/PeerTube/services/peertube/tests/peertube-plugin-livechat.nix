23c51a17b608181655a15eda2ded137378ebe7b1
June Stepp <git@junestepp.me>
2025-06-21 13:30:19 -0500

    projects/PeerTube: split off livechat test
    The order between it and the hello subtest weren't fullly consistent.

---
+++ projects/PeerTube/services/peertube/tests/peertube-plugin-livechat.nix
@@ -0,0 +1,41 @@
+{
+  sources,
+  pkgs,
+  lib,
+  ...
+}:
+{
+  name = "peertube-plugin-livechat";
+
+  nodes = {
+    server =
+      { config, ... }:
+      {
+        imports = [
+          sources.modules.ngipkgs
+          sources.modules.services.peertube
+          sources.examples.PeerTube.basic
+        ];
+
+        services.peertube.plugins.plugins = lib.mkForce [
+          pkgs.peertube-plugin-livechat
+        ];
+
+        boot.kernelPackages = pkgs.linuxPackages_latest;
+      };
+  };
+
+  testScript =
+    { nodes, ... }:
+    let
+      url = "http://${nodes.server.services.peertube.localDomain}:${toString nodes.server.services.peertube.listenWeb}";
+    in
+    ''
+      start_all()
+
+      # Wait until we can get through to the instance and trigger some initial loading
+      server.wait_until_succeeds("curl -Ls ${url}")
+
+      server.wait_for_console_text("loading peertube admins and moderators")
+    '';
+}
