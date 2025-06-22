bf075a24dd9c26932d6a30d3940d439091472c15
Sizhe Zhao <prc.zhao@outlook.com>
2025-06-22 11:59:32 +0800

    projects/NodeBB: add demo vm

--- projects/NodeBB/services/nodebb/examples/postgresql.nix
+++ projects/NodeBB/services/nodebb/examples/postgresql.nix
@@ -19,4 +19,11 @@
   services.postgresql.initialScript = pkgs.writeText "init-sql-script" ''
     CREATE ROLE nodebb LOGIN PASSWORD 'nodebb';
   '';
+
+  # demo-vm
+  networking.firewall.allowedTCPPorts = [ 4567 ];
+  services.getty.helpLine = ''
+    NodeBB needs some time to set up and will list on port 4567 when ready.
+    View journal with `journalctl -efu nodebb.sevice` to see progress.
+  '';
 }
