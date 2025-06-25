0e9c78d245a0cc0429f7ec3afb8e9797509b5051
Sizhe Zhao <prc.zhao@outlook.com>
2025-06-25 13:14:49 +0800

    projects/OpenWebCalendar: add demo vm

--- projects/OpenWebCalendar/default.nix
+++ projects/OpenWebCalendar/default.nix
@@ -6,39 +6,46 @@
 }@args:
 {
   metadata.subgrants = [ "OpenWebCalendar" ];
-  nixos.modules.services.open-web-calendar = {
-    name = "open-web-calendar";
-    module = lib.moduleLocFromOptionString "services.open-web-calendar";
-    examples.basic = {
-      module = ./example.nix;
-      description = "";
-      tests.basic = "${sources.inputs.nixpkgs}/nixos/tests/web-apps/open-web-calendar.nix";
+  nixos = {
+    modules.services.open-web-calendar = {
+      name = "open-web-calendar";
+      module = lib.moduleLocFromOptionString "services.open-web-calendar";
+      examples.basic = {
+        module = ./example.nix;
+        description = "";
+        tests.basic = "${sources.inputs.nixpkgs}/nixos/tests/web-apps/open-web-calendar.nix";
+      };
+      links = {
+        development = {
+          text = "Development";
+          url = "https://open-web-calendar.quelltext.eu/dev/";
+        };
+        maintain = {
+          text = "Maintain the project";
+          url = "https://open-web-calendar.quelltext.eu/dev/maintain/";
+        };
+        configure = {
+          text = "Configure the service";
+          url = "https://open-web-calendar.quelltext.eu/host/configure/";
+        };
+        executable-pypi = {
+          text = "Install and run the exexutable from PyPI";
+          url = "https://open-web-calendar.quelltext.eu/host/pypi/";
+        };
+        executable-docker = {
+          text = "Install and run the exexutable with Docker";
+          url = "https://open-web-calendar.quelltext.eu/host/docker/";
+        };
+        pypi = {
+          text = "PyPI project page";
+          url = "https://pypi.org/project/open-web-calendar/";
+        };
+      };
     };
-    links = {
-      development = {
-        text = "Development";
-        url = "https://open-web-calendar.quelltext.eu/dev/";
-      };
-      maintain = {
-        text = "Maintain the project";
-        url = "https://open-web-calendar.quelltext.eu/dev/maintain/";
-      };
-      configure = {
-        text = "Configure the service";
-        url = "https://open-web-calendar.quelltext.eu/host/configure/";
-      };
-      executable-pypi = {
-        text = "Install and run the exexutable from PyPI";
-        url = "https://open-web-calendar.quelltext.eu/host/pypi/";
-      };
-      executable-docker = {
-        text = "Install and run the exexutable with Docker";
-        url = "https://open-web-calendar.quelltext.eu/host/docker/";
-      };
-      pypi = {
-        text = "PyPI project page";
-        url = "https://pypi.org/project/open-web-calendar/";
-      };
+    demo.vm = {
+      module = ./example.nix;
+      description = "Deployment for demo purposes";
+      tests.basic = "${sources.inputs.nixpkgs}/nixos/tests/web-apps/open-web-calendar.nix";
     };
   };
 }
