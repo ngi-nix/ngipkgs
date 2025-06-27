8109be98c58242de1c5cc070320beaa672b70a2d
eljamm <fedi.jamoussi@protonmail.ch>
2025-06-27 08:14:19 +0200

    treewide: mark broken project tests with problem type
    and add a reason for why it broke.

--- projects/Wireguard/default.nix
+++ projects/Wireguard/default.nix
@@ -36,9 +36,18 @@
       examples.basic.module = null;
     };
   };
-
-  nixos.tests = lib.removeAttrs pkgs.nixosTests.wireguard [
-    # FIX: https://buildbot.ngi.nixos.org/#/builders/987/builds/1
-    "wireguard-dynamic-refresh-networkd-linux-latest"
-  ];
+  nixos.tests =
+    let
+      nixosTests = lib.mapAttrs (_: test: {
+        module = test;
+      }) pkgs.nixosTests.wireguard;
+    in
+    lib.recursiveUpdate nixosTests {
+      # FIX:
+      "wireguard-dynamic-refresh-networkd-linux-latest" = {
+        problem.broken.reason = ''
+          https://buildbot.ngi.nixos.org/#/builders/987/builds/1
+        '';
+      };
+    };
 }
