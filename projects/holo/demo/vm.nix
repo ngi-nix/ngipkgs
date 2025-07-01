89674641ffd573d23ba9f37198534aa9aa56a270
eljamm <fedi.jamoussi@protonmail.ch>
2025-07-01 09:59:30 +0200

    projects/holo: add VM demo instructions

--- projects/holo/demo/vm.nix
+++
@@ -1,9 +0,0 @@
-{ ... }:
-{
-  programs.holo.enable = true;
-  services.holo-daemon.enable = true;
-
-  #TODO: Move this to the module confgiguration
-  #      It is only needed for the demo
-  services.getty.autologinUser = "root";
-}
