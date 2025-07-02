386ff744a44ab2e32362e0a907fc706baa13b79a
Ivan Mincik <ivan.mincik@gmail.com>
2025-07-02 18:41:52 +0200

    nominatim: add service example and demo

--- projects/Nominatim/default.nix
+++ projects/Nominatim/default.nix
@@ -38,9 +38,46 @@
   nixos.modules.services = {
     nominatim = {
       name = "nominatim";
-      module = null;
+      module = lib.moduleLocFromOptionString "services.nominatim";
+      examples."Nominatim service with API, UI and CLI support" = {
+        module = ./services/nominatim/examples/basic.nix;
+        tests.basic.module = import ./services/nominatim/tests/basic.nix args;
+      };
     };
   };
 
-  nixos.demo = null;
+  nixos.demo.vm = {
+    module = ./services/nominatim/examples/basic.nix;
+    description = ''
+      A demo VM for testing Nominatim.
+
+      1. First, download some OpenStreetMap data from
+      [Geofabrik.de](https://download.geofabrik.de/), for example Andorra.
+
+      $ cd /tmp
+      $ wget https://download.geofabrik.de/europe/andorra-latest.osm.pbf
+
+      2. Import data to Nominatim database
+
+      $ sudo -u nominatim \
+        NOMINATIM_DATABASE_WEBUSER=nominatim-api \
+        nominatim import --continue import-from-file --osm-file andorra-latest.osm.pbf
+
+      3. Restart Nominatim service
+
+      $ sudo systemctl restart nominatim.service
+
+      4. Test Nominatim API in terminal
+
+      $ curl -k https://localhost:8443/status
+      $ curl -k "https://localhost:8443/search?q=Andorra&format=geojson"
+
+      5. Test Nominatim UI in browser
+
+      $ open https://localhost:8443
+    '';
+
+    # FIXME:
+    # tests.demo.module = import "${sources.inputs.nixpkgs}/nixos/tests/nominatim.nix" args;
+  };
 }
