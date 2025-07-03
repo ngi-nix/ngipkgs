2d82e0752c90ea133c043c86caef4ac69ad58201
OPNA2608 <opna2608@protonmail.com>
2025-07-03 15:56:46 +0200

    Blink: Add metadata, module, example and VM test

---
+++ projects/Blink/default.nix
@@ -0,0 +1,33 @@
+{
+  lib,
+  pkgs,
+  sources,
+  ...
+}@args:
+{
+  metadata = {
+    summary = ''
+      Blink is a mature open source real-time communication application that can be used on different operating systems,
+      based on the IETF SIP standard. It offers audio, video, instant messaging and desktop sharing. It supports
+      end-to-end asynchronous messaging and end-to-end encryption which works both online (OTR) and offline (OpenPGP).
+    '';
+    subgrants = [
+      "BlinkRELOAD"
+      "Blink-OTR-OpenPGP"
+      "Blink-Windows"
+    ];
+  };
+
+  nixos = {
+    modules.programs.blink = {
+      module = ./module.nix;
+      examples.basic = {
+        description = ''
+          Example for how to enable Blink.
+        '';
+        module = ./examples/basic.nix;
+        tests.basic.module = import ./tests/basic.nix args;
+      };
+    };
+  };
+}
