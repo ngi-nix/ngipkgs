305e5104237a2d735f347ef804658d86d409a77b
Sizhe Zhao <prc.zhao@outlook.com>
2025-07-04 11:36:09 +0800

    projects/NodeBB: move instructions to module

--- projects/NodeBB/services/nodebb/examples/postgresql.nix
+++ projects/NodeBB/services/nodebb/examples/postgresql.nix
@@ -20,11 +20,4 @@
   services.postgresql.initialScript = pkgs.writeText "init-sql-script" ''
     CREATE ROLE nodebb LOGIN PASSWORD 'nodebb';
   '';
-
-  # demo-vm
-  programs.bash.interactiveShellInit = ''
-    echo "NodeBB is starting. Please wait ..."
-    until systemctl show nodebb.service | grep -q ActiveState=active; do sleep 1; done
-    echo "NodeBB is ready at http://localhost:4567"
-  '';
 }
