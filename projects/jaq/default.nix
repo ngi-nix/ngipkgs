6eaf6d99ffbdb1ba84bf965a82bfac8ef566cbb9
Ethan Carter Edwards <ethan@ethancedwards.com>
2025-07-08 14:03:37 -0400

    projects/jaq: add full description
    Signed-off-by: Ethan Carter Edwards <ethan@ethancedwards.com>

--- projects/jaq/default.nix
+++ projects/jaq/default.nix
@@ -28,7 +28,24 @@
 
   nixos.demo.shell = {
     module = ./programs/jaq/examples/basic.nix;
-    description = "jaq example";
+    description = ''
+      A demo shell for testing jaq, a data wrangling tool with formal semantics similar to jq.
+
+      Let's get a basic understanding of jaq's capabilities:
+
+      Here, we can pass in a set or object full of numbers and have jaq add them together.
+
+      $ echo '{"a": 1, "b": 2}' | jaq 'add' # equal to 3
+
+      We can also do more complicated operations like mapping over a list and using conditionals:
+
+      $ echo '[0, 1, 2, 3]' | jaq 'map(.*2) | [.[] | select(. < 5)] | add' # equal to 6
+
+      Finally, jaq will fail if any malformed input is passed
+
+      $ echo "0, 1, 4, " | jaq
+    '';
+
     tests.basic.module = import ./programs/jaq/tests/shell.nix args;
   };
 }
