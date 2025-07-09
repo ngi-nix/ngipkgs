a8f0777cfed86ca18c72a36d43b0ee7622f33bfa
eljamm <fedi.jamoussi@protonmail.ch>
2025-07-09 18:57:29 +0200

    Galene: add demo; use NixOS tests

--- projects/Galene/example.nix
+++ projects/Galene/example.nix
@@ -1,5 +1,39 @@
-{ ... }:
 {
-  services.galene.enable = true;
-  services.galene.insecure = true;
+  lib,
+  pkgs,
+  ...
+}:
+let
+  galeneTestGroupsDir = "/var/lib/galene/groups";
+  galeneTestGroupFile = "galene-test-config.json";
+  galenePort = 8443;
+  galeneTestGroupAdminName = "admin";
+  galeneTestGroupAdminPassword = "1234";
+in
+{
+  services.galene = {
+    enable = true;
+    insecure = true;
+    openFirewall = true;
+    httpPort = galenePort;
+    groupsDir = galeneTestGroupsDir;
+  };
+
+  # https://galene.org/INSTALL.html
+  environment.etc.${galeneTestGroupFile}.source =
+    (pkgs.formats.json { }).generate galeneTestGroupFile
+      {
+        op = [
+          {
+            username = galeneTestGroupAdminName;
+            password = galeneTestGroupAdminPassword;
+          }
+        ];
+        other = [ { } ];
+      };
+
+  environment.systemPackages = with pkgs; [
+    ffmpeg
+    galene-stream
+  ];
 }
