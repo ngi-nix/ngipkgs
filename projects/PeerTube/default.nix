0b6a1b7fde30c7eb3bf2674ff4a982ba7ddb8281
June Stepp <git@junestepp.me>
2025-07-11 20:19:28 -0500

    projects/PeerTube: add demo
    Simplify example and use it for demo.
    The tests still run for all plugins.

--- projects/PeerTube/default.nix
+++ projects/PeerTube/default.nix
@@ -28,36 +28,52 @@
     };
   };
 
-  nixos.modules = {
-    programs = {
-      peertube-cli = {
-        module = ./programs/peertube-cli/module.nix;
-        examples.basic-cli = {
-          module = ./programs/peertube-cli/examples/basic.nix;
-          description = ''
-            Enable peertube-cli, a tool for remotely managing PeerTube instances
-          '';
-          tests.basic-cli.module = import ./programs/peertube-cli/tests/basic.nix args;
+  nixos = {
+    modules = {
+      programs = {
+        peertube-cli = {
+          module = ./programs/peertube-cli/module.nix;
+          examples.basic-cli = {
+            module = ./programs/peertube-cli/examples/basic.nix;
+            description = ''
+              Enable peertube-cli, a tool for remotely managing PeerTube instances
+            '';
+            tests.basic-cli.module = import ./programs/peertube-cli/tests/basic.nix args;
+          };
+          links = {
+            docs = {
+              text = "Documentation";
+              url = "https://docs.joinpeertube.org/maintain/tools#remote-peertube-cli";
+            };
+          };
         };
-        links = {
-          docs = {
-            text = "Documentation";
-            url = "https://docs.joinpeertube.org/maintain/tools#remote-peertube-cli";
+      };
+
+      services = {
+        peertube = {
+          module = ./services/peertube/module.nix;
+          examples.basic = {
+            module = ./services/peertube/examples/basic.nix;
+            description = "Basic server configuration";
+            tests.peertube-plugins.module = import ./services/peertube/tests/peertube-plugins.nix args;
+            tests.peertube-plugin-livechat.module = import ./services/peertube/tests/peertube-plugin-livechat.nix args;
           };
         };
       };
     };
+    demo.vm = {
+      module = ./services/peertube/examples/basic.nix;
+      description = ''
+        PeerTube service demo.
 
-    services = {
-      peertube = {
-        module = ./services/peertube/module.nix;
-        examples.basic = {
-          module = ./services/peertube/examples/basic.nix;
-          description = "Basic configuration mainly used for testing purposes";
-          tests.peertube-plugins.module = import ./services/peertube/tests/peertube-plugins.nix args;
-          tests.peertube-plugin-livechat.module = import ./services/peertube/tests/peertube-plugin-livechat.nix args;
-        };
-      };
+        The web UI is available at http://localhost:9000/.
+
+        You can log in with:
+          - username: root
+          - password: changeme
+      '';
+      tests.peertube-plugins.module = import ./services/peertube/tests/peertube-plugins.nix args;
+      tests.peertube-plugin-livechat.module = import ./services/peertube/tests/peertube-plugin-livechat.nix args;
     };
   };
 }
