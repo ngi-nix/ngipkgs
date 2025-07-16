f4fc801ea73ea5338dbe39635de09cf1105c7b55
Ethan Carter Edwards <ethan@ethancedwards.com>
2025-07-15 20:17:21 -0400

    shell: add ngipkgs-test to test nixpkgs pr's against ngipkgs
    Based off a discussion in Matrix.
    
    Co-authored-by: Valentin Gagarin <valentin@gagarin.work>
    Signed-off-by: Ethan Carter Edwards <ethan@ethancedwards.com>

--- default.nix
+++ default.nix
@@ -304,6 +304,20 @@ rec {
       (pkgs.devmode.override {
         buildArgs = "-A overview --show-trace -v";
       })
+
+      (pkgs.writeShellApplication {
+        # TODO: have the program list available tests
+        name = "ngipkgs-test";
+        text = ''
+          export pr="$1"
+          export proj="$2"
+          export test="$3"
+          # remove the first args and feed the rest (for example flags)
+          export args="''${*:4}"
+
+          nix build --override-input nixpkgs "github:NixOS/nixpkgs?ref=pull/$pr/merge" .#checks.x86_64-linux.projects/"$proj"/nixos/tests/"$test" "$args"
+        '';
+      })
     ];
   };
 
