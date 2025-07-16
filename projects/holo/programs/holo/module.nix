4c7e85fbdcc8f38956f4c52a3af2b33daa5d8c01
eljamm <fedi.jamoussi@protonmail.ch>
2025-07-16 11:16:02 +0200

    Holo: fix holo-cli
    A more up-to-date version is needed for holo-daemon

--- projects/holo/programs/holo/module.nix
+++ projects/holo/programs/holo/module.nix
@@ -15,7 +15,22 @@ in
 
   config = lib.mkIf cfg.enable {
     environment.systemPackages = with pkgs; [
-      cfg.package
+      # FIX: remove after this is merged:
+      # https://github.com/NixOS/nixpkgs/pull/425724
+      (cfg.package.overrideAttrs rec {
+        pname = "holo-cli";
+        version = "0.5.0-unstable-2025-07-01";
+        src = fetchFromGitHub {
+          owner = "holo-routing";
+          repo = "holo-cli";
+          rev = "f04c1d0dcd6d800e079f33b8431b17fa00afeeb1";
+          hash = "sha256-ZJeXGT5oajynk44550W4qz+OZEx7y52Wwy+DYzrHZig=";
+        };
+        cargoDeps = rustPlatform.fetchCargoVendor {
+          inherit pname version src;
+          hash = "sha256-bsoxWjOMzRRtFGEaaqK0/adhGpDcejCIY0Pzw1HjQ5U=";
+        };
+      })
     ];
   };
 }
