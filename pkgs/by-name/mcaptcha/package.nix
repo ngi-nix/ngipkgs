738a3da03e4c8f1b52118bf4a2197881efe58596
OPNA2608 <opna2608@protonmail.com>
2025-07-28 22:09:51 +0200

    mcaptcha: Apply patch to fix flaky tests
    Sometimes, mcaptcha_cache is not the first entry in the module listing that libmcaptcha receives. In that case, it
    determines that the module is not loaded, and mcaptcha crashes.
    
    Check all returned modules, not just the first.

--- pkgs/by-name/mcaptcha/package.nix
+++ pkgs/by-name/mcaptcha/package.nix
@@ -192,4 +192,19 @@ let
       };
     };
 in
-mcaptcha
+# Temporary fix: libmcaptcha crashes when redis doesn't reply to a module listing request with the mcaptcha module as
+# the first entry
+# Until this is addressed upstream and has found its way into the version of libmcaptcha used in mcaptcha, add our patch
+mcaptcha.overrideAttrs (oa: {
+  cargoDeps = oa.cargoDeps.overrideAttrs (oa2: {
+    buildCommand = oa2.buildCommand + ''
+      realdir="$(realpath $out/libmcaptcha-0.2.4)"
+      rm "$out/libmcaptcha-0.2.4"
+      cp -r --no-preserve=mode "$realdir" "$out/libmcaptcha-0.2.4"
+
+      pushd "$out/libmcaptcha-0.2.4"
+      patch -p1 < ${./1001-libmcaptcha-Allow-redis-module-to-not-be-first-in-list.patch}
+      popd
+    '';
+  });
+})
