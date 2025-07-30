74a5a6f1c922e36ab284d203e5da2a882f4f3027
OPNA2608 <opna2608@protonmail.com>
2025-07-31 00:20:44 +0200

    heads: Add more boards that build, add more support for further boards
    - Added more deps, so TPM2 Qemu targets build
    - Added all Qemu TPM2 targets to allowedBoards
    - Changed generateBoards & generic functions to take details about blobs usage & target arch
    - Added attrset to override blobs usage & target arch default values, with entry for Talos II board
    - Comment out a patch for Talos II coreboot that makes patch from Heads not apply
      Haven't debugged what the one in Heads is trying to change, because the Talos II target needs a dependency
      with no known-good source.

--- pkgs/by-name/heads/patches/coreboot-talos_2/default.nix
+++ pkgs/by-name/heads/patches/coreboot-talos_2/default.nix
@@ -11,10 +11,15 @@
     name = "0001-coreboot-Make-build-verbose.patch";
     patch = ./0001-coreboot-Make-build-verbose.patch;
   }
-  {
-    name = "0002-coreboot-Hardcode-build-and-host-configure-flags.patch";
-    patch = replaceVars ./0002-coreboot-Hardcode-build-and-host-configure-flags.patch.in {
-      inherit buildConfig hostConfig;
-    };
-  }
+  # TODO
+  # This patch conflicts with a patch that Heads wants to apply. Haven't checked if their patch might already address
+  # what this one is doing, because the board target currently doesn't build.
+  /*
+    {
+      name = "0002-coreboot-Hardcode-build-and-host-configure-flags.patch";
+      patch = replaceVars ./0002-coreboot-Hardcode-build-and-host-configure-flags.patch.in {
+        inherit buildConfig hostConfig;
+      };
+    }
+  */
 ]
