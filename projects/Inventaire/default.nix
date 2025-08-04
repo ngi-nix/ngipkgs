f08562475266dce36e4602b915455bb246abeb26
eljamm <fedi.jamoussi@protonmail.ch>
2025-08-04 12:39:37 +0200

    Inventaire: unbreak test

--- projects/Inventaire/default.nix
+++ projects/Inventaire/default.nix
@@ -11,11 +11,6 @@ let
     '';
     module = ./examples/basic.nix;
     tests.basic.module = import ./tests/basic.nix args;
-    tests.basic.problem.broken.reason = ''
-      couchdb fails to build
-
-      https://github.com/NixOS/nixpkgs/issues/425717
-    '';
   };
 in
 {
