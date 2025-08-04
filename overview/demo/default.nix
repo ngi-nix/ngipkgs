1f3a91a41bc832d3c97f708af85c083dc7aaa04f
eljamm <fedi.jamoussi@protonmail.ch>
2025-08-04 10:47:15 +0200

    Extract demo shell specifics from nixos modules
    Co-authored-by: Valentin Gagarin <valentin@gagarin.work>

--- overview/demo/default.nix
+++ overview/demo/default.nix
@@ -3,33 +3,24 @@
   pkgs,
   system,
   sources,
-  extendedNixosModules,
+  nixos-modules,
+  demo-modules,
 }:
-let
+rec {
   eval =
-    demo-module:
+    module:
     import (sources.nixpkgs + "/nixos/lib/eval-config.nix") {
       inherit system;
       modules = [
-        demo-module
+        module
         (sources.nixpkgs + "/nixos/modules/profiles/qemu-guest.nix")
         (sources.nixpkgs + "/nixos/modules/virtualisation/qemu-vm.nix")
-        ./shell.nix
-        ./vm
-        {
-          options.demo = lib.mkOption {
-            type = lib.types.bool;
-            description = "Whether the configuration should run as a demo.";
-            default = false;
-          };
-          config.demo = true;
-        }
       ]
-      ++ extendedNixosModules;
+      ++ nixos-modules
+      ++ demo-modules;
       specialArgs = { inherit sources; };
     };
-in
-{
+
   demo-vm =
     module:
     pkgs.writeShellScript "demo-vm" ''
