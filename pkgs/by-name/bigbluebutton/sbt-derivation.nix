687a83500b418040498cb8837f4647b6a816a4cc
OPNA2608 <opna2608@protonmail.com>
2025-08-05 18:04:51 +0200

    bigbluebutton.bbb-etherpad: init at 3.0.10-bigbluebutton

--- pkgs/by-name/bigbluebutton/sbt-derivation.nix
+++ pkgs/by-name/bigbluebutton/sbt-derivation.nix
@@ -2,17 +2,26 @@
   stdenv,
   lib,
   fetchFromGitHub,
-  runCommandNoCC,
+  buildNpmPackage,
   mkSbtDerivation,
+  runCommand,
+  stdenvNoCC,
   bashNonInteractive,
   dpkg,
   fakeroot,
   fpm,
-  makeWrapper,
   jdk,
+  libsecret,
+  makeWrapper,
+  nodejs,
+  npmHooks,
+  pkg-config,
+  pnpm_9,
 }:
 
 let
+  pnpm = pnpm_9;
+
   # Defined as git clone commands in the *.placeholder.sh files in BBB root
   externalDeps = [
     {
@@ -104,18 +113,30 @@ let
     tag = "v3.0.10";
     hash = "sha256-r1s+5AFwBrbIUOC+zuWPWNWqiuzHWgBDrWV8JN5bNGM=";
   };
-  src = runCommandNoCC "bigbluebutton-src" { } ''
-    cp -vr ${srcBare} $out
-    chmod +w $out
 
-    ${lib.strings.concatMapStringsSep "\n" (dep: "cp -vr ${dep.src} $out/${dep.name}") externalDeps}
-  '';
+  sharedBbbThings = {
+    src = runCommand "bigbluebutton-src" { } ''
+      cp -vr ${srcBare} $out
+      chmod +w $out
+
+      ${lib.strings.concatMapStringsSep "\n" (dep: "cp -vr ${dep.src} $out/${dep.name}") externalDeps}
+    '';
+
+    postPatch = ''
+      patchShebangs build/setup-inside-docker.sh build/packages-template
+
+      # This is for setting up cache persistency in docker across runs. We don't want this.
+      substituteInPlace build/setup-inside-docker.sh \
+        --replace-fail 'ln -s "''${SOURCE}/cache/''${dir}" "/root/''${dir}"' '#ln -s "''${SOURCE}/cache/''${dir}" "/root/''${dir}"' \
+        --replace-fail 'CACHE_DIR="/root/"' 'CACHE_DIR="''${SOURCE}/cache/"'
+    '';
+  };
 
   bbb-common-message = mkSbtDerivation {
     pname = "bbb-common-message";
     version = "3.0.10-bigbluebutton";
 
-    inherit src;
+    inherit (sharedBbbThings) src postPatch;
 
     overrideDepsAttrs = final: prev: {
       preBuild = ''
@@ -129,15 +150,6 @@ let
     depsArchivalStrategy = "copy";
     depsOptimize = false;
     depsSha256 = "sha256-SwtcQrEicIggDBAL51xeHnnPg21Z4wlq8ZyatcFe320=";
-
-    postPatch = ''
-      patchShebangs build/setup-inside-docker.sh build/packages-template
-
-      # This is for setting up cache persistency in docker across runs. We don't want this.
-      substituteInPlace build/setup-inside-docker.sh \
-        --replace-fail 'ln -s "''${SOURCE}/cache/''${dir}" "/root/''${dir}"' '#ln -s "''${SOURCE}/cache/''${dir}" "/root/''${dir}"' \
-        --replace-fail 'CACHE_DIR="/root/"' 'CACHE_DIR="''${SOURCE}/cache/"'
-    '';
 
     strictDeps = true;
 
@@ -165,7 +177,7 @@ let
     pname = "bbb-apps-akka";
     version = "3.0.10-bigbluebutton";
 
-    inherit src;
+    inherit (sharedBbbThings) src;
 
     overrideDepsAttrs = final: prev: {
       preBuild = ''
@@ -187,14 +199,7 @@ let
     depsOptimize = false;
     depsSha256 = "sha256-duIlX1aEOIfM66Eo4+A0ZdJJ3PLnAbSEC8N9DUUH8Y0=";
 
-    postPatch = ''
-      patchShebangs build/setup-inside-docker.sh build/packages-template
-
-      # This is for setting up cache persistency in docker across runs. We don't want this.
-      substituteInPlace build/setup-inside-docker.sh \
-        --replace-fail 'ln -s "''${SOURCE}/cache/''${dir}" "/root/''${dir}"' '#ln -s "''${SOURCE}/cache/''${dir}" "/root/''${dir}"' \
-        --replace-fail 'CACHE_DIR="/root/"' 'CACHE_DIR="''${SOURCE}/cache/"'
-
+    postPatch = sharedBbbThings.postPatch + ''
       # Skipping version mangling & building of dependencies
       substituteInPlace build/packages-template/bbb-apps-akka/build.sh \
         --replace-fail 'EPHEMERAL_VERSION=0.0.$(date +%s)-SNAPSHOT' 'cat <<EOF >/dev/null' \
@@ -262,16 +267,7 @@ let
     pname = "bbb-config";
     version = "3.0.10-bigbluebutton";
 
-    inherit src;
-
-    postPatch = ''
-      patchShebangs build/setup-inside-docker.sh build/packages-template
-
-      # This is for setting up cache persistency in docker across runs. We don't want this.
-      substituteInPlace build/setup-inside-docker.sh \
-        --replace-fail 'ln -s "''${SOURCE}/cache/''${dir}" "/root/''${dir}"' '#ln -s "''${SOURCE}/cache/''${dir}" "/root/''${dir}"' \
-        --replace-fail 'CACHE_DIR="/root/"' 'CACHE_DIR="''${SOURCE}/cache/"'
-    '';
+    inherit (sharedBbbThings) src postPatch;
 
     strictDeps = true;
 
@@ -310,7 +306,253 @@ let
     '';
 
   });
+
+  bbb-etherpad =
+    let
+      plainNpmPackage =
+        {
+          pname,
+          version,
+          src,
+          sourceRoot ? ".",
+          npmPackageName ? pname,
+        }:
+
+        runCommand "${pname}-${version}"
+          {
+            passthru = {
+              inherit npmPackageName;
+            };
+          }
+          ''
+            mkdir -p $out/lib/node_modules
+            cp -r --no-preserve=all ${src}/${sourceRoot} $out/lib/node_modules/${npmPackageName}
+          '';
+
+      # These get fetched from HEAD, packed & installed during the build script
+      npmExtraDeps = {
+        ep_pad_ttl = plainNpmPackage {
+          pname = "ep_pad_ttl";
+          version = "0-unstable-2021-03-21";
+
+          src = fetchFromGitHub {
+            owner = "mconf";
+            repo = "ep_pad_ttl";
+            rev = "360136cd38493dd698435631f2373cbb7089082d";
+            hash = "sha256-cAeEiLULVQjMyd+2LBPIx3zS82Jcsi0FHvjDRAdi/F0=";
+          };
+        };
+
+        bbb-etherpad-plugin = plainNpmPackage {
+          pname = "bbb-etherpad-plugin";
+          version = "0-unstable-2022-11-11";
+
+          src = fetchFromGitHub {
+            owner = "alangecker";
+            repo = "bbb-etherpad-plugin";
+            rev = "4dbc28d62c44742ffae79ce88c069802bc533068";
+            hash = "sha256-oUw+nIl4/29zOrB1GhuBenvdxLOPXANvMi7Tb9UOgvQ=";
+          };
+
+          npmPackageName = "ep_bigbluebutton_patches";
+        };
+
+        ep_redis_publisher = buildNpmPackage {
+          pname = "ep_redis_publisher";
+          version = "0.0.3-unstable-2023-07-24";
+
+          src = fetchFromGitHub {
+            owner = "mconf";
+            repo = "ep_redis_publisher";
+            rev = "2b6e47c1c59362916a0b2961a29b259f2977b694";
+            hash = "sha256-KQ+w2QUBNSB3dzBfb9PpbQ1ubDYioZvtAavCMSiobBc=";
+          };
+
+          npmDepsHash = "sha256-i/b3PWIUdZUhf5GejDvSjvMPIpytG80pz/k5JAPhNoE=";
+
+          postPatch = ''
+            cp -v ${./ep_redis_publisher.package-lock.json} package-lock.json
+          '';
+
+          dontBuild = true;
+
+          passthru.npmPackageName = "ep_redis_publisher";
+        };
+
+        ep_cursortrace = stdenvNoCC.mkDerivation (finalAttrs: {
+          pname = "ep_cursortrace";
+          version = "3.1.20-unstable-2025-01-22";
+
+          src = fetchFromGitHub {
+            owner = "mconf";
+            repo = "ep_cursortrace";
+            rev = "56fb8c2b211cdda4fc8715ec99e1cb7b7d9eb851";
+            hash = "sha256-rSjEhBpAV44iDZVPx48Rg/abHOYghx4T4rHl3QQYmyg=";
+          };
+
+          pnpmDeps = pnpm.fetchDeps {
+            inherit (finalAttrs) pname src;
+            fetcherVersion = 2;
+            hash = "sha256-78cgB+2+30blVIELhOrAyEwJkgIt8TO6CJTKiJFY5rk=";
+          };
+
+          strictDeps = true;
+
+          nativeBuildInputs = [
+            pnpm.configHook
+            nodejs
+            npmHooks.npmInstallHook
+          ];
+
+          # Seems to hang? Don't know why.
+          dontNpmPrune = true;
+
+          passthru.npmPackageName = "ep_cursortrace";
+        });
+
+        ep_disable_chat = plainNpmPackage rec {
+          pname = "ep_disable_chat";
+          version = "0.0.12";
+
+          src = fetchFromGitHub {
+            owner = "ether";
+            repo = "ether-plugins";
+            tag = "ep_disable_chat@v${version}";
+            hash = "sha256-8k4EbtlYQvbynsLEiE9ch0GgLTSikzEIyf5qDyoJnj8=";
+          };
+
+          sourceRoot = "ep_disable_chat";
+        };
+
+        ep_auth_session = plainNpmPackage {
+          pname = "ep_auth_session";
+          version = "1.1.1";
+
+          src = fetchFromGitHub {
+            owner = "Kurounin";
+            repo = "ep_auth_session";
+            # Not tagged
+            rev = "897767d8b077735def09dacd35e0070cce95eaf3";
+            hash = "sha256-FlZQiESCkLmK6ZuJ4pz20hS/huW/aOee+VDyOeiHYhA=";
+          };
+        };
+      };
+
+      bbb-etherpad-skin = fetchFromGitHub {
+        owner = "alangecker";
+        repo = "bbb-etherpad-skin";
+        rev = "91b052c2cc4c169f2e381538e4342e894f944dbe";
+        hash = "sha256-aQxntcI33SvCKbSmVnr9mEFnbHLezzTWSURtlNHSg4o=";
+      };
+    in
+    buildNpmPackage {
+      pname = "bbb-etherpad";
+      version = "3.0.10-bigbluebutton";
+
+      inherit (sharedBbbThings) src;
+
+      # > Error: Git dependency node_modules/sqlite3 contains install scripts, but has no lockfile, which is something that will probably break. Open an issue if you can't feasibly patch this dependency out, and we'll come up with a workaround.
+      # > If you'd like to attempt to try to use this dependency anyways, set `forceGitDeps = true`.
+      # Let's see if we're lucky, and just enable it.
+      forceGitDeps = true;
+      npmDepsHash = "sha256-dPoRhp1ex9ohMZ/s26C1EBkAuKVX8OqmuV1OeeI1U+8=";
+
+      postPatch = sharedBbbThings.postPatch + ''
+        # Don't install a different npm
+        # Don't try to install npm deps
+        # Use pre-downloaded skin
+        # Skip all other git clones
+        # Skip packing of additional npm deps
+        substituteInPlace build/packages-template/bbb-etherpad/build.sh \
+          --replace-fail 'npm i -g npm@6.14.11' 'echo "Not installing a different NPM version"' \
+          --replace-fail 'bin/installDeps.sh' 'echo "Letting Nix set up the base NPM deps"' \
+          --replace-fail 'git clone https://github.com/alangecker/bbb-etherpad-skin.git' 'cp -r --no-preserve=all ${bbb-etherpad-skin}' \
+          --replace-fail 'git clone' 'echo Skipping cloning of:' \
+          --replace-fail 'npm pack' 'echo Skipping packing of:' \
+          ${
+            let
+              packedPackage = packageName: "./${packageName}-*.tgz";
+
+              installCall =
+                packageName:
+                "npm install "
+                + lib.optionalString (packageName == "ep_auth_session") "--no-save --legacy-peer-deps "
+                + (
+                  if (packageName != "ep_disable_chat" && packageName != "ep_auth_session") then
+                    packedPackage packageName
+                  else
+                    packageName
+                );
+            in
+            lib.strings.concatMapAttrsStringSep " " (
+              name: value:
+              "--replace-fail '${installCall value.passthru.npmPackageName}' 'ln -vs ${value}/lib/node_modules/${value.passthru.npmPackageName} src/node_modules/${value.passthru.npmPackageName}'"
+            ) npmExtraDeps
+          }
+
+        # For npmDeps to get generated properly
+        pushd bbb-etherpad/src
+        cp -v ${./etherpad-lite.package-lock.json} package-lock.json
+      '';
+
+      strictDeps = true;
+
+      nativeBuildInputs = [
+        dpkg
+        fpm
+        pkg-config
+      ];
+
+      buildInputs = [
+        libsecret
+      ];
+
+      preConfigure = ''
+        # No longer setting up npmDeps
+        popd
+      '';
+
+      buildPhase = ''
+        runHook preBuild
+
+        env LOCAL_BUILD=1 build/setup-inside-docker.sh bbb-etherpad
+
+        runHook postBuild
+      '';
+
+      # FIXME
+      # [ERROR] settings - soffice (libreoffice) does not exist at this path, check your settings file. File location: /usr/share/bbb-libreoffice-conversion/etherpad-export.sh
+      # Specified in $out/share/etherpad-lite/settings.json, will need to point at another package's out / a shared root.
+      installPhase = ''
+        runHook preInstall
+
+        dpkg -x artifacts/*.deb $out
+
+        # Fix up Debian-isms
+
+        # No usr please, we have the prefix for that
+        mv -vt $out/ $out/usr/*
+        rmdir $out/usr
+
+        substituteInPlace $out/lib/systemd/system/etherpad.service \
+          --replace-fail '/usr/share' "$out/share" \
+          --replace-fail '/usr/bin/node' '${lib.getExe nodejs}'
+
+        # Add Nix-isms
+
+        runHook postInstall
+      '';
+
+      passthru = {
+        inherit npmExtraDeps bbb-etherpad-skin;
+      };
+    };
 in
 {
-  inherit bbb-common-message bbb-apps-akka bbb-config;
+  inherit
+    bbb-common-message
+    bbb-apps-akka
+    bbb-config
+    bbb-etherpad
+    ;
 }
