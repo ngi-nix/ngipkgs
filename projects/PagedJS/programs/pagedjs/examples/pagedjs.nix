a1eabd3ee87891bd2abbf34d8a8d82fc4fcc238f
themadbit <marktanui75@gmail.com>
2025-08-08 12:17:33 +0300

    projects/PagedJS: implement demo example
    Signed-off-by: themadbit <marktanui75@gmail.com>

--- projects/PagedJS/programs/pagedjs/examples/pagedjs.nix
+++ projects/PagedJS/programs/pagedjs/examples/pagedjs.nix
@@ -1,4 +1,28 @@
-{ ... }:
 {
+  lib,
+  pkgs,
+  ...
+}:
+{
+  # Enable X11
+  services.xserver.enable = true;
+
   programs.pagedjs.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    evince # PDF viewer
+  ];
+
+  environment.etc."pagedjs.html".text = ''
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <title>PagedJS Example</title>
+      </head>
+      <body>
+        <h1>Hello, PagedJS!</h1>
+        <p>This is a simple example of using PagedJS.</p>
+      </body>
+    </html>
+  '';
 }
