cbec3e2947608f0838dfe6cc99ee5f576cdf57e3
themadbit <marktanui75@gmail.com>
2025-08-13 19:44:46 +0300

    projects/Briar: add package option to program module

--- projects/Briar/programs/briar/module.nix
+++ projects/Briar/programs/briar/module.nix
@@ -9,14 +9,15 @@ let
 in
 {
   options.programs.briar = {
-    enable = lib.mkEnableOption "briar";
+    enable = lib.mkEnableOption "Enable briar";
+    package = lib.mkPackageOption pkgs "briar-desktop" { };
   };
 
   config = lib.mkIf cfg.enable {
     environment.systemPackages =
       with pkgs;
       lib.optionals (lib.meta.availableOn stdenv.hostPlatform briar-desktop) [
-        briar-desktop
+        cfg.package
       ];
   };
 }
