a14d600b134d652874a811d2c3889f0917b36156
eljamm <fedi.jamoussi@protonmail.ch>
2025-08-14 12:34:28 +0200

    overview: render non-list subgrants

--- overview/content-types/metadata-subgrants.nix
+++ overview/content-types/metadata-subgrants.nix
@@ -3,7 +3,6 @@ let
   inherit (lib)
     mkOption
     types
-    optionalString
     ;
 
   types' = import ../../projects/types.nix { inherit lib; };
@@ -17,16 +16,45 @@ in
       readOnly = true;
       default =
         self:
-        (optionalString (self.subgrants != null && self.subgrants != [ ])) ''
-          <p>
-          This project is funded by NLnet through these subgrants:
+        let
+          inherit (lib)
+            concatMapAttrsStringSep
+            concatMapStrings
+            isAttrs
+            isList
+            optionalString
+            ;
+
+          subgrants-list = ''
             <ul>
-            ${lib.concatMapStrings (subgrant: ''
+            ${concatMapStrings (subgrant: ''
               <li>
-                <a href="https://nlnet.nl/project/${subgrant}">${subgrant}</a>
+              <a href="https://nlnet.nl/project/${subgrant}">${subgrant}</a>
               </li>
             '') self.subgrants}
             </ul>
+          '';
+
+          subgrants-attrset = ''
+            <dl class="subgrant-list">
+            ${concatMapAttrsStringSep "\n" (name: subgrants: ''
+              ${optionalString (subgrants != [ ]) ''
+                <dt>${name}</dt>
+                ${concatMapStrings (subgrant: ''
+                  <dd>
+                  <a href="https://nlnet.nl/project/${subgrant}">${subgrant}</a>
+                  </dd>
+                '') subgrants}
+              ''}
+            '') self.subgrants}
+            </dl>
+          '';
+        in
+        optionalString (self.subgrants != null) ''
+          <p>
+          This project is funded by NLnet through these subgrants:
+            ${optionalString (isList self.subgrants) subgrants-list}
+            ${optionalString (isAttrs self.subgrants) subgrants-attrset}
           </p>
         '';
     };
