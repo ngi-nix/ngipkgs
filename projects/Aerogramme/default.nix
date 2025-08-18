e6fcc08a67f6c22b3281f2fbdddc4af841cd8af3
themadbit <marktanui75@gmail.com>
2025-08-18 12:33:02 +0300

    projects/Aerogramme: clean up project
    - Move service module to it's own file
    - Clean up links and default.nix

--- projects/Aerogramme/default.nix
+++ projects/Aerogramme/default.nix
@@ -10,36 +10,30 @@
     subgrants = [
       "Aerogramme"
     ];
+    links = {
+      config = {
+        text = "Configuration reference";
+        url = "https://aerogramme.deuxfleurs.fr/documentation/reference/config/";
+      };
+      service-manager = {
+        text = "Using with service managers";
+        url = "https://aerogramme.deuxfleurs.fr/documentation/cookbook/service-manager/";
+      };
+      nixpkgs = {
+        text = "Nixpkgs derivation";
+        url = "https://github.com/NixOS/nixpkgs/blob/nixos-24.11/pkgs/by-name/ae/aerogramme/package.nix";
+      };
+    };
   };
 
-  nixos = {
-    modules.services.aerogramme = {
-      links = {
-        config = {
-          text = "Configuration reference";
-          url = "https://aerogramme.deuxfleurs.fr/documentation/reference/config/";
-        };
-        service-manager = {
-          text = "Using with service managers";
-          url = "https://aerogramme.deuxfleurs.fr/documentation/cookbook/service-manager/";
-        };
-        nixpkgs = {
-          text = "Nixpkgs derivation";
-          url = "https://github.com/NixOS/nixpkgs/blob/nixos-24.11/pkgs/by-name/ae/aerogramme/package.nix";
-        };
+  nixos.modules.services = {
+    aerogramme = {
+      module = ./services/module.nix;
+      examples."Enable aerogramme" = {
+        module = ./services/examples/basic.nix;
+        description = "";
+        tests.basic.module = null;
       };
-      module =
-        { lib, pkgs, ... }:
-        {
-          options.services.aerogramme = {
-            enable = lib.mkEnableOption "Aerogramme";
-            package = lib.mkPackageOption pkgs "aerogramme" { };
-          };
-          # TODO: add a service definition
-          # FIX: figure out how to express that something doesn't work as intended yet
-          # meta.broken = true;
-        };
     };
-    examples.basic.module = null;
   };
 }
