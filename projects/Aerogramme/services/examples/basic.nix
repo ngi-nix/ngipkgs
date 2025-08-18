85d23196b4fc2f55294361e4f85a45a0474f6819
themadbit <marktanui75@gmail.com>
2025-08-18 12:33:30 +0300

    projects/Aerogramme: add service module example

---
+++ projects/Aerogramme/services/examples/basic.nix
@@ -0,0 +1,6 @@
+{
+  ...
+}:
+{
+  services.aerogramme.enable = true;
+}
--- projects/Aerogramme/services/module.nix
+++ projects/Aerogramme/services/module.nix
@@ -5,6 +5,4 @@
     package = lib.mkPackageOption pkgs "aerogramme" { };
   };
   # TODO: add a service definition
-  # FIX: figure out how to express that something doesn't work as intended yet
-  # meta.broken = true;
 }
