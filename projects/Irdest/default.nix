8ca338c62c110f4a79431d276e39bbc1f1e57241
June Stepp <git@junestepp.me>
2025-08-25 20:40:10 -0500

    projects/Irdest: add missing deliverables info

--- projects/Irdest/default.nix
+++ projects/Irdest/default.nix
@@ -28,17 +28,28 @@
     };
   };
 
+  # https://github.com/ngi-nix/ngipkgs/issues/1512
+  binary.lora-modem-firmware.data = null;
+
   nixos.modules = {
-    services.ratmand = {
-      module = ./services/ratmand/module.nix;
-      examples.basic-ratmand = {
-        module = ./services/ratmand/examples/basic.nix;
-        description = "Basic ratmand configuration";
-        tests = {
-          ratmand-config.module = import ./services/ratmand/tests/config.nix args;
-          peer-communication.module = import ./services/ratmand/tests/peer-communication.nix args;
+    # https://github.com/ngi-nix/ngipkgs/issues/1514
+    programs.irdest-mblog.module = null;
+    services = {
+      ratmand = {
+        module = ./services/ratmand/module.nix;
+        examples.basic-ratmand = {
+          module = ./services/ratmand/examples/basic.nix;
+          description = "Basic ratmand configuration";
+          tests = {
+            ratmand-config.module = import ./services/ratmand/tests/config.nix args;
+            peer-communication.module = import ./services/ratmand/tests/peer-communication.nix args;
+          };
         };
       };
+      # https://github.com/ngi-nix/ngipkgs/issues/1511
+      irdest-proxy.module = null;
+      # https://github.com/ngi-nix/ngipkgs/issues/1513
+      irdest-echo.module = null;
     };
   };
 }
