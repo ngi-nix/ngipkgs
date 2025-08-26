73cebd32162c4ead6bfb56aea6cec784973b86f0
June Stepp <git@junestepp.me>
2025-08-25 19:01:30 -0500

    projects/Irdest: init ratmand module

---
+++ projects/Irdest/services/ratmand/module.nix
@@ -0,0 +1,107 @@
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+let
+  cfg = config.services.ratmand;
+
+  # Convert `cfg.settings` to arguments for `ratmand generate` to generate the config
+  # file.
+  configPatchArgs =
+    lib.concatLists (
+      (lib.mapAttrsToList (
+        section: sectionAttrs:
+        lib.mapAttrsToList (
+          key: value:
+          ''--patch "${section}/${key}=${
+            if lib.isBool value then lib.boolToString value else toString value
+          }"''
+        ) sectionAttrs
+      ) (cfg.settings // { ratmand = lib.removeAttrs (cfg.settings.ratmand or { }) [ "peers" ]; }))
+    )
+    ++ lib.map (peer: ''--add-peer "${peer}"'') cfg.settings.ratmand.peers;
+
+  configFile = pkgs.runCommand "ratmand-config" { } ''
+    ${lib.getExe' cfg.package "ratmand"} --config "$out" generate ${lib.concatStringsSep " " configPatchArgs}
+  '';
+in
+{
+  options.services.ratmand = {
+    enable = lib.mkEnableOption "ratmand, a decentralised peer-to-peer packet router";
+
+    package = lib.mkPackageOption pkgs "ratman" { };
+
+    settings = lib.mkOption {
+      type = lib.types.submodule (
+        { config, ... }:
+        {
+          freeformType =
+            with lib.types;
+            attrsOf (
+              attrsOf (oneOf [
+                str
+                bool
+                int
+              ])
+            );
+          options = {
+            ratmand.peers = lib.mkOption {
+              type = with lib.types; listOf str;
+              default = [ ];
+              example = [
+                "inet:hyperion.kookie.space:5860"
+                "inet:hub.irde.st:5860"
+              ];
+              description = ''
+                Initial peers. The format is: `<driver>:<hostname/ip address>:[<port>]`.
+              '';
+            };
+          };
+        }
+      );
+      default = { };
+      example = {
+        ratmand = {
+          accept_unknown_peers = true;
+          peers = [
+            "inet:hyperion.kookie.space:5860"
+            "inet:hub.irde.st:5860"
+          ];
+        };
+        lan.enable = false;
+      };
+      description = ''
+        Configuration for ratmand.
+
+        See available options at https://codeberg.org/irdest/irdest/src/branch/main/ratman/src/config/ratmand-0.5.kdl.
+      '';
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    # Make ratman-tools available
+    environment.systemPackages = [ cfg.package ];
+
+    systemd.services.ratmand = {
+      description = "Decentralized peer-to-peer packet router";
+      wantedBy = [ "multi-user.target" ];
+      wants = [ "network-online.target" ];
+      after = [ "network-online.target" ];
+
+      serviceConfig = {
+        ExecStart = ''
+          ${lib.getExe' cfg.package "ratmand"} \
+            --config "${configFile}" \
+            --dir "$STATE_DIRECTORY"
+        '';
+        DynamicUser = true;
+        Restart = "always";
+        RestartSec = 1;
+        StateDirectory = "ratmand";
+        StateDirectoryMode = "0700";
+      };
+    };
+  };
+}
---
+++ projects/Irdest/services/ratmand/tests/peer-communication.nix
@@ -0,0 +1,76 @@
+{
+  sources,
+  lib,
+  ...
+}:
+let
+  sharedConfig = {
+    imports = [
+      sources.modules.ngipkgs
+      sources.modules.services.ratmand
+      sources.examples.Irdest.basic-ratmand
+    ];
+
+    # ratmand inet driver
+    networking.firewall.allowedTCPPorts = [ 5860 ];
+
+    # ratmand fails to allocate all of its memory with only 1024
+    virtualisation.memorySize = 1536;
+  };
+
+  serverIP = "192.168.2.10";
+  clientIP = "192.168.2.11";
+in
+{
+  name = "ratmand-peer-communication";
+
+  nodes = {
+    server = lib.recursiveUpdate sharedConfig {
+      networking.interfaces.eth1 = {
+        ipv4.addresses = [
+          {
+            address = serverIP;
+            prefixLength = 24;
+          }
+        ];
+      };
+      services.ratmand.settings.ratmand.accept_unknown_peers = true;
+    };
+    client = lib.recursiveUpdate sharedConfig {
+      networking.interfaces.eth1 = {
+        ipv4.addresses = [
+          {
+            address = clientIP;
+            prefixLength = 24;
+          }
+        ];
+      };
+      services.ratmand.settings.ratmand.peers = [ "inet:${serverIP}:5860" ];
+    };
+  };
+
+  testScript =
+    { nodes, ... }:
+    ''
+      server.start()
+      server.wait_for_unit("ratmand.service")
+      # API isn't available immediately after the service is ready.
+      server.succeed("sleep 3")
+
+      client.start()
+      client.wait_for_unit("ratmand.service")
+      # API isn't available immediately after the service is ready.
+      client.succeed("sleep 3")
+
+      server.succeed("ratctl addr create", "ratctl addr up")
+      server_address = server.succeed("ratctl addr list").strip()
+      server.succeed("ratcat recv > ratcat_output &")
+
+      client.succeed("ratctl addr create", "ratctl addr up")
+      client.succeed("echo 'Greetings my trusty VM compatriot!' | "
+                    f"ratcat send --addr {server_address}")
+      server.wait_until_succeeds(
+        "cat ratcat_output | grep 'Greetings my trusty VM compatriot!'"
+      )
+    '';
+}
