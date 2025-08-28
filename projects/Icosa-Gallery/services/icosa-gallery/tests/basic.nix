7213039d047547e71b74dc2b9715cf54faea256a
Sizhe Zhao <prc.zhao@outlook.com>
2025-08-28 12:45:55 +0800

    projects/Icosa-Gallery: add basic test

--- projects/Icosa-Gallery/services/icosa-gallery/tests/basic.nix
+++ projects/Icosa-Gallery/services/icosa-gallery/tests/basic.nix
@@ -20,9 +20,40 @@
 
   testScript =
     { nodes, ... }:
+    let
+      port = toString nodes.machine.services.icosa-gallery.port;
+    in
     ''
       start_all()
 
-      machine.succeed()
+      machine.wait_for_unit("icosa-gallery.service")
+      machine.wait_for_open_port(${port})
+
+      machine.succeed("curl -v http://localhost:${port} >&2")
     '';
+
+  interactive.sshBackdoor.enable = true;
+  interactive.nodes = {
+    machine =
+      { config, ... }:
+      {
+        services.icosa-gallery.host = "0.0.0.0";
+
+        # forward ports from VM to host
+        virtualisation.forwardPorts =
+          let
+            inherit (config.services.icosa-gallery) port;
+          in
+          [
+            {
+              from = "host";
+              host = { inherit port; };
+              guest = { inherit port; };
+            }
+          ];
+
+        # forwarded ports need to be accessible
+        networking.firewall.enable = false;
+      };
+  };
 }
