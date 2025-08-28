8cd315c30d14ab651f34e1b1276bb8bb663a408f
OPNA2608 <opna2608@protonmail.com>
2025-08-28 16:02:09 +0200

    bigbluebutton.{bbb-fsesl-client,bbb-fsesl-akka}: init at 3.0.10-bigbluebutton

--- pkgs/by-name/bigbluebutton/sbt-derivation.nix
+++ pkgs/by-name/bigbluebutton/sbt-derivation.nix
@@ -247,8 +247,8 @@ let
 
     depsWarmupCommand = ''
       mkdir -p $SBT_DEPS/project/.ivy/local
-      for thing in ${bbb-common-message}/*; do
-        ln -vs "$thing" $SBT_DEPS/project/.ivy/local/"$(basename "$thing")"
+      for dep in ${bbb-common-message}; do
+        lndir "$dep" $SBT_DEPS/project/.ivy/local
       done
 
       sbt compile
@@ -271,6 +271,7 @@ let
     nativeBuildInputs = [
       dpkg
       fakeroot
+      lndir
       makeWrapper
     ];
 
@@ -280,8 +281,8 @@ let
       cd akka-bbb-apps
 
       mkdir -p $SBT_DEPS/project/.ivy/local
-      for thing in ${bbb-common-message}/*; do
-        ln -vs "$thing" $SBT_DEPS/project/.ivy/local/"$(basename "$thing")"
+      for dep in ${bbb-common-message}; do
+        lndir "$dep" $SBT_DEPS/project/.ivy/local
       done
 
       sbt debian:packageBin
@@ -1142,6 +1143,137 @@ let
       };
     });
 
+  bbb-fsesl-client = mkSbtDerivation {
+    pname = "bbb-fsesl-client";
+
+    inherit (sharedBbbThings) version src postPatch;
+
+    overrideDepsAttrs = final: prev: {
+      preBuild = ''
+        cd bbb-fsesl-client
+      '';
+    };
+
+    depsWarmupCommand = ''
+      sbt compile
+    '';
+    depsArchivalStrategy = "copy";
+    depsOptimize = false;
+    depsSha256 = "sha256-C0Myb0SC8DWgWcjQyYhthFY9jvH+zG2qm+4dYr6aIVw=";
+
+    strictDeps = true;
+
+    buildPhase = ''
+      runHook preBuild
+
+      cd bbb-fsesl-client
+
+      sbt publishLocal
+
+      runHook postBuild
+    '';
+
+    installPhase = ''
+      runHook preInstall
+
+      mkdir $out
+      cp -t $out/ -r $SBT_DEPS/project/.ivy/local/*
+
+      runHook postInstall
+    '';
+
+    meta = sharedBbbThings.meta // {
+      description = sharedBbbThings.meta.description + " (bbb-fsesl-client)";
+    };
+  };
+
+  bbb-fsesl-akka = mkSbtDerivation {
+    pname = "bbb-fsesl-akka";
+
+    inherit (sharedBbbThings) version src postPatch;
+
+    overrideDepsAttrs = final: prev: {
+      preBuild = ''
+        cd akka-bbb-fsesl
+      '';
+    };
+
+    depsWarmupCommand = ''
+      mkdir -p $SBT_DEPS/project/.ivy/local
+      for dep in ${bbb-common-message} ${bbb-fsesl-client}; do
+        lndir "$dep" $SBT_DEPS/project/.ivy/local
+      done
+
+      sbt compile
+
+      find $SBT_DEPS/project/.ivy/local -type l -exec rm -v {} \;
+    '';
+    depsArchivalStrategy = "copy";
+    depsOptimize = false;
+    depsSha256 = "sha256-pTtfvZS6GeV/c+x8mYjFazps2DW7faTVmBAoThpD6rQ=";
+
+    strictDeps = true;
+
+    nativeBuildInputs = [
+      dpkg
+      fakeroot
+      lndir
+      makeWrapper
+    ];
+
+    buildPhase = ''
+      runHook preBuild
+
+      cd akka-bbb-fsesl
+
+      mkdir -p $SBT_DEPS/project/.ivy/local
+      for dep in ${bbb-common-message} ${bbb-fsesl-client}; do
+        lndir "$dep" $SBT_DEPS/project/.ivy/local
+      done
+
+      sbt debian:packageBin
+
+      find $SBT_DEPS/project/.ivy/local -type l -exec rm -v {} \;
+
+      runHook postBuild
+    '';
+
+    # TODO: More hardcoded assumptions in $out/share/bbb-fsesl-akka/conf/*
+    installPhase = ''
+      runHook preInstall
+
+      dpkg -x target/*.deb $out
+
+      # Fix up Debian-isms
+
+      # No usr please, we have the prefix for that
+      mv -vt $out/ $out/usr/*
+      rmdir $out/usr
+
+      # Fix broken symlinks, due to prefix not being / and no more usr
+      ln -vfs $out/share/bbb-fsesl-akka/conf $out/etc/bbb-fsesl-akka
+      ln -vfs $out/share/bbb-fsesl-akka/bin/bbb-fsesl-akka $out/bin/bbb-fsesl-akka
+
+      ln -vfs /var/lib/bbb-fsesl-akka/logs $out/share/bbb-fsesl-akka/logs
+
+      # We won't be using this, since it's read-only
+      rm -r $out/var/log
+      rmdir $out/var # Just to make sure we notice if there's ever smth else worth keeping here
+
+      # Add Nix-isms
+
+      # Add default Java
+      wrapProgram $out/share/bbb-fsesl-akka/bin/bbb-fsesl-akka \
+        --set-default JAVA_HOME ${jdk.home}
+
+      runHook postInstall
+    '';
+
+    meta = sharedBbbThings.meta // {
+      description = sharedBbbThings.meta.description + " (bbb-fsesl-akka)";
+    };
+  };
+
 in
 {
   inherit
@@ -1151,5 +1283,7 @@ in
     bbb-etherpad
     bbb-freeswitch-core
     bbb-freeswitch-sounds
+    bbb-fsesl-akka
+    bbb-fsesl-client
     ;
 }
