79a58f16eeb283cbeee683d8d0086f8376df59e2
Ethan Carter Edwards <ethan@ethancedwards.com>
2025-09-02 18:33:07 -0400

    projects/hockeypuck: init
    Signed-off-by: Ethan Carter Edwards <ethan@ethancedwards.com>

---
+++ projects/hockeypuck/default.nix
@@ -0,0 +1,32 @@
+{
+  lib,
+  pkgs,
+  sources,
+  ...
+}@args:
+
+{
+  metadata = {
+    summary = "Next generation OpenPGP keyserver";
+    subgrants = [
+      "Hockeypuck"
+    ];
+  };
+
+  nixos.modules.services = {
+    hockeypuck = {
+      name = "hockeypuck";
+      module = lib.moduleLocFromOptionString "services.hockeypuck";
+      examples."Enable hockeypuck" = {
+        module = ./services/hockeypuck/examples/basic.nix;
+        tests.basic.module = pkgs.nixosTests.hockeypuck;
+      };
+    };
+  };
+
+  nixos.demo.vm = {
+    module = ./services/hockeypuck/examples/basic.nix;
+    description = "Demo for hockeypuck";
+    tests.basic.module = pkgs.nixosTests.hockeypuck;
+  };
+}
