fc5543525ebb48e9058b51a2e64ca6c2f574a2f8
eljamm <fedi.jamoussi@protonmail.ch>
2025-09-15 12:13:17 +0200

    pkgs/kip: fix build

--- pkgs/by-name/kip/package.nix
+++ pkgs/by-name/kip/package.nix
@@ -1,6 +1,7 @@
 {
   lib,
   stdenv,
+  fetchFromGitLab,
   cmake,
   pkg-config,
   arpa2cm,
@@ -22,7 +23,6 @@
   libressl,
   cacert,
   gnutls,
-  fetchFromGitLab,
 }:
 let
   python-with-packages = python3.withPackages (
@@ -34,28 +34,15 @@ let
       colored
     ]
   );
-  libkrb5' = libkrb5.overrideAttrs (old: {
-    # kip needs the symbol `_et_list` associated with the com_err library.
-    # This is provided by e2fsprogs.
-    # Another dependency of kip, libkrb5, also provides the com_err library,
-    # but a version of it that doesn't provide `_et_list`.
-    # Without intervention, com_err from libkrb5 is selected, failing the build.
-    #
-    # Exclude com_err from libkrb5's outputs.
-    configureFlags = old.configureFlags ++ [ "--with-system-et" ];
-
-    # Provide libkrb5 with the com_err library.
-    buildInputs = old.buildInputs ++ [ e2fsprogs ];
-  });
 in
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "kip";
   version = "0.15.0";
 
   src = fetchFromGitLab {
     owner = "arpa2";
     repo = "kip";
-    rev = "v${version}";
+    tag = "v${finalAttrs.version}";
     hash = "sha256-A+tPaImjd9j1Vq69Dgh3j86xI/OcovwTZSULLkOVZaI=";
   };
 
@@ -77,7 +64,7 @@ stdenv.mkDerivation rec {
     unbound
     e2fsprogs
     cyrus_sasl
-    libkrb5'
+    libkrb5
     libev
     json_c
     bison
@@ -93,4 +80,4 @@ stdenv.mkDerivation rec {
   preBuild = ''
     patchShebangs test
   '';
-}
+})
