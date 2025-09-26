108746a7993a86e96b5a92602a2a7294c550aab7
June Stepp <git@junestepp.me>
2025-09-26 12:47:01 -0500

    overview: format render-template.py

--- overview/render-template.py
+++ overview/render-template.py
@@ -10,26 +10,28 @@ from pygments.formatters import HtmlFormatter
 jinja2_template_file = sys.argv[1]
 output_file = sys.argv[2]
 
+
 # Include code from a file and highlight it in HTML
 def include_code(language, file_path, relative_path=False):
-  if relative_path:
-    file_path = os.path.dirname(output_file) + "/" + file_path
-  with open(file_path) as file:
-    code = file.read()
-    try:
-        lexer = get_lexer_by_name(language, stripall=True)
-        formatter = HtmlFormatter(cssclass="code")
-        return highlight(code, lexer, formatter)
-    except Exception:
-        print("Fallback: Return the code without highlighting")
-        return f'<pre class="shell" class="shell"><code>{code}</code></pre>'
+    if relative_path:
+        file_path = os.path.dirname(output_file) + "/" + file_path
+    with open(file_path) as file:
+        code = file.read()
+        try:
+            lexer = get_lexer_by_name(language, stripall=True)
+            formatter = HtmlFormatter(cssclass="code")
+            return highlight(code, lexer, formatter)
+        except Exception:
+            print("Fallback: Return the code without highlighting")
+            return f'<pre class="shell" class="shell"><code>{code}</code></pre>'
+
 
 md = MarkdownIt("commonmark")
 
 with open(jinja2_template_file) as file:
-  jinja2_template = Template(file.read())
+    jinja2_template = Template(file.read())
 
 rendered = jinja2_template.render(markdown_to_html=md.render, include_code=include_code)
 
 with open(output_file, "w") as file:
-  file.write(rendered)
+    file.write(rendered)
