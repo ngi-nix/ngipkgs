ea3d243c815dc0882694120f596ebddc0d3f2fe6
eljamm <fedi.jamoussi@protonmail.ch>
2025-10-13 19:36:23 +0200

    taldir: add update script

--- pkgs/by-name/taldir/package.nix
+++ pkgs/by-name/taldir/package.nix
@@ -1,8 +1,12 @@
 {
-  fetchgit,
   lib,
+  fetchgit,
   recutils,
   buildGoModule,
+  gitUpdater,
+  writeShellApplication,
+  nix,
+  _experimental-update-script-combinators,
 }:
 buildGoModule (finalAttrs: {
   pname = "taldir";
@@ -38,6 +42,33 @@ buildGoModule (finalAttrs: {
 
   # dial error (dial tcp [::1]:5432: connect: connection refused)
   doCheck = false;
+
+  passthru = {
+    updateScriptSrc = gitUpdater { rev-prefix = "v"; };
+    updateScriptVendor = writeShellApplication {
+      name = "update-taldir-vendorHash";
+      runtimeInputs = [ nix ];
+      text = ''
+        export UPDATE_NIX_ATTR_PATH="''${UPDATE_NIX_ATTR_PATH:-taldir}"
+
+        oldhash="$(nix-instantiate . --eval --strict -A "$UPDATE_NIX_ATTR_PATH.goModules.drvAttrs.outputHash" | cut -d'"' -f2)"
+        newhash="$(nix-build -A "$UPDATE_NIX_ATTR_PATH.goModules" --no-out-link 2>&1 | tail -n3 | grep 'got:' | cut -d: -f2- | xargs echo || true)"
+
+        if [ "$newhash" == "" ]; then
+          echo "No new vendorHash."
+          exit 0
+        fi
+
+        fname="$(nix-instantiate --eval -E "with import ./. {}; (builtins.unsafeGetAttrPos \"version\" $UPDATE_NIX_ATTR_PATH).file" | cut -d'"' -f2)"
+
+        sed -i "s/$oldhash/$newhash/" "$fname"
+      '';
+    };
+    updateScript = _experimental-update-script-combinators.sequence [
+      finalAttrs.passthru.updateScriptSrc.command
+      (lib.getExe finalAttrs.passthru.updateScriptVendor)
+    ];
+  };
 
   meta = {
     homepage = "https://git.taler.net/taldir.git";
