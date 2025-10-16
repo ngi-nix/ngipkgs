1421ca0ee7826571a1a642eec03d565b4d631c97
eljamm <fedi.jamoussi@protonmail.ch>
2025-10-16 12:31:03 +0200

    inventaire-i18n: refactor update script

--- pkgs/by-name/inventaire-i18n/package.nix
+++ pkgs/by-name/inventaire-i18n/package.nix
@@ -2,14 +2,10 @@
   lib,
   buildNpmPackage,
   fetchFromGitea,
-  unstableGitUpdater,
-  writeShellApplication,
-  _experimental-update-script-combinators,
-  nix,
-  sd,
+  nix-update-script,
 }:
 
-buildNpmPackage rec {
+buildNpmPackage (finalAttrs: {
   pname = "inventaire-i18n";
   version = "0-unstable-2025-10-07";
 
@@ -27,40 +23,13 @@ buildNpmPackage rec {
     patchShebangs scripts
   '';
 
-  passthru = {
-    updateScriptSrc = unstableGitUpdater { };
-    updateScriptNpmDeps = writeShellApplication {
-      name = "update-inventaire-i18n-npmDepsHash";
-      runtimeInputs = [
-        nix
-        sd
-      ];
-      text = ''
-        export UPDATE_NIX_ATTR_PATH="''${UPDATE_NIX_ATTR_PATH:-inventaire-i18n}"
-
-        oldhash="$(nix-instantiate . --eval --strict -A ngipkgs."$UPDATE_NIX_ATTR_PATH".npmDepsHash | cut -d'"' -f2)"
-        newhash="$(nix-build -A ngipkgs."$UPDATE_NIX_ATTR_PATH".npmDeps --no-out-link 2>&1 | tail -n3 | grep 'got:' | cut -d: -f2- | xargs echo || true)"
-
-        if [ "$newhash" == "" ]; then
-          echo "No new npmDepsHash."
-          exit 0
-        fi
-
-        fname="$(nix-instantiate --eval -E "with import ./. {}; (builtins.unsafeGetAttrPos \"version\" ngipkgs.$UPDATE_NIX_ATTR_PATH).file" | cut -d'"' -f2)"
-        ${sd.meta.mainProgram} --string-mode "$oldhash" "$newhash" "$fname"
-      '';
-    };
-    updateScript = _experimental-update-script-combinators.sequence [
-      passthru.updateScriptSrc
-      (lib.getExe passthru.updateScriptNpmDeps)
-    ];
-  };
+  passthru.updateScript = nix-update-script { extraArgs = [ "--version=branch" ]; };
 
   meta = {
     description = "Repository hosting inventaire i18n strings and scripts";
     homepage = "https://codeberg.org/inventaire/inventaire-i18n";
     license = lib.licenses.agpl3Only;
     platforms = lib.platforms.all;
-    maintainers = with lib.maintainers; [ ];
+    teams = with lib.teams; [ ngi ];
   };
-}
+})
