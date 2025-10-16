a061e879fb85a652f5915f0d5479a1ed5b6e7e76
eljamm <fedi.jamoussi@protonmail.ch>
2025-10-16 12:54:34 +0200

    Skip updating packages with broken update scripts

--- default.nix
+++ default.nix
@@ -176,11 +176,42 @@ rec {
         runtimeInputs = with pkgs; [ nix-update ];
         text =
           let
+            skipped-packages = [
+              "atomic-browser" # -> atomic-server
+              "atomic-cli" # -> atomic-server
+              "firefox-meta-press" # -> meta-press
+              "inventaire" # -> inventaire-client
+              "kbin" # -> kbin-backend
+              "kbin-frontend" # -> kbin-backend
+              "pretalxFull" # -> pretalx
+              # FIX: needs custom update script
+              "marginalia-search"
+              "peertube-plugin-livechat"
+              # FIX: dream2nix
+              "corestore"
+              "liberaforms"
+              # FIX: package scope
+              "bigbluebutton"
+              "heads"
+              # FIX: don't update `sparql-queries` if there is no version change
+              "inventaire-client"
+              # fetcher not supported
+              "libervia-backend"
+              "libervia-desktop-kivy"
+              "libervia-media"
+              "libervia-templates"
+              "sat-tmp"
+              "urwid-satext"
+              # broken package
+              "libresoc-nmigen"
+              "libresoc-verilog"
+            ];
+            update-packages = with lib; filter (x: !elem x skipped-packages) (attrNames ngipkgs);
             update-commands = lib.concatMapStringsSep "\n" (package: ''
               if ! nix-update --flake --use-update-script "${package}" "$@"; then
                 echo "${package}" >> "$TMPDIR/failed_updates.txt"
               fi
-            '') (lib.attrNames ngipkgs);
+            '') update-packages;
           in
           # bash
           ''
