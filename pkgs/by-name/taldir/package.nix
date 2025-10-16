f9a43f7ba3b9474c5170fd8f044a76850e590082
eljamm <fedi.jamoussi@protonmail.ch>
2025-10-16 12:26:19 +0200

    taldir: 1.0.5 -> 1.0.5-unstable-2025-10-15; refactor update script

--- pkgs/by-name/taldir/package.nix
+++ pkgs/by-name/taldir/package.nix
@@ -3,22 +3,21 @@
   fetchgit,
   recutils,
   buildGoModule,
-  gitUpdater,
-  writeShellApplication,
-  nix,
   _experimental-update-script-combinators,
+  unstableGitUpdater,
+  nix-update-script,
 }:
 buildGoModule (finalAttrs: {
   pname = "taldir";
-  version = "1.0.5";
+  version = "1.0.5-unstable-2025-10-15";
 
   src = fetchgit {
     url = "https://git.taler.net/taldir.git";
-    tag = "v${finalAttrs.version}";
-    hash = "sha256-ZKNkMV0IV6E+yCQeabGXpIQclx1S4YEgFn4whGXTaks=";
+    rev = "8fbc813afb14807dbc62c4c95402f282a8165f07";
+    hash = "sha256-2tIS98MTPGTEYym7L9ch+tWdDJBvKVQwpfAkxZiJaSc=";
   };
 
-  vendorHash = "sha256-QCwakJTpRP7VT69EzQeInCCGBuNu3WsNCytnQcBdKQw=";
+  vendorHash = "sha256-G8eujeYLSlQ95hNLspAlWSj7MSB/eyg4iD2pp3kSupQ=";
 
   nativeBuildInputs = [
     recutils
@@ -43,32 +42,12 @@ buildGoModule (finalAttrs: {
   # dial error (dial tcp [::1]:5432: connect: connection refused)
   doCheck = false;
 
-  passthru = {
-    updateScriptSrc = gitUpdater { rev-prefix = "v"; };
-    updateScriptVendor = writeShellApplication {
-      name = "update-taldir-vendorHash";
-      runtimeInputs = [ nix ];
-      text = ''
-        export UPDATE_NIX_ATTR_PATH="''${UPDATE_NIX_ATTR_PATH:-taldir}"
-
-        oldhash="$(nix-instantiate . --eval --strict -A "$UPDATE_NIX_ATTR_PATH.goModules.drvAttrs.outputHash" | cut -d'"' -f2)"
-        newhash="$(nix-build -A "$UPDATE_NIX_ATTR_PATH.goModules" --no-out-link 2>&1 | tail -n3 | grep 'got:' | cut -d: -f2- | xargs echo || true)"
-
-        if [ "$newhash" == "" ]; then
-          echo "No new vendorHash."
-          exit 0
-        fi
-
-        fname="$(nix-instantiate --eval -E "with import ./. {}; (builtins.unsafeGetAttrPos \"version\" $UPDATE_NIX_ATTR_PATH).file" | cut -d'"' -f2)"
-
-        sed -i "s/$oldhash/$newhash/" "$fname"
-      '';
-    };
-    updateScript = _experimental-update-script-combinators.sequence [
-      finalAttrs.passthru.updateScriptSrc.command
-      (lib.getExe finalAttrs.passthru.updateScriptVendor)
-    ];
-  };
+  # Currently, `nix-update-script` can only get latest version from:
+  # codeberg/crates.io/gitea/github/gitlab/pypi/savannah/sourcehut/rubygems/npm
+  passthru.updateScript = _experimental-update-script-combinators.sequence [
+    (unstableGitUpdater { tagPrefix = "v"; }) # update version + source
+    (nix-update-script { extraArgs = [ "--version=skip" ]; }) # update deps
+  ];
 
   meta = {
     homepage = "https://git.taler.net/taldir.git";
