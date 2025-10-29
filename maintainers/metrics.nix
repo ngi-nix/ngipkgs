e44c4e650913207a7b4b21d37ab21b6475c0f6c2
eljamm <fedi.jamoussi@protonmail.ch>
2025-10-29 18:13:01 +0100

    refactor: metrics and get more accurate results

--- maintainers/metrics.nix
+++ maintainers/metrics.nix
@@ -1,3 +1,9 @@
+# Provide useful metrics about NGIpkgs deliverables
+
+## USAGE
+
+# nix eval --json -f default.nix metrics.summary
+
 {
   lib,
   pkgs,
@@ -11,17 +17,102 @@ let
     attrValues
     filterAttrs
     mapAttrs
-    count
     elem
     optionalAttrs
     filter
+    length
     ;
+
+  # return number of elements for a project's component
+  countComponent =
+    project: component:
+    if project.${component} == null then 0 else length (attrNames project.${component});
+
+  project-metrics = mapAttrs (
+    _: p:
+    optionalAttrs (p ? nixos) {
+      metadata = {
+        subgrants =
+          if lib.isAttrs p.metadata.subgrants then
+            mapAttrs (_: length) p.metadata.subgrants
+          else
+            { Uncategorized = length p.metadata.subgrants; };
+      };
+      nixos = {
+        tests = countComponent p.nixos "tests";
+        examples = countComponent p.nixos "examples";
+      }
+      // optionalAttrs (p ? nixos.modules.services) {
+        services = countComponent p.nixos.modules "services";
+      }
+      // optionalAttrs (p ? nixos.modules.programs) {
+        programs = countComponent p.nixos.modules "programs";
+      }
+      // optionalAttrs (p ? nixos.demo) {
+        demos = countComponent p.nixos "demo";
+      };
+    }
+  ) raw-projects;
+
+  /**
+    Sum all metrics of an attribute set
+
+    # Inputs
+
+    `attrPath`
+
+    : String that contains the attribute path
+
+    # Type
+
+    ```
+    countAttrs :: String -> AttrSet
+    ```
+  */
+  countAttrs =
+    attrPath:
+    lib.foldlAttrs (
+      acc: name: value:
+      let
+        component = lib.attrByPath (lib.splitString "." attrPath) { } value;
+        names = attrNames component;
+        accumulate = attr: (acc.${attr} or 0) + (component.${attr} or 0);
+      in
+      lib.foldl (acc: name: acc // { "${name}" = accumulate name; }) acc names
+    ) { } project-metrics;
+
+  summary = {
+    ngipkgs = {
+      projects = length (attrNames raw-projects);
+      derivations = length (attrNames ngipkgs);
+      metadata = {
+        subgrants = countAttrs "metadata.subgrants";
+      };
+      update-scripts = length (filter (d: d ? passthru.updateScript) (attrValues ngipkgs));
+    };
+    nixos = (countAttrs "nixos") // {
+      # TODO: not accurate since it doesn't take scopes into account
+      # use https://github.com/ngi-nix/ngipkgs/pull/1725
+      update-scripts = length (
+        filter (
+          d:
+          (builtins.tryEval d).success
+          && (elem lib.teams.ngi d.meta.teams or [ ])
+          && d ? passthru.updateScript
+        ) (attrValues pkgs)
+      );
+    };
+  };
 in
 rec {
+  inherit
+    project-metrics
+    summary
+    ;
+
   metrics = {
     projects = attrNames raw-projects;
-    in-ngipkgs = attrNames ngipkgs;
-    derivations = concatMap (p: attrNames p.packages) (attrValues raw-projects);
+    derivations = attrNames ngipkgs;
     with-services = attrNames (
       filterAttrs (name: p: p ? nixos.modules.services && p.nixos.modules.services != null) raw-projects
     );
@@ -63,32 +154,5 @@ rec {
     demo = filter (p: p.nixos.demo != null) (attrValues raw-projects);
   };
 
-  metrics-count = mapAttrs (name: value: count (_: true) value) metrics;
-
-  project-metrics = mapAttrs (
-    _: p:
-    {
-      derivations = count (_: true) (attrNames p.packages);
-    }
-    // optionalAttrs (p ? nixos) {
-      nixos = {
-        tests = if p.nixos.tests == null then 0 else count (_: true) (attrNames p.nixos.tests);
-        examples = if p.nixos.examples == null then 0 else count (_: true) (attrNames p.nixos.examples);
-      }
-      // optionalAttrs (p ? nixos.modules.services) {
-        services =
-          if p.nixos.modules.services == null then
-            0
-          else
-            count (_: true) (attrNames p.nixos.modules.services);
-      }
-      // optionalAttrs (p ? nixos.modules.programs) {
-        programs =
-          if p.nixos.modules.programs == null then
-            0
-          else
-            count (_: true) (attrNames p.nixos.modules.programs);
-      };
-    }
-  ) raw-projects;
+  count = mapAttrs (name: length) metrics;
 }
