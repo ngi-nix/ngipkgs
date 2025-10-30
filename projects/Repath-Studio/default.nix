5d2933eb87b5094818aacddd6bb08697108c6e6e
phanirithvij <phanirithvij2000@gmail.com>
2025-10-30 13:24:10 +0530

    projects/repath-studio: init
    Signed-off-by: phanirithvij <phanirithvij2000@gmail.com>

---
+++ projects/Repath-Studio/default.nix
@@ -0,0 +1,70 @@
+{
+  lib,
+  pkgs,
+  sources,
+  ...
+}@args:
+
+{
+  metadata = {
+    summary = "Vector graphics editor, that combines procedural tooling with traditional design workflows";
+    subgrants.Commons = [ "RepathStudio" ];
+    links = {
+      repo = {
+        text = "Source repository";
+        url = "https://github.com/repath-project/repath-studio";
+      };
+      homepage = {
+        text = "Homepage";
+        url = "https://repath.studio";
+      };
+      docs = {
+        text = "Documentation";
+        url = "https://repath.studio/get-started/build-from-source/";
+      };
+      blog = {
+        text = "Blog";
+        url = "https://repath.studio/blog/";
+      };
+    };
+  };
+
+  nixos.modules.programs = {
+    repath-studio = {
+      name = "repath-studio";
+      module = ./programs/repath-studio/module.nix;
+      examples."Enable repath-studio" = {
+        module = ./programs/repath-studio/examples/basic.nix;
+        tests.basic.module = import ./programs/repath-studio/tests/basic.nix args;
+      };
+      links = {
+        build = {
+          text = "Build from source";
+          url = "https://repath.studio/get-started/build-from-source/";
+        };
+        test = {
+          text = "Test instructions";
+          url = "https://repath.studio/get-started/interactive-shell/#examples";
+        };
+      };
+    };
+  };
+
+  nixos.demo.shell = {
+    module = ./demo/module.nix;
+    module-demo = ./demo/module-demo.nix;
+    usage-instructions = [
+      {
+        instruction = ''
+          Run `repath-studio` in the terminal
+        '';
+      }
+      {
+        instruction = ''
+          Verify the graphical window opens for Repath Studio.
+        '';
+      }
+    ];
+    tests.demo.module = import ./programs/repath-studio/tests/basic.nix args;
+  };
+}
---
+++ projects/Repath-Studio/demo/module.nix
@@ -0,0 +1,5 @@
+{ ... }:
+
+{
+  programs.repath-studio.enable = true;
+}
