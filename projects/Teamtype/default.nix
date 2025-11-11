956be0f7b97b551245ddbf7b44661574423512db
eljamm <fedi.jamoussi@protonmail.ch>
2025-11-11 09:37:58 +0100

    projects(Ethersync): rename to Teamtype; refactor
    - make description shorter and simpler
    - improve and expand on demo usage instructions

---
+++ projects/Teamtype/default.nix
@@ -0,0 +1,102 @@
+{
+  lib,
+  pkgs,
+  sources,
+  ...
+}@args:
+
+{
+  metadata = {
+    summary = "Peer-to-peer, editor-agnostic collaborative editing of local text files.";
+    subgrants = {
+      Core = [
+        "Teamtype"
+      ];
+    };
+    links = {
+      repo = {
+        text = "Homepage";
+        url = "https://teamtype.github.io";
+      };
+      docs = {
+        text = "Documentation";
+        url = "https://teamtype.github.io";
+      };
+      src = {
+        text = "Source repository";
+        url = "https://github.com/teamtype/teamtype";
+      };
+    };
+  };
+
+  nixos.modules.programs = {
+    teamtype = {
+      module = ./programs/teamtype/module.nix;
+      examples."Enable Teamtype" = {
+        module = ./programs/teamtype/examples/basic.nix;
+        tests.basic.module = import ./programs/teamtype/tests/basic.nix args;
+        tests.basic.problem.broken.reason = ''
+          Needs a self-hosted relay server to work non-interactively (without internet).
+          Requires: https://github.com/teamtype/teamtype/issues/344
+        '';
+      };
+    };
+  };
+
+  nixos.demo.shell = {
+    module = ./programs/teamtype/examples/basic.nix;
+    module-demo = ./module-demo.nix;
+    usage-instructions = [
+      {
+        instruction = ''
+          First, create a directory with a subdirectory named `.teamtype` and
+          share it:
+
+          ```
+          $ mkdir .teamtype
+          $ teamtype share --show-secret-address
+          ```
+
+          Wait a few seconds and a secret address will be printed that can be
+          used to connect to this server.
+        '';
+      }
+      {
+        instruction = ''
+          Next, create another directory and add the secret address from the
+          previous step to its config file:
+
+          ```
+          $ mkdir .teamtype
+          $ echo peer="<secret-address>" >.teamtype/config
+          $ teamtype join
+          ```
+
+          Then, join the shared directory:
+
+          ```
+          $ teamtype join
+          ```
+
+          The two directories are now connected.
+        '';
+      }
+      {
+        instruction = ''
+          You can now edit files in one directory and the changes will be synchronized
+          in the other. If you use Neovim, the instance in this shell has also been
+          configured with the Teamtype plugin, so you can try using `:TeamtypeInfo` and
+          `:TeamtypeJumpToCursor`.
+
+          For more information on using this project, please refer to the
+          [project documentation](https://teamtype.github.io).
+        '';
+      }
+    ];
+    tests.demo.module = import ./programs/teamtype/tests/basic.nix args;
+    tests.demo.problem.broken.reason = ''
+      Needs a self-hosted relay server to work non-interactively (without internet).
+      Requires: https://github.com/teamtype/teamtype/issues/344
+    '';
+  };
+}
--- projects/Teamtype/programs/teamtype/examples/basic.nix
+++ projects/Teamtype/programs/teamtype/examples/basic.nix
@@ -1,23 +1,23 @@
 { pkgs, ... }:
 
 {
-  programs.ethersync.enable = true;
+  programs.teamtype.enable = true;
 
   programs.vscode = {
     enable = true;
     # vscodium because vscode is unfree
     package = pkgs.vscodium;
     extensions = [
-      pkgs.vscode-extensions.ethersync.ethersync
+      pkgs.vscode-extensions.teamtype.teamtype
     ];
   };
 
   programs.neovim = {
     enable = true;
     configure = {
-      packages.ethersync = {
+      packages.teamtype = {
         start = [
-          pkgs.vimPlugins.ethersync
+          pkgs.vimPlugins.teamtype
         ];
       };
     };
