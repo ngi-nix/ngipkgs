02450224ab717eeb6de45708db3965fa2bd11559
Julien Moutinho <julm+ngipkgs@sourcephile.fr>
2025-11-14 01:25:11 +0100

    projects(bonfire): init service

---
+++ projects/Bonfire/default.nix
@@ -0,0 +1,50 @@
+{
+  lib,
+  pkgs,
+  sources,
+  ...
+}@args:
+
+{
+  metadata = {
+    summary = "An open-source framework for building federated digital spaces where people can gather, interact, and form communities online";
+    subgrants = {
+      Commons = [ ];
+      Core = [ ];
+      Entrust = [
+        "Bonfire-FederatedGroups"
+        "Bonfire-Framework"
+      ];
+      Review = [ "Bonfire" ];
+    };
+    links = {
+      homepage = {
+        text = "Home page";
+        url = "https://bonfirenetworks.org";
+      };
+      src = {
+        text = "Source code (only the top-level repository)";
+        url = "https://github.com/bonfire-networks/bonfire-app";
+      };
+    };
+  };
+
+  nixos.modules.services = {
+    bonfire = {
+      name = "service name";
+      module = ./services/bonfire/module.nix;
+      examples."Enable bonfire" = {
+        module = ./services/bonfire/examples/basic.nix;
+        description = ''
+          Usage instructions
+
+          1. Run `nix -L run -f . hydrated-projects.Bonfire.nixos.tests.basic.driverInteractive`
+          2. Open your browser to <http://localhost:4000/signup>
+          3. Create an account.
+        '';
+        tests.basic.module = import ./services/bonfire/tests/basic.nix args;
+      };
+    };
+  };
+
+}
