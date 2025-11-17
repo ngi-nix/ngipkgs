6ba7c255a2fd58c8e86b4552df6567b689c61bd7
Sizhe Zhao <prc.zhao@outlook.com>
2025-11-17 20:15:55 +0800

    manyfold: init at 0.129.1

---
+++ pkgs/by-name/manyfold/package.nix
@@ -0,0 +1,111 @@
+{
+  assimp,
+  bundlerEnv,
+  dart-sass,
+  fetchFromGitHub,
+  lib,
+  libarchive,
+  nodejs_22,
+  ruby_3_4,
+  stdenvNoCC,
+  writeTextDir,
+  yarn-berry_3,
+}:
+let
+  yarn-berry = yarn-berry_3;
+  ruby = ruby_3_4;
+  gems = bundlerEnv {
+    name = "manyfold-env";
+    inherit ruby;
+    gemdir = ./.;
+    extraConfigPaths = [
+      "${writeTextDir ".ruby-version" "3.4.7"}/.ruby-version"
+    ];
+  };
+in
+stdenvNoCC.mkDerivation (finalAttrs: {
+  pname = "manyfold";
+  version = "0.129.1";
+
+  src = fetchFromGitHub {
+    owner = "manyfold3d";
+    repo = "manyfold";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-L/kM5A47DxypckTYRno9qK4hYYD9HC6CXxIzCRlOAWU=";
+  };
+
+  prePatch = ''
+    echo 3.4.7 >.ruby-version
+    substituteInPlace Gemfile.lock \
+      --replace-fail '   ruby 3.4.1p0' "   ruby 3.4.7" \
+      --replace-fail '   2.6.2' "   2.6.9"
+  '';
+
+  nativeBuildInputs = [
+    nodejs_22
+    ruby
+    yarn-berry
+    yarn-berry.yarnBerryConfigHook
+  ];
+
+  missingHashes = ./missing-hashes.json;
+  offlineCache = yarn-berry.fetchYarnBerryDeps {
+    inherit (finalAttrs) src missingHashes;
+    hash = "sha256-Hrw7+1DOkKuoq4IUR/Gr/wMVjAPHGdI0nElYAgqAvnU=";
+  };
+
+  # dynamically loaded by ruby gems
+  env.LD_LIBRARY_PATH = lib.makeLibraryPath [
+    assimp
+    libarchive
+  ];
+
+  preBuild = ''
+    mkdir -p vendor/bundle/ruby
+    ln -s ${gems}/${ruby.gemPath} vendor/bundle/ruby/
+
+    bundle config set deployment true
+    bundle config set without "development test"
+
+    # force sass-embedded to use our own sass instead of the bundled one
+    substituteInPlace node_modules/sass-embedded/dist/lib/src/compiler-path.js \
+        --replace-fail 'compilerCommand = (() => {' 'compilerCommand = (() => { return ["${lib.getExe dart-sass}"];'
+  '';
+
+  buildPhase = ''
+    runHook preBuild
+
+    touch db/schema.rb
+
+    export DATABASE_URL="nulldb://user:pass@localhost/db"
+    export SECRET_KEY_BASE="placeholder"
+    export RACK_ENV="production"
+    export RAILS_ASSETS_PRECOMPILE=1
+
+    bundle exec rake assets:precompile
+
+    rm db/schema.rb
+
+    runHook postBuild
+  '';
+
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out
+    ln -s ${gems}/bin $out/
+
+    mkdir -p $out/lib/manyfold
+    cp -r . $out/lib/manyfold/
+
+    runHook postInstall
+  '';
+
+  meta = {
+    description = "ActivityPub-powered tool for storing and sharing 3d models";
+    homepage = "https://manyfold.app/";
+    downloadPage = "https://github.com/manyfold3d/manyfold";
+    license = lib.licenses.mit;
+    teams = [ lib.teams.ngi ];
+  };
+})
