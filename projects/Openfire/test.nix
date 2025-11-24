1094be25af1abe30ef2957a94bdaba505e884c44
eljamm <fedi.jamoussi@protonmail.ch>
2025-11-24 16:58:39 +0100

    wip test

--- projects/Openfire/test.nix
+++ projects/Openfire/test.nix
@@ -15,22 +15,53 @@
           sources.modules.ngipkgs
           sources.modules.services.openfire-server
           sources.examples.Openfire."Enable Openfire server"
+
+          # enable graphical session + users (alice, bob)
+          "${sources.inputs.nixpkgs}/nixos/tests/common/x11.nix"
+          "${sources.inputs.nixpkgs}/nixos/tests/common/user-account.nix"
         ];
+
+        services.xserver.enable = true;
+        test-support.displayManager.auto.user = "alice";
+
+        programs.chromium.enable = true;
+
+        environment.systemPackages = with pkgs; [
+          chromium
+          xdotool # automate clicks
+        ];
+
+        # more memory for chrome
+        virtualisation.memorySize = 4096;
       };
   };
+
+  enableOCR = true;
 
   testScript =
     { nodes, ... }:
     let
       port = toString nodes.server.services.openfire-server.servicePort;
+      user = nodes.server.users.users.alice;
+      env = "DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/${toString user.uid}/bus DISPLAY=:0";
     in
+    # py
     ''
+      def click_position(x: int, y: int):
+        server.succeed(f"su - alice -c '${env} xdotool mousemove --sync {x} {y} click 1'")
+        server.sleep(1)
+
       start_all()
 
       server.wait_for_unit("openfire-server.service")
       server.wait_for_open_port(${port})
 
       server.succeed("curl -f http://localhost:${port}")
+
+      server.wait_for_x()
+
+      # open browser
+      server.succeed("su - alice -c '${env} chromium http://127.0.0.1:9090 >&2 &'")
     '';
 
   # ssh -o User=root vsock/3
@@ -46,23 +77,22 @@
           let
             cfg = config.services.openfire-server;
           in
-          [
-            {
+          map
+            (port: {
               from = "host";
-              host.port = 2222;
-              guest.port = 22;
-            }
-            {
-              from = "host";
-              host.port = cfg.servicePort;
-              guest.port = cfg.servicePort;
-            }
-            {
-              from = "host";
-              host.port = cfg.securePort;
-              guest.port = cfg.securePort;
-            }
-          ];
+              host = { inherit port; };
+              guest = { inherit port; };
+            })
+            [
+              cfg.securePort
+              cfg.servicePort
+              # client to server
+              5222
+              5223
+              # web binding
+              7979
+              7443
+            ];
       };
   };
 }
