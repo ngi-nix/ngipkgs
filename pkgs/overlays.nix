6e305bcea510938b4911cf65fe0117d1da1fbe7c
eljamm <fedi.jamoussi@protonmail.ch>
2025-11-24 08:25:41 +0100

    chore: remove propagated package fixes

--- pkgs/overlays.nix
+++ pkgs/overlays.nix
@@ -16,19 +16,4 @@
         oldAttrs.checkFlags or [ ] ++ [ "-skip=^${lib.concatStringsSep "$|^" skippedTests}$" ];
     });
   })
-  # https://github.com/NixOS/nixpkgs/pull/462698
-  (final: prev: {
-    python3 = prev.python3.override {
-      packageOverrides = pyfinal: pyprev: {
-        sipsimple = pyprev.sipsimple.override { ffmpeg = final.ffmpeg_7; };
-      };
-    };
-    python3Packages = final.python3.pkgs;
-  })
-  # https://github.com/NixOS/nixpkgs/pull/462483
-  (final: prev: {
-    osm2pgsql = prev.osm2pgsql.override (oldAttrs: {
-      fmt = final.fmt_11;
-    });
-  })
 ]
