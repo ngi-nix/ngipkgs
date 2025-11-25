ee7e8169db6672b70d2eef2f7392d9358b0d470e
eljamm <fedi.jamoussi@protonmail.ch>
2025-11-25 12:25:43 +0100

    projects(SSTorytime): add options for local database
    Co-authored-by: Ivan Mincik <ivan.mincik@gmail.com>

--- projects/SSTorytime/services/sstorytime/examples/basic.nix
+++ projects/SSTorytime/services/sstorytime/examples/basic.nix
@@ -4,25 +4,14 @@
   services.sstorytime = {
     enable = true;
     port = 3030;
+    openFirewall = true;
+    database = {
+      createLocally = true;
+      dbname = "sstoryline";
+      user = "sstoryline";
+      # WARNING: ! Don't use this in production !
+      # Use a proper secret-management solution like `sops` or `agenix`.
+      passwordFile = pkgs.writeText "database-secret" "sst_1234";
+    };
   };
-
-  services.postgresql = {
-    enable = true;
-    ensureUsers = [
-      {
-        name = "sstoryline";
-        ensureDBOwnership = true;
-      }
-    ];
-    ensureDatabases = [ "sstoryline" ];
-    # WARNING: handling passwords in plaintext is not secure in production and
-    # is merely used for illustrative purposes, here.
-    initialScript = pkgs.writeText "postgresql-password" ''
-      CREATE ROLE sstoryline WITH LOGIN PASSWORD 'sst_1234' CREATEDB;
-    '';
-  };
-
-  # make sure SSToryTime only starts after the database has been initialized
-  systemd.services.sstorytime.requires = [ "postgresql.target" ];
-  systemd.services.sstorytime.after = [ "postgresql.target" ];
 }
--- projects/SSTorytime/services/sstorytime/module.nix
+++ projects/SSTorytime/services/sstorytime/module.nix
@@ -6,16 +6,22 @@
 }:
 let
   inherit (lib)
+    mkEnableOption
+    mkPackageOption
     mkOption
+    mkIf
     types
     ;
 
   cfg = config.services.sstorytime;
+
+  # only create a local database if we're gonna connect to it locally
+  localDB = cfg.database.createLocally;
 in
 {
   options.services.sstorytime = {
-    enable = lib.mkEnableOption "SSTorytime";
-    package = lib.mkPackageOption pkgs "sstorytime" { };
+    enable = mkEnableOption "SSTorytime";
+    package = mkPackageOption pkgs "sstorytime" { };
 
     port = mkOption {
       type = types.port;
@@ -23,32 +29,149 @@ in
       default = 8080;
     };
 
-    openFirewall = lib.mkEnableOption "the default ports in the firewall for the SSTorytime server.";
+    openFirewall = mkEnableOption "the default ports in the firewall for the SSTorytime server.";
+
+    user = mkOption {
+      type = types.nonEmptyStr;
+      default = "sstorytime";
+      description = "User account under which SSTorytime runs.";
+    };
+
+    group = mkOption {
+      type = types.nonEmptyStr;
+      default = "sstorytime";
+      description = "Group under which SSTorytime runs.";
+    };
+
+    database = {
+      createLocally = mkEnableOption "configure a local PostgreSQL database for SSTorytime.";
+
+      host = mkOption {
+        type = types.str;
+        default = "/var/run/postgresql";
+        example = "192.168.23.42";
+        description = "Database host address or unix socket.";
+      };
+
+      port = mkOption {
+        type = with types; nullOr port;
+        default = if localDB then null else 5432;
+        defaultText = lib.literalExpression ''
+          if `config.services.sstorytime.database.host` is `localhost` or `/run/postgresql`
+          then null
+          else 5432
+        '';
+        description = "Database host port.";
+      };
+
+      dbname = mkOption {
+        type = types.str;
+        default = "sstoryline";
+        description = "Database name.";
+      };
+
+      user = mkOption {
+        type = types.str;
+        default = "sstoryline";
+        description = "Database user.";
+      };
+
+      passwordFile = mkOption {
+        type = types.nullOr types.path;
+        default = null;
+        example = "/var/run/secrets/db-password";
+        description = ''
+          Path to a file containing the PostgreSQL password for
+          {option}`database.user`.
+        '';
+      };
+    };
   };
 
-  config = lib.mkIf cfg.enable {
+  config = mkIf cfg.enable {
+    assertions = [
+      {
+        assertion = localDB -> cfg.database.passwordFile != null;
+        message = ''
+          `services.sstorytime.database.passwordFile` must be set when using a local database.
+        '';
+      }
+    ];
+
+    services.sstorytime.database.createLocally = lib.mkDefault true;
+
+    users.groups.${cfg.group} = { };
+    users.users.${cfg.user} = {
+      description = "SSTorytime service user";
+      home = "/var/lib/sstorytime";
+      createHome = true;
+      isSystemUser = true;
+      group = cfg.group;
+    };
+
     systemd.services.sstorytime = {
       description = "SSTorytime Server";
       serviceConfig = {
-        ExecStart = ''
-          ${lib.getExe' cfg.package "http_server"}
-        '';
-        DynamicUser = true;
+        User = cfg.user;
+        Group = cfg.group;
+        LoadCredential = lib.optionals localDB [
+          "db_password:${toString cfg.database.passwordFile}"
+        ];
         Restart = "on-failure";
         RestartSec = 5;
-        StateDirectory = "sstorytime";
+      };
+      environment = {
+        SST_SERVER_PORT = toString cfg.port;
+        PGHOST = cfg.database.host;
+        PGUSER = cfg.database.user;
+        PGDATABASE = cfg.database.dbname;
+        PGPASSFILE = "%d/db_password";
       };
       unitConfig = {
         StartLimitBurst = 5;
         StartLimitInterval = 100;
       };
-      environment.SST_SERVER_PORT = toString cfg.port;
-      wantedBy = [ "multi-user.target" ];
-      after = [ "network.target" ];
+      script = ''
+        ${lib.getExe' cfg.package "http_server"}
+      '';
+      wantedBy = [
+        "multi-user.target"
+      ];
+      after = [
+        "network.target"
+        "sstorytime-setup.service"
+      ]
+      ++ lib.optionals localDB [ "postgresql.target" ];
     };
 
     networking.firewall.allowedTCPPorts = lib.optionals cfg.openFirewall [
       cfg.port
     ];
+
+    services.postgresql = mkIf localDB {
+      enable = true;
+      ensureUsers = [
+        {
+          name = cfg.database.user;
+        }
+      ];
+      ensureDatabases = [
+        cfg.database.dbname
+      ];
+      authentication = ''
+        local  all  all  trust
+
+        # This is a workaround for command connections, which appear to be
+        # trying to use tcp/ip instead of sockets.
+        # Remove when this is fixed, upstream.
+        host ${cfg.database.dbname} ${cfg.database.user} localhost trust
+      '';
+    };
+
+    systemd.services.postgresql-setup.postStart = lib.optionalString localDB ''
+      psql -tA <<'EOF'
+        ALTER DATABASE "${cfg.database.dbname}" OWNER TO "${cfg.database.user}"
+      EOF
+    '';
   };
 }
