e0125027cf348d853d65fa53d6a2fd29e138d4c8
eljamm <fedi.jamoussi@protonmail.ch>
2025-11-27 17:01:37 +0100

    projects(PdfDing): improve demo experience
    Co-Authored-By: phanirithvij <phanirithvij2000@gmail.com>

--- projects/PdfDing/demo/module-demo.nix
+++ projects/PdfDing/demo/module-demo.nix
@@ -1,6 +1,7 @@
 {
   lib,
   config,
+  pkgs,
   ...
 }:
 let
@@ -12,6 +13,34 @@ in
       echo "PdfDing is starting. Please wait ..."
       until systemctl show pdfding.service | grep -q ActiveState=active; do sleep 1; done
       echo "PdfDing is ready at http://localhost:${toString cfg.port}"
+    '';
+
+    services.pdfding.installWrapper = true;
+    services.pdfding.installTestHelpers = true;
+
+    systemd.services.pdfding.path = [ pkgs.sqlite ];
+    systemd.services.pdfding.postStart = ''
+      export DJANGO_SUPERUSER_PASSWORD=admin
+
+      ${cfg.package}/bin/pdfding-manage \
+        createsuperuser \
+          --no-input \
+          --username admin \
+          --email admin@localhost || echo "Admin user already exists. Skipping."
+
+      # Use grid layout by default
+      sqlite3 \
+        ${cfg.dataDir}/db/db.sqlite3 \
+        "UPDATE users_profile SET layout = 'Grid' WHERE layout = 'Compact';"
+    '';
+
+    systemd.services.pdfding-background.postStart = ''
+      # copy example file to consume it
+      install -D \
+        ${pkgs.pdfding.src}/pdfding/pdf/tests/data/dummy.pdf \
+        ${cfg.dataDir}/consume/1/example.pdf
+
+      consume-immediate
     '';
   };
 }
