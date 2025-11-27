9c83d303e45be0d673ca5493683b2606db2817df
eljamm <fedi.jamoussi@protonmail.ch>
2025-11-27 17:01:37 +0100

    projects(PdfDing): improve demo experience
    Co-Authored-By: phanirithvij <phanirithvij2000@gmail.com>

--- projects/PdfDing/demo/module.nix
+++ projects/PdfDing/demo/module.nix
@@ -3,6 +3,11 @@
   services.pdfding = {
     enable = true;
     openFirewall = true;
+    # NOTE: this is just for demo purposes
+    # you shoud not use this in production, use a secret management tool like sops-nix instead.
+    # see https://wiki.nixos.org/wiki/Comparison_of_secret_managing_schemes for other alternatives.
     secretKeyFile = pkgs.writeText "django_secret" "foobarbaz";
+    consume.enable = true; # allows bulk importing pdf files
+    consume.schedule = "*/1 * * * *"; # once every minute
   };
 }
