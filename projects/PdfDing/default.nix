eed3a90aba08c0bdb2e3435531187f2ca2cba606
phanirithvij <phanirithvij2000@gmail.com>
2025-11-29 15:46:22 +0530

    projects(PdfDing): init examples
    Co-authored-by: eljamm <fedi.jamoussi@protonmail.ch>
    Signed-off-by: phanirithvij <phanirithvij2000@gmail.com>

--- projects/PdfDing/default.nix
+++ projects/PdfDing/default.nix
@@ -29,6 +29,20 @@
   nixos.modules.services = {
     pdfding = {
       module = ./services/pdfding/module.nix;
+      examples = {
+        basic = {
+          module = ./services/pdfding/examples/basic.nix;
+          description = "Sqlite default service";
+        };
+        postgres = {
+          module = ./services/pdfding/examples/postgres.nix;
+          description = "Postgres and consume feature";
+        };
+        minio = {
+          module = ./services/pdfding/examples/minio.nix;
+          description = "Backup feature of pdfding";
+        };
+      };
       links = {
         build = {
           text = "Build from source";
