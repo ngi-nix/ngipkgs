aabb485ebbd009525a65e8451fc71d8953eaf94a
eljamm <fedi.jamoussi@protonmail.ch>
2025-12-05 10:42:02 +0100

    Refactor Openfire settings

--- projects/Openfire/settings.nix
+++ projects/Openfire/settings.nix
@@ -8,173 +8,166 @@ let
     mkOption
     types
     ;
-
-  # Define option types
-  user.options = {
-    username = mkOption {
-      type = types.str;
-    };
-    password = mkOption {
-      type = types.str;
-    };
-    name = mkOption {
-      type = types.str;
-      default = "";
-    };
-    email = mkOption {
-      type = types.str;
-      default = "";
-    };
-    roster = mkOption {
-      type = types.attrs;
-      default = { };
-    };
-  };
 in
+# See:
+# - https://download.igniterealtime.org/openfire/docs/latest/documentation/install-guide.html#autosetup
+# - https://github.com/igniterealtime/Openfire/tree/7693a6f8a19f61b5b026a54fe73f6d735dbe8336/xmppserver/src/main/webapp/setup
 {
-  autosetup.enable = mkOption {
-    type = types.bool;
-    default = true;
-    description = "Enable autosetup feature.";
-  };
-
-  autosetup.locale = mkOption {
-    type = types.str;
-    default = "en";
-    description = "Locale setting.";
-  };
-
-  autosetup.xmpp = {
-    domain = mkOption {
-      type = types.str;
-      default = "localhost";
-      description = "XMPP domain.";
+  autosetup = {
+    enable = mkOption {
+      type = types.bool;
+      default = true;
+      description = "Enable autosetup feature.";
     };
 
-    fqdn = mkOption {
+    locale = mkOption {
       type = types.str;
-      default = "localhost";
-      description = "Fully qualified domain name.";
+      default = "en";
+      description = "Locale setting.";
     };
 
-    auth = {
-      anonymous = mkOption {
+    xmpp = {
+      domain = mkOption {
+        type = types.str;
+        default = "localhost";
+        description = "XMPP domain.";
+      };
+      fqdn = mkOption {
+        type = types.str;
+        default = "localhost";
+        description = "Fully qualified domain name.";
+      };
+      auth.anonymous = mkOption {
         type = types.bool;
         default = true;
-        description = "Enable anonymous authentication.";
+        description = "Whether to enable anonymous authentication.";
+      };
+      socket.ssl.active = mkOption {
+        type = types.bool;
+        default = true;
+        description = "Whether to enable SSL.";
       };
     };
 
-    socket = {
-      ssl = {
-        active = mkOption {
-          type = types.bool;
-          default = true;
-          description = "Enable SSL.";
+    encryption = {
+      algorithm = mkOption {
+        type = types.str;
+        default = "AES";
+        description = "Encryption algorithm.";
+      };
+      key = mkOption {
+        type = types.str;
+        default = "some-key";
+        description = "Encryption key.";
+      };
+    };
+
+    database = {
+      # TODO: enum?
+      mode = mkOption {
+        type = types.str;
+        default = "standard";
+        description = "Database mode.";
+      };
+
+      defaultProvider = {
+        driver = mkOption {
+          type = types.str;
+          default = "org.postgresql.Driver";
+          description = "Database driver.";
+        };
+        serverURL = mkOption {
+          type = types.str;
+          default = "jdbc:postgresql://localhost:5432/a-database";
+          description = "Database server URL.";
+        };
+        username = mkOption {
+          type = types.str;
+          default = "a-database";
+          description = "Database username.";
+        };
+        password = mkOption {
+          type = types.str;
+          default = "a-password";
+          description = "Database password.";
+        };
+        minConnections = mkOption {
+          type = types.int;
+          default = 5;
+          description = "Minimum connections to the database.";
+        };
+        maxConnections = mkOption {
+          type = types.int;
+          default = 25;
+          description = "Maximum connections to the database.";
+        };
+        connectionTimeout = mkOption {
+          type = types.float;
+          default = 1.0;
+          description = "Timeout for database connections (in seconds).";
         };
       };
     };
-  };
 
-  autosetup.encryption = {
-    algorithm = mkOption {
-      type = types.str;
-      default = "AES";
-      description = "Encryption algorithm.";
-    };
-    key = mkOption {
-      type = types.str;
-      default = "some-key";
-      description = "Encryption key.";
-    };
-  };
-
-  autosetup.database = {
-    mode = mkOption {
-      type = types.str;
-      default = "standard";
-      description = "Database mode.";
-    };
-
-    defaultProvider = {
-      driver = mkOption {
+    admin = {
+      email = mkOption {
         type = types.str;
-        default = "org.postgresql.Driver";
-        description = "Database driver.";
+        default = "admin@example.com";
+        description = "Admin email.";
       };
-
-      serverURL = mkOption {
-        type = types.str;
-        default = "jdbc:postgresql://localhost:5432/a-database";
-        description = "Database server URL.";
-      };
-
-      username = mkOption {
-        type = types.str;
-        default = "a-database";
-        description = "Database username.";
-      };
-
       password = mkOption {
         type = types.str;
-        default = "a-password";
-        description = "Database password.";
-      };
-
-      minConnections = mkOption {
-        type = types.int;
-        default = 5;
-        description = "Minimum connections to the database.";
-      };
-
-      maxConnections = mkOption {
-        type = types.int;
-        default = 25;
-        description = "Maximum connections to the database.";
-      };
-
-      connectionTimeout = mkOption {
-        type = types.float;
-        default = 1.0;
-        description = "Timeout for database connections (in seconds).";
+        default = "admin";
+        description = "Admin password.";
       };
     };
-  };
 
-  autosetup.admin.email = mkOption {
-    type = types.str;
-    default = "admin@example.com";
-    description = "Admin email.";
-  };
-
-  autosetup.admin.password = mkOption {
-    type = types.str;
-    default = "admin";
-    description = "Admin password.";
-  };
-
-  autosetup.authprovider.mode = mkOption {
-    type = types.str;
-    default = "default";
-  };
-
-  autosetup.users = mkOption {
-    type = types.attrsOf (types.submodule user);
-    default = { };
-    example = {
-      user1 = {
-        username = "jane";
-        password = "secret";
-        name = "Jane Doe";
-        email = "user1@example.org";
-        roster = [
-          {
-            jid = "john@example.com";
-            nickname = "John";
-          }
-        ];
-      };
+    authprovider.mode = mkOption {
+      type = types.str;
+      default = "default";
     };
-    description = "User configurations.";
+
+    users =
+      let
+        user.options = {
+          username = mkOption {
+            type = types.str;
+          };
+          password = mkOption {
+            type = types.str;
+          };
+          name = mkOption {
+            type = types.str;
+            default = "";
+          };
+          email = mkOption {
+            type = types.str;
+            default = "";
+          };
+          roster = mkOption {
+            type = types.attrs;
+            default = { };
+          };
+        };
+      in
+      mkOption {
+        type = types.attrsOf (types.submodule user);
+        default = { };
+        example = {
+          user1 = {
+            username = "jane";
+            password = "secret";
+            name = "Jane Doe";
+            email = "user1@example.org";
+            roster = [
+              {
+                jid = "john@example.com";
+                nickname = "John";
+              }
+            ];
+          };
+        };
+        description = "User configurations.";
+      };
   };
 }
