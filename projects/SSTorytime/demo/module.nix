7017cc3fa6e21bbfa3b7a610918066f1ed025bef
eljamm <fedi.jamoussi@protonmail.ch>
2025-12-11 10:51:58 +0100

    projects(sstorytime): simplify database config options

--- projects/SSTorytime/demo/module.nix
+++ projects/SSTorytime/demo/module.nix
@@ -11,13 +11,6 @@
     enable = true;
     port = 3030;
     openFirewall = true;
-    database = {
-      createLocally = true;
-      dbname = "sstoryline";
-      user = "sstoryline";
-      # WARNING: ! Don't use this in production !
-      # Use a proper secret-management solution like `sops` or `agenix`.
-      passwordFile = pkgs.writeText "database-secret" "sst_1234";
-    };
+    createLocalDatabase = true;
   };
 }
--- projects/SSTorytime/services/sstorytime/examples/basic.nix
+++ projects/SSTorytime/services/sstorytime/examples/basic.nix
@@ -1,17 +1,13 @@
-{ pkgs, ... }:
+{
+  pkgs,
+  ...
+}:
 
 {
   services.sstorytime = {
     enable = true;
     port = 3030;
     openFirewall = true;
-    database = {
-      createLocally = true;
-      dbname = "sstoryline";
-      user = "sstoryline";
-      # WARNING: ! Don't use this in production !
-      # Use a proper secret-management solution like `sops` or `agenix`.
-      passwordFile = pkgs.writeText "database-secret" "sst_1234";
-    };
+    createLocalDatabase = true;
   };
 }
