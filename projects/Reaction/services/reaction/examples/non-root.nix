bf24a8980fa6c5b3d8a1e73011f78b19c97d0498
phanirithvij <phanirithvij2000@gmail.com>
2025-12-17 00:47:46 +0530

    projects/Reaction: pull in nixpkgs package, module, tests, examples
    Co-authored-by: eljamm <fedi.jamoussi@protonmail.ch>
    Signed-off-by: phanirithvij <phanirithvij2000@gmail.com>

--- projects/Reaction/services/reaction/examples/non-root.nix
+++ projects/Reaction/services/reaction/examples/non-root.nix
@@ -3,14 +3,18 @@
   config = {
     services.reaction = {
       enable = true;
+      stopForFirewall = false;
       # example.jsonnet/example.yml can be copied and modified from ${pkgs.reaction}/share/examples
       settingsFiles = [ "${pkgs.reaction}/share/examples/example.jsonnet" ];
       runAsRoot = false;
     };
     services.openssh.enable = true;
     # If not running as root you need to give the reaction user and service the proper permissions
-    # e.g. these are required for permitting `journalctl` and `iptables`, use
+
+    # allows reading journal logs of processess
     users.users.reaction.extraGroups = [ "systemd-journal" ];
+
+    # allows modifying ip firewall rules
     systemd.services.reaction.unitConfig.ConditionCapability = "CAP_NET_ADMIN";
     systemd.services.reaction.serviceConfig = {
       CapabilityBoundingSet = [ "CAP_NET_ADMIN" ];
