ab3b996815c649f1800f91d7d99236a527f1401b
eljamm <fedi.jamoussi@protonmail.ch>
2025-12-16 09:50:54 +0100

    projects(Openfire): enable autosetup

--- projects/Openfire/example.nix
+++ projects/Openfire/example.nix
@@ -5,5 +5,12 @@
     openFirewall = true;
     servicePort = 9090;
     securePort = 9191;
+
+    # Settings to be configured on first startup.
+    # For available options, see:
+    # https://download.igniterealtime.org/openfire/docs/latest/documentation/install-guide.html#Autosetup
+    settings.jive.autosetup = {
+      run = true;
+    };
   };
 }
