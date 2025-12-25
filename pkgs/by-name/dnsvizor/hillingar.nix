1fad067ec699b34ee2ed5ef7ee1701d6c1e4b9fb
Lin Jian <me@linj.tech>
2025-12-26 05:16:13 +0800

    dnsvizor: add comments about supported targets

--- pkgs/by-name/dnsvizor/hillingar.nix
+++ pkgs/by-name/dnsvizor/hillingar.nix
@@ -59,11 +59,12 @@ let
 
   # bad: eval failure by IFD or build failure
   # do not use/set meta.broken because it doesn't cover eval failure by IFD
+  # more detailed support state can be found in https://github.com/Solo5/solo5/blob/dabc69fd89b8119449ec4088c54b458d4ccc851b/docs/building.md?plain=1#L55
   knownBad =
     target:
     (lib.elem target [
       "macosx"
-      "genode"
+      "genode" # removed in solo5 0.7.0
     ])
     || (
       stdenv.hostPlatform.isAarch64
