098ca4747d1e762943af10ed047fce3b2ea413cd
eljamm <fedi.jamoussi@protonmail.ch>
2026-01-05 22:50:28 +0100

    pkgs: clean up overlays

--- pkgs/overlays.nix
+++ pkgs/overlays.nix
@@ -16,50 +16,6 @@
         oldAttrs.checkFlags or [ ] ++ [ "-skip=^${lib.concatStringsSep "$|^" skippedTests}$" ];
     });
   })
-  # Fix compatibility with qt 6.10 + switch to unstable
-  # https://github.com/NixOS/nixpkgs/pull/467792
-  # https://github.com/NixOS/nixpkgs/pull/467794
-  (final: prev: {
-    kaidan =
-      (prev.kaidan.overrideAttrs (oldAttrs: {
-        version = "0.13.0-unstable-2025-12-03";
-        src = final.fetchFromGitLab {
-          domain = "invent.kde.org";
-          owner = "network";
-          repo = "kaidan";
-          rev = "f9d9d236aa0fc584771524c1078ab899a9cd5822";
-          hash = "sha256-O3L3VEB7HsPYF0FyJtma98SlxgFIADZd/uhfJyEucGQ=";
-        };
-      })).override
-        {
-          # https://github.com/NixOS/nixpkgs/pull/460354
-          qxmpp = prev.qxmpp.overrideAttrs rec {
-            version = "1.12.0";
-            src = final.fetchFromGitLab {
-              domain = "invent.kde.org";
-              owner = "libraries";
-              repo = "qxmpp";
-              tag = "v${version}";
-              hash = "sha256-soOu6JyS/SEdwUngOUd0suImr70naZms9Zy2pRwBn5E=";
-            };
-          };
-        };
-  })
-  # Canaille
-  (final: prev: {
-    python3 = prev.python3.override {
-      packageOverrides = pyfinal: pyprev: {
-        # https://github.com/pallets-eco/flask-alembic/issues/47
-        flask-alembic = pyprev.flask-alembic.overridePythonAttrs {
-          meta.broken = false;
-          doCheck = pyfinal.pythonOlder "3.13";
-        };
-      };
-    };
-    canaille = prev.canaille.overridePythonAttrs (oldAttrs: {
-      doCheck = final.python3Packages.pythonOlder "3.13";
-    });
-  })
   (final: prev: {
     sylkserver = prev.sylkserver.overrideAttrs (oldAttrs: {
       postPatch = oldAttrs.postPatch or "" + ''
@@ -71,10 +27,22 @@
       '';
     });
   })
-  # https://github.com/NixOS/nixpkgs/pull/477157
   (final: prev: {
     python3 = prev.python3.override {
       packageOverrides = pyfinal: pyprev: {
+        # Canaille
+        # https://github.com/pallets-eco/flask-alembic/issues/47
+        flask-alembic = pyprev.flask-alembic.overridePythonAttrs rec {
+          version = "3.2.0";
+          src = final.fetchFromGitHub {
+            owner = "pallets-eco";
+            repo = "flask-alembic";
+            tag = version;
+            hash = "sha256-g5xl5CEfSZUbZxCLYykjd94eVjxzBAkgoBcR4y7IYfM=";
+          };
+          meta.broken = false;
+        };
+        # https://github.com/NixOS/nixpkgs/pull/477157
         sipsimple = pyprev.sipsimple.overridePythonAttrs (
           oldAttrs:
           let
