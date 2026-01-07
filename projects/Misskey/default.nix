b09b888544bf16e74272f659cdabec8ac4b4fb7f
eljamm <fedi.jamoussi@protonmail.ch>
2026-01-07 11:43:13 +0100

    pkgs(misskey): 2025.7.0 -> 2025.12.2

--- projects/Misskey/default.nix
+++ projects/Misskey/default.nix
@@ -16,11 +16,25 @@
   nixos.modules.services = {
     misskey = {
       name = "Misskey";
-      module = lib.moduleLocFromOptionString "services.misskey";
+      # Revert when this is merged and propagated:
+      # https://github.com/NixOS/nixpkgs/pull/477674
+      module =
+        { config, ... }:
+        let
+          cfg = config.services.misskey;
+        in
+        {
+          imports = [ (lib.moduleLocFromOptionString "services.misskey") ];
+          config.systemd.services.misskey.preStart = lib.optionalString cfg.enable ''
+            install -m 700 ${
+              (pkgs.formats.json { }).generate "misskey-config.json" cfg.settings
+            } /run/misskey/default.json
+          '';
+        };
       examples.basic = {
         module = ./services/misskey/examples/basic.nix;
         description = "";
-        tests.misskey.module = pkgs.nixosTests.misskey;
+        tests.misskey.module = import ./services/misskey/test.nix args;
       };
     };
   };
