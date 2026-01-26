d8d827071aff1cd0c04185b765b4c605b522db58
eljamm <fedi.jamoussi@protonmail.ch>
2026-01-26 16:14:03 +0100

    projects(Hypermachine): disable node libraries

--- projects/Hypermachines/programs/Hypermachines/module.nix
+++ projects/Hypermachines/programs/Hypermachines/module.nix
@@ -14,12 +14,17 @@ in
 
   config = lib.mkIf cfg.enable {
     environment.systemPackages = with pkgs; [
-      autobase
-      corestore
+      # TODO: These have been removed from Nixpkgs because they're nodejs
+      # libraries and it wasn't clear how they would be used. See:
+      # - https://github.com/NixOS/nixpkgs/pull/403638#issuecomment-3339125475
+      # - https://github.com/NixOS/nixpkgs/pull/379542
+      #
+      # autobase
+      # corestore
+      # hypercore
+      # hyperswarm
       hyperbeam
       hyperblobs
-      hypercore
-      hyperswarm
     ];
   };
 }
