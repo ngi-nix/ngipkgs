5d552372bd5351c2d21e8161152843cfe924b054
eljamm <fedi.jamoussi@protonmail.ch>
2026-02-02 10:12:19 +0100

    projects(repath-studio): fix and refactor test

--- projects/Repath-Studio/programs/repath-studio/tests/basic.nix
+++ projects/Repath-Studio/programs/repath-studio/tests/basic.nix
@@ -43,18 +43,14 @@
     ''
       start_all()
 
-      # `env DISPLAY=:0 sudo -u alice watch -t -n 0.1 xdotool getmouselocation` for finding coordinates
-      def click_position(x: int, y: int):
-          machine.succeed(f"env DISPLAY=:0 sudo -u alice xdotool mousemove --sync {x} {y} click 1")
-          machine.sleep(1)
-
       machine.wait_for_x()
       machine.succeed("env DISPLAY=:0 sudo -u alice repath-studio &> /tmp/repath.log &")
-      machine.wait_for_text("Welcome") # initial telemetry prompt
+      machine.wait_for_text(r"(Welcome|Repath|Studio)") # initial telemetry prompt
 
       machine.screenshot("Repath-Studio-GUI-Welcome")
-      click_position(440, 440) # "No, thank you" button
+      machine.send_key("kp_enter") # OK
 
+      machine.sleep(2)
       machine.send_key("ctrl-shift-s")
       machine.sleep(2)
       machine.send_chars("/tmp/saved.rps\n")
