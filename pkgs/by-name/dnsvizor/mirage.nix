b868d7444c819b469f5a6666fef7660201db3023
Julien Moutinho <julm+ngipkgs@sourcephile.fr>
2026-02-05 01:14:33 +0100

    build/standardize(mirage): rewrite with the following improvements:
    - use multiple outputs instead of multiple derivations
    - use a single materializedDir
    - use lib.extendMkDerivation
    - use camelCase
    - use nix eval to get materialization paths
    - use stripDebugFlags instead of stripAllList
    - use $out/share/mirageos (automatically stripped) instead of $out
    - use upstream's letter casing: "DNSvisor" in documentation

--- pkgs/by-name/dnsvizor/mirage.nix
+++ pkgs/by-name/dnsvizor/mirage.nix
@@ -18,10 +18,10 @@ rec {
       mirageDir ? ".",
       query,
       src,
-      target,
       opamPackages ? opam-nix.queryToScope { } ({ mirage = "*"; } // query),
       ...
     }:
+    target:
     stdenv.mkDerivation {
       name = "mirage-${pname}-${target}";
       inherit src version;
@@ -45,100 +45,151 @@ rec {
       '';
     };
 
-  # Description: read opam files from mirage-conf and build the unikernel.
-  build =
-    {
-      pname,
-      version,
-      mirageDir ? ".",
-      queryArgs ? { },
-      query ? { },
-      monorepoQuery,
-      packages-materialized-path,
-      monorepo-materialized-path,
-      target,
-      overrideAttrs ? finalAttrs: previousAttrs: { },
-      ...
-    }@args:
-    let
-      name = "mirage-${pname}-${target}";
-      mirage-conf = configure args;
-      mirage-conf-unmaterialized = configure (
-        args
-        // {
-          opamPackages = packages-unmaterialized;
-        }
-      );
-      packages-materialized = opam-nix.materializeOpamProject { } name mirage-conf query;
-      monorepo-materialized = opam-nix.materializeBuildOpamMonorepo { } mirage-conf monorepoQuery;
-      monorepo-unmaterialized = opam-nix.unmaterializeQueryToMonorepo { } monorepo-materialized-path;
-      packages-unmaterialized =
-        (opam-nix.materializedDefsToScope {
-          sourceMap.${name} = mirage-conf-unmaterialized;
-        } packages-materialized-path).overrideScope
-          (
-            finalOpam: previousOpam: {
-              ${name} = previousOpam.${name}.overrideAttrs (previousAttrs: {
-                inherit version;
-                __intentionallyOverridingVersion = true;
-
-                env =
-                  previousAttrs.env or { }
-                  // lib.optionalAttrs (finalOpam ? "ocaml-solo5") {
-                    OCAMLFIND_CONF = finalOpam.ocaml-solo5 + "/lib/findlib.conf";
-                  };
-
-                buildPhase = ''
-                  runHook preBuild
-                  mkdir duniverse
-                  echo '(vendored_dirs *)' > duniverse/dune
-                  ${lib.concatStringsSep "\n" (
-                    lib.mapAttrsToList (
-                      # ToDo: get dune build to pick up symlinks?
-                      name: path: "cp -r ${path} duniverse/${lib.toLower name}"
-                    ) monorepo-unmaterialized
-                  )}
-                  # Note: doesn't fail on warnings
-                  dune build ${mirageDir} --profile release
-                  runHook postBuild
-                '';
-
-                installPhase = ''
-                  runHook preInstall
-                  mkdir $out
-                  cp -L ${mirageDir}/dist/${pname}* $out/
-                  runHook postInstall
-                '';
-
-                # Reduce the full closure size by several hundreds MiB
-                # By not propagating inputs and stripping all symbols.
-                doNixSupport = false;
-                stripAllList = previousAttrs.stripAllList or [ ] ++ [ "." ];
-              });
+  # Description: read opam files from mirage configuration
+  # and build a unikernel in a separate output
+  # for each one of the given targets.
+  build = lib.extendMkDerivation {
+    constructDrv = stdenv.mkDerivation;
+    excludeDrvArgNames = [
+      "materializedDir"
+      "monorepoQuery"
+      "overrideUnikernel"
+      "query"
+      "queryArgs"
+    ];
+    extendDrvArgs =
+      finalAttrs:
+      {
+        pname,
+        version,
+        targets,
+        src,
+        monorepoQuery,
+        materializedDir,
+        mirageDir ? ".",
+        queryArgs ? { },
+        query ? { },
+        overrideUnikernel ? finalAttrs: previousAttrs: { },
+        ...
+      }@args:
+      let
+        name = "mirage-${pname}";
+        mirageConfIFD = configure args;
+        mirageConf =
+          target:
+          configure (
+            args
+            // {
+              opamPackages = packages target;
             }
-          );
-      unikernel =
-        if lib.pathExists packages-materialized-path && lib.pathExists monorepo-materialized-path then
-          packages-unmaterialized.${name}
-        else
-          # Give access to `passthru` when materialized files
-          # have not yet been generated.
-          stdenv.mkDerivation {
-            name = "stub";
-            src = null;
+          ) target;
+        packagesMaterialized =
+          target: opam-nix.materializeOpamProject { } "${name}-${target}" (mirageConfIFD target) query;
+        monorepoMaterialized =
+          target: opam-nix.materializeBuildOpamMonorepo { } (mirageConfIFD target) monorepoQuery;
+        monorepo =
+          target: opam-nix.unmaterializeQueryToMonorepo { } (materializedDir + "/${target}/monorepo.json");
+        packages =
+          target:
+          (opam-nix.materializedDefsToScope {
+            sourceMap."${name}-${target}" = finalAttrs.passthru.mirageConf.${target};
+          } (materializedDir + "/${target}/packages.json")).overrideScope
+            (
+              finalOpam: previousOpam: {
+                "${name}-${target}" = previousOpam."${name}-${target}".overrideAttrs (
+                  lib.composeExtensions (finalUnikernel: previousUnikernel: {
+                    inherit version;
+                    __intentionallyOverridingVersion = true;
+
+                    env =
+                      previousUnikernel.env or { }
+                      // lib.optionalAttrs (finalOpam ? "ocaml-solo5") {
+                        OCAMLFIND_CONF = finalOpam.ocaml-solo5 + "/lib/findlib.conf";
+                      };
+
+                    buildPhase = ''
+                      runHook preBuild
+                      mkdir duniverse
+                      echo '(vendored_dirs *)' > duniverse/dune
+                      ${lib.concatStringsSep "\n" (
+                        lib.mapAttrsToList (name: path: "cp -r ${path} duniverse/${lib.toLower name}") (
+                          finalAttrs.passthru.monorepo.${target}
+                        )
+                      )}
+                      dune build ${mirageDir} --profile release
+                      runHook postBuild
+                    '';
+
+                    installPhase = ''
+                      runHook preInstall
+                      mkdir -p $out/share/mirageos/
+                      cp -L ${mirageDir}/dist/${pname}* $out/share/mirageos/
+                      runHook postInstall
+                    '';
+
+                    # Reduce the full closure size by several hundreds MiB
+                    # since if you're using an unikernel you probably care about this.
+                    doNixSupport = false;
+                    stripAllList = previousUnikernel.stripAllList or [ ] ++ [ "share/mirageos" ];
+                  }) overrideUnikernel
+                );
+              }
+            );
+      in
+      {
+        inherit name;
+        inherit src;
+        outputs = [ "out" ] ++ targets;
+        installPhase = ''
+          runHook preInstall
+          ${
+            if stdenv.hostPlatform.isLinux && lib.elem "unix" targets then
+              "ln -s $unix $out"
+            else if stdenv.hostPlatform.isDarwin && lib.elem "macosx" targets then
+              "ln -s $macosx $out"
+            else
+              "mkdir $out"
+          }
+          ${lib.concatMapStringsSep "\n" (target: ''
+            cp -R ${finalAttrs.passthru.packages.${target}."${name}-${target}"} ''$${target}
+          '') targets}
+          runHook postInstall
+        '';
+        passthru = {
+          updateScript = writeShellApplication {
+            name = "dnsvizor-update";
+            runtimeInputs = [
+              coreutils
+              jq
+              nix
+            ];
+            text = ''
+              set -x
+              materializedDir=$(nix --extra-experimental-features nix-command -L eval \
+                -f. ${pname}.passthru.materializedDir)
+            ''
+            + lib.concatMapStringsSep "\n" (target: ''
+              mkdir -p "${materializedDir}/${target}/"
+              packagesJson=$(nix --extra-experimental-features nix-command -L build \
+                --no-link --print-out-paths --allow-import-from-derivation --show-trace \
+                -f. ${pname}.passthru.packagesMaterialized.${target})
+              jq <"$packagesJson" >"''${materializedDir}/${target}/packages.json"
+              monorepoJson=$(nix --extra-experimental-features nix-command -L build \
+                --no-link --print-out-paths --allow-import-from-derivation --show-trace \
+                -f. ${pname}.passthru.monorepoMaterialized.${target})
+              jq <"$monorepoJson" >"''${materializedDir}/${target}/monorepo.json"
+            '') targets;
           };
-    in
-    unikernel.overrideAttrs (previousAttrs: {
-      passthru = previousAttrs.passthru or { } // {
-        inherit
-          mirage-conf
-          mirage-conf-unmaterialized
-          monorepo-materialized
-          packages-materialized
-          packages-unmaterialized
-          ;
+          mirageConfIFD = lib.genAttrs targets mirageConfIFD;
+          mirageConf = lib.genAttrs targets mirageConf;
+          monorepoMaterialized = lib.genAttrs targets monorepoMaterialized;
+          packagesMaterialized = lib.genAttrs targets packagesMaterialized;
+          packages = lib.genAttrs targets packages;
+          monorepo = lib.genAttrs targets monorepo;
+          inherit materializedDir;
+        };
       };
-    });
+  };
 
   possibleTargets = [
     "genode"
@@ -151,68 +202,4 @@ rec {
     "virtio"
     "xen"
   ];
-
-  builds =
-    {
-      pname,
-      targets,
-      packages-materialized-path,
-      monorepo-materialized-path,
-      overrideAttrs ? _finalAttrs: _previousAttrs: { },
-      ...
-    }@args:
-    let
-      self = lib.genAttrs targets (
-        target:
-        (build (
-          args
-          // {
-            inherit target;
-            monorepo-materialized-path = monorepo-materialized-path + "/${target}.json";
-            packages-materialized-path = packages-materialized-path + "/${target}.json";
-          }
-        )).overrideAttrs
-          (
-            lib.composeExtensions (finalAttrs: previousAttrs: {
-              passthru = previousAttrs.passthru or { } // {
-                updateScript = writeShellApplication {
-                  name = "${pname}-${target}-update";
-                  runtimeInputs = [
-                    coreutils
-                    jq
-                    nix
-                  ];
-                  text = ''
-                    set -x
-                    packagesJson=$(nix --extra-experimental-features nix-command -L build \
-                      --no-link --print-out-paths --allow-import-from-derivation -f. \
-                      ${pname}.${target}.packages-materialized)
-                    jq <"$packagesJson" |
-                    install -Dm660 /dev/stdin pkgs/by-name/${pname}/packages-materialized/${target}.json
-
-                    monorepoJson=$(nix --extra-experimental-features nix-command -L build \
-                      --no-link --print-out-paths --allow-import-from-derivation -f. \
-                      ${pname}.${target}.monorepo-materialized)
-                    jq <"$monorepoJson" |
-                    install -Dm660 /dev/stdin pkgs/by-name/${pname}/monorepo-materialized/${target}.json
-                  '';
-                };
-              };
-            }) overrideAttrs
-          )
-      );
-    in
-    lib.recurseIntoAttrs (
-      self
-      // {
-        updateScript = writeShellApplication {
-          name = "dnsvizor-update";
-          runtimeInputs = [
-            jq
-            nix
-          ];
-          text = lib.concatMapStringsSep "\n" (target: lib.getExe self.${target}.updateScript) targets;
-        };
-      }
-    );
 }
