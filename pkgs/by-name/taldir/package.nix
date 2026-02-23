6dbeea993b382707018f408a6e8f93f578207718
eljamm <fedi.jamoussi@protonmail.ch>
2026-02-23 09:38:04 +0100

    taldir: 1.0.5-unstable-2025-11-07 -> 1.3.3-unstable-2026-02-17

--- pkgs/by-name/taldir/package.nix
+++ pkgs/by-name/taldir/package.nix
@@ -9,45 +9,36 @@
 }:
 buildGoModule (finalAttrs: {
   pname = "taldir";
-  version = "1.0.5-unstable-2025-11-07";
+  version = "1.3.3-unstable-2026-02-17";
 
   src = fetchgit {
     url = "https://git-www.taler.net/taldir.git";
-    rev = "dc4fbabd435b108a78d0cc445bb4225f92eb0e22";
-    hash = "sha256-WgkOZ+FWN6NLL8DR/wcscKJ0h2uOs+pi3BbT8t3/XE4=";
-    # Update submodules to use `git-www.taler.net` since `git.gnunet.org` no
-    # longer hosts source code.
-    leaveDotGit = true;
-    fetchSubmodules = false;
-    postFetch = ''
-      pushd $out
-        git reset --hard HEAD
-
-        substituteInPlace .gitmodules \
-          --replace-fail "git.gnunet.org" "git-www.taler.net"
-
-        git submodule update --init --recursive
-
-        rm -rf .git
-      popd
-    '';
+    rev = "228a58526dcdb6863021e10109dafb411b5726d4";
+    hash = "sha256-f/50jnf7h0SzHnRVbp7vbwRpMsMb6wqb1P7m+kDRrPY=";
   };
 
-  vendorHash = "sha256-G8eujeYLSlQ95hNLspAlWSj7MSB/eyg4iD2pp3kSupQ=";
+  vendorHash = "sha256-eZFE/hWQPG88lZT9KM/j2B0uoWvWQaPoMNcBKf5jWj8=";
 
   nativeBuildInputs = [
     recutils
   ];
 
-  # From Makefile
+  # From contrib/gana_update.sh
   preBuild = ''
-    mkdir -p internal/gana
+    TALDIR_SRC_ROOT="$PWD"
+    GANA_TMP=`mktemp -d`
 
-    pushd third_party/gana/gnu-taler-error-codes
-    make taler_error_codes.go
+    cleanup() { rm -rf "$GANA_TMP" ; }
+    trap cleanup EXIT
+
+    pushd $GANA_TMP || exit 1
+    cp -R ${finalAttrs.passthru.gana}/. .
+    chmod -R u+w .
+    make -C gnu-taler-error-codes taler_error_codes.go  >/dev/null && \
+      cp gnu-taler-error-codes/taler_error_codes.go $TALDIR_SRC_ROOT/internal/gana/ || exit 1
     popd
 
-    cp third_party/gana/gnu-taler-error-codes/taler_error_codes.go internal/gana/
+    cleanup
   '';
 
   subPackages = [
@@ -58,11 +49,17 @@ buildGoModule (finalAttrs: {
   # dial error (dial tcp [::1]:5432: connect: connection refused)
   doCheck = false;
 
+  passthru.gana = fetchgit {
+    url = "https://git-www.taler.net/gana.git";
+    rev = "eb54e871ae23f3f8ca7ce46c314fd995c930511e";
+    hash = "sha256-hUTo/h5fT/40PdzvxHl8tX8oPDdWHC1gw3yIBbrCxuI=";
+  };
+
   # Currently, `nix-update-script` can only get latest version from:
   # codeberg/crates.io/gitea/github/gitlab/pypi/savannah/sourcehut/rubygems/npm
   passthru.updateScript = _experimental-update-script-combinators.sequence [
-    (unstableGitUpdater { tagPrefix = "v"; }) # update version + source
-    (nix-update-script { extraArgs = [ "--version=skip" ]; }) # update deps
+    (unstableGitUpdater { tagPrefix = "v"; }) # version + source
+    (nix-update-script { extraArgs = [ "--version=skip" ]; }) # Go deps
   ];
 
   meta = {
