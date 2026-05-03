6291ebd51e95fb66651fdb757c7f1f5bf3776b86
alikhere <alikhurshid.dev@gmail.com>
2026-05-03 08:20:39 +0530

    Mobilizon: add metadata.links
    Co-authored-by: loudgolem <phanirithvij2000@gmail.com>

--- projects/Mobilizon/default.nix
+++ projects/Mobilizon/default.nix
@@ -17,6 +17,20 @@
         "Mobilizon"
       ];
     };
+    links = {
+      homepage = {
+        text = "Homepage";
+        url = "https://mobilizon.org/";
+      };
+      docs = {
+        text = "Documentation";
+        url = "https://docs.mobilizon.org/";
+      };
+      source = {
+        text = "Source Code";
+        url = "https://framagit.org/framasoft/mobilizon";
+      };
+    };
   };
 
   nixos.modules.services = {
